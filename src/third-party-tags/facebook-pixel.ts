0144b038e8cd03a15ca4b235be4fa078c11281c0
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-09-29 12:58:28 +0300

    chore: switch fb to use name

--- src/third-party-tags/facebook-pixel.ts
+++ src/third-party-tags/facebook-pixel.ts
@@ -6,6 +6,6 @@ export const fbPixel: GetThirdPartyTag = ({
 }) => ({
 	shouldRun,
 	url: `https://www.facebook.com/tr?id=${facebookAccountId}&ev=PageView&noscript=1`,
-	sourcepointId: '5e7e1298b8e05c54a85c52d2',
+	name: 'fb',
 	useImage: true,
 });
