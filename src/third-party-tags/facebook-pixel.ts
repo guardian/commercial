f4713dfc6a1d1706bb1223acedfed333d99f758f
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-09-29 13:46:17 +0300

    chore: lint fix

--- src/third-party-tags/facebook-pixel.ts
+++ src/third-party-tags/facebook-pixel.ts
@@ -1,8 +1,6 @@
 import { GetThirdPartyTag } from '../types';
 
-export const fbPixel: GetThirdPartyTag = ({
-	shouldRun,
-}) => ({
+export const fbPixel: GetThirdPartyTag = ({ shouldRun }) => ({
 	shouldRun,
 	url: `https://www.facebook.com/tr?id=279880532344561&ev=PageView&noscript=1`,
 	name: 'fb',
