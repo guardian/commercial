00188c7238a376b1d48731d7fba395c3fb3bec3c
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-10-07 12:15:51 +0100

    added twitter and lotame 3pts

---
+++ src/third-party-tags/lotame.ts
@@ -0,0 +1,32 @@
+import { GetThirdPartyTag } from '../types';
+import { lotameScript } from './external-scripts/lotame-script';
+
+export type LotameData = {
+	ozoneLotameData: Array<string>;
+	ozoneLotameProfileId: string;
+};
+
+let lotameData: LotameData;
+
+const ozoneLotameCallback = (obj: {
+	getAudiences: () => Array<string>;
+	getProfileId: () => string;
+}) => {
+	lotameData = {
+		ozoneLotameData: obj.getAudiences(),
+		ozoneLotameProfileId: obj.getProfileId(),
+	};
+};
+
+const beforeLoad = () => {
+	lotameScript(ozoneLotameCallback);
+};
+
+export const getLotameData: () => LotameData = () => lotameData;
+
+export const lotame: GetThirdPartyTag = ({ shouldRun }) => ({
+	shouldRun,
+	url: '//tags.crwdcntrl.net/lt/c/12666/lt.min.js',
+	beforeLoad,
+	name: 'lotame',
+});
