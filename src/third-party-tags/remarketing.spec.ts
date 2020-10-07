4f03585c4eb33d62603d8093f0b17c0abdd23493
Ashish Puliyel <ashish@puliyel.com>
2020-10-07 14:07:59 +0100

    test(remarketing.spec): fix for remarketing test

--- src/third-party-tags/remarketing.spec.ts
+++ src/third-party-tags/remarketing.spec.ts
@@ -1,11 +1,11 @@
 import { remarketing } from './remarketing';
 
 describe('remarketing', () => {
-	it('should use the feature switch option', () => {
-		expect(remarketing({ shouldRun: true })).toBe(expect.objectContaining({
+	it('should have expected properties', () => {
+		expect(remarketing({ shouldRun: true })).toMatchObject({
 			shouldRun: true,
 			url: '//www.googleadservices.com/pagead/conversion_async.js',
 	        name: 'remarketing',
-		}));
+		});
 	});
 });
