7fdab7d8e043aedde5242e4bbd9fc77490625099
Ashish Puliyel <ashish@puliyel.com>
2020-10-09 10:04:41 +0100

    refactor(remarketing.ts): moved types to type file

--- src/third-party-tags/remarketing.ts
+++ src/third-party-tags/remarketing.ts
@@ -1,18 +1,5 @@
 import { GetThirdPartyTag } from '../types';
 
-type GoogleTagParams = unknown;
-type GoogleTrackConversionObject = {
-	google_conversion_id: number;
-	google_custom_params: GoogleTagParams;
-	google_remarketing_only: boolean;
-};
-declare global {
-	interface Window {
-		google_trackConversion: (arg0: GoogleTrackConversionObject) => void;
-		google_tag_params: GoogleTagParams;
-	}
-}
-
 const onLoad = () => {
 	window.google_trackConversion({
 		google_conversion_id: 971225648,
