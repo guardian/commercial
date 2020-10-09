517ddd1fde5721fe61cecb2d1b71ec91d5fb92a1
Max Duval <max@mxdvl.com>
2020-10-09 16:13:47 +0100

    feat(third-party-tag): add inizio

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { permutive } from './third-party-tags/permutive';
 export { fbPixel } from './third-party-tags/facebook-pixel';
 export { twitter } from './third-party-tags/twitter-uwt';
 export { lotame } from './third-party-tags/lotame';
+export { inizio } from './third-party-tags/inizio';
---
+++ src/third-party-tags/inizio.ts
@@ -0,0 +1,34 @@
+import { GetThirdPartyTag } from '../types';
+
+const onLoad = () => {
+	const handleQuerySurveyDone = (
+		surveyAvailable: boolean,
+		survey: { measurementId: string },
+	) => {
+		if (surveyAvailable) {
+			if (window && window.googletag) {
+				window.googletag.cmd.push(() => {
+					window.googletag.pubads().setTargeting('inizio', 't');
+				});
+			}
+			console.log(`surveyAvailable: ${survey.measurementId}`);
+		}
+	};
+	// eslint-disable-next-line no-underscore-dangle
+	window._brandmetrics = window._brandmetrics || [];
+	// eslint-disable-next-line no-underscore-dangle
+	window._brandmetrics.push({
+		cmd: '_querySurvey',
+		val: {
+			callback: handleQuerySurveyDone,
+		},
+	});
+};
+
+export const inizio: GetThirdPartyTag = ({ shouldRun }) => ({
+	shouldRun,
+	url:
+		'//cdn.brandmetrics.com/survey/script/e96d04c832084488a841a06b49b8fb2d.js',
+	sourcepointId: '5e37fc3e56a5e6615502f9c9',
+	onLoad,
+});
