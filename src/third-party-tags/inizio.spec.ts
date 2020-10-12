6e04d600d8c57dddc3b888c6e56ef7fe6b8666b1
Max Duval <max@mxdvl.com>
2020-10-12 14:16:45 +0100

    refactor(inizio): looser check on matching

--- src/third-party-tags/inizio.spec.ts
+++ src/third-party-tags/inizio.spec.ts
@@ -3,12 +3,11 @@ import { inizio } from './inizio';
 describe('index', () => {
 	it('should use the feature switch option', () => {
 		const inizioInstance = inizio({ shouldRun: true });
-		expect(inizioInstance).toStrictEqual({
+		expect(inizioInstance).toMatchObject({
 			shouldRun: true,
 			url:
 				'//cdn.brandmetrics.com/survey/script/e96d04c832084488a841a06b49b8fb2d.js',
 			name: 'inizio',
-			onLoad: inizioInstance.onLoad,
 		});
 	});
 });
