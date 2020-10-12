97c54a90775e493008604bef12056b43d7be5e30
Ashish Puliyel <ashish@puliyel.com>
2020-10-12 15:48:52 +0100

    feat(google remarketing tags): added support for GoogleRemarketing tags as a thirdparty tag (#52)
    Co-authored-by: Ashish Puliyel <ashish.puliyel.freelancer@guardian.co.uk>

---
+++ src/third-party-tags/remarketing.spec.ts
@@ -0,0 +1,11 @@
+import { remarketing } from './remarketing';
+
+describe('remarketing', () => {
+	it('should have expected properties', () => {
+		expect(remarketing({ shouldRun: true })).toMatchObject({
+			shouldRun: true,
+			url: '//www.googleadservices.com/pagead/conversion_async.js',
+			name: 'remarketing',
+		});
+	});
+});
