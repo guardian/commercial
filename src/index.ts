8a222a89ae74d2978ade1f0347575b615d7edbb2
ioanna0 <ioanna.kyprianou@guardian.co.uk>
2020-10-13 17:00:15 +0300

    Export lotame data

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { ias } from './third-party-tags/ias';
 export { permutive } from './third-party-tags/permutive';
 export { fbPixel } from './third-party-tags/facebook-pixel';
 export { twitter } from './third-party-tags/twitter-uwt';
-export { lotame } from './third-party-tags/lotame';
+export { lotame, getLotameData } from './third-party-tags/lotame';
+export type { LotameData } from './third-party-tags/lotame';
