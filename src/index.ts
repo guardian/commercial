cf3d6dee134cada558e9e41efbdbbafc0b8b593b
Ioanna Kyprianou <51630004+ioanna0@users.noreply.github.com>
2020-10-14 17:50:46 +0300

    fix: export TPT type  (#60)
    * fix: export TPT type, export remarketing

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export { ias } from './third-party-tags/ias';
 export { permutive } from './third-party-tags/permutive';
 export { fbPixel } from './third-party-tags/facebook-pixel';
 export { twitter } from './third-party-tags/twitter-uwt';
+export { remarketing } from './third-party-tags/remarketing';
 export { lotame, getLotameData } from './third-party-tags/lotame';
-export type { LotameData } from './types';
+export type { LotameData, ThirdPartyTag } from './types';
--- src/third-party-tags/remarketing.spec.ts
+++ src/third-party-tags/remarketing.spec.ts
@@ -2,10 +2,12 @@ import { remarketing } from './remarketing';
 
 describe('remarketing', () => {
 	it('should have expected properties', () => {
-		expect(remarketing({ shouldRun: true })).toMatchObject({
+		const remarketingTag = remarketing({ shouldRun: true });
+		expect(remarketingTag).toMatchObject({
 			shouldRun: true,
 			url: '//www.googleadservices.com/pagead/conversion_async.js',
 			name: 'remarketing',
+			onLoad: remarketingTag.onLoad,
 		});
 	});
 });
