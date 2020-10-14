b489be9c9d5c6c2435fec38a2a2e05358f77f28d
Ioanna Kyprianou <51630004+ioanna0@users.noreply.github.com>
2020-10-14 16:34:59 +0300

    Revert "export lotamedata type (#58)"
    This reverts commit ce3f03b12d3ddb6dedad2f0b1ef7d2aa88ed8767.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,3 @@ export { permutive } from './third-party-tags/permutive';
 export { fbPixel } from './third-party-tags/facebook-pixel';
 export { twitter } from './third-party-tags/twitter-uwt';
 export { lotame, getLotameData } from './third-party-tags/lotame';
-export type { LotameData } from './types';
