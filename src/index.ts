4120b69a177d0605628d89df449b3d306d5357fa
Ioanna Kyprianou <51630004+ioanna0@users.noreply.github.com>
2020-10-14 13:29:55 +0300

    feat: Export more lotame data (#56)
    Export lotame data

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { ias } from './third-party-tags/ias';
 export { permutive } from './third-party-tags/permutive';
 export { fbPixel } from './third-party-tags/facebook-pixel';
 export { twitter } from './third-party-tags/twitter-uwt';
-export { lotame } from './third-party-tags/lotame';
+export { lotame, getLotameData } from './third-party-tags/lotame';
