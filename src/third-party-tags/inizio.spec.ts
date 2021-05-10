14f266d7b1a9c688d27fc73398f4d59125875985
Max Duval <max.duval@guardian.co.uk>
2021-05-10 14:06:32 +0100

    fix lint

--- src/third-party-tags/inizio.spec.ts
+++ src/third-party-tags/inizio.spec.ts
@@ -5,8 +5,7 @@ describe('index', () => {
 		const inizioInstance = inizio({ shouldRun: true });
 		expect(inizioInstance).toMatchObject({
 			shouldRun: true,
-			url:
-				'//cdn.brandmetrics.com/survey/script/e96d04c832084488a841a06b49b8fb2d.js',
+			url: '//cdn.brandmetrics.com/survey/script/e96d04c832084488a841a06b49b8fb2d.js',
 			name: 'inizio',
 		});
 	});
