a125585b672b25ec75e48ab25900d4885e684b32
Chris Jones <chrislomaxjones96@gmail.com>
2021-09-14 18:17:09 +0100

    Update src/ad-sizes.test.ts
    Co-authored-by: Max Duval <max.duval@theguardian.com>

--- src/ad-sizes.test.ts
+++ src/ad-sizes.test.ts
@@ -4,15 +4,15 @@ const { getAdSize } = _;
 
 describe('ad sizes', () => {
 	it.each([
-		[2, 2, 2, 2, '2,2'],
-		[100, 100, 100, 100, '100,100'],
-		[320, 50, 320, 50, '320,50'],
-		[970, 250, 970, 250, '970,250'],
-		[0, 0, 0, 0, 'fluid'],
+		[2, 2, '2,2'],
+		[100, 100, '100,100'],
+		[320, 50, '320,50'],
+		[970, 250, '970,250'],
+		[0, 0, 'fluid'],
 	])(
-		'getAdSize(%d,%d) outputs width %d, height %d, string %s',
-		(width, height, expectedWidth, expectedHeight, expectedString) => {
-			const adSize = getAdSize(width, height);
+		'getAdSize(%d,%d) outputs string "%s"',
+		(expectedWidth, expectedHeight, expectedString) => {
+			const adSize = getAdSize(expectedWidth, expectedHeight);
 			expect(adSize.width).toEqual(expectedWidth);
 			expect(adSize.height).toEqual(expectedHeight);
 			expect(adSize.toString()).toEqual(expectedString);
