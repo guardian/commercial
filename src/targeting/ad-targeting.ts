68ff20488756a083355d16862e0c5ea4064c60f9
Max Duval <max.duval@theguardian.com>
2021-11-03 15:29:25 +0000

    add meaty JS Docs comments
    and by meaty I mean chunky,
    ideally not made from animal flesh

--- src/targeting/ad-targeting.ts
+++ src/targeting/ad-targeting.ts
@@ -56,10 +56,47 @@ type ContentType =
 	| 'video';
 
 type NotSureTargeting = {
-	gdncrm: string | string[]; // GuarDiaN CRM
-	ms: string; // Media Source
-	slot: string; // (predefined list)
-	x: string; // kruX user segments (deprecated?)
+	/**
+	 * **G**uar**d**ia**n** **C**ustomer **R**elation **M**anagement - [see on Ad Manager][gam]
+	 *
+	 * Type: *Dynamic*
+	 *
+	 * Sample values:
+	 * - `"PXPE"`
+	 * - `"PXHCI2"`
+	 *
+	 * [gam]: https://example.com/
+	 */
+	gdncrm: string | string[];
+	/** **M**edia **S**ource - [see on Ad Manager][gam]
+	 *
+	 * Type: *Dynamic*
+	 *
+	 * Sample values:
+	 * - `"epictv"`
+	 * - `"crane.tv"`
+	 * - `"a-rational-fear"`
+	 *
+	 * [gam]: https://example.com
+	 */
+	ms: string;
+	/**
+	 * Ad **Slot** ID
+	 *
+	 * Type: *Predefined*
+	 *
+	 * Sample values: 'top-above-nav', 'inline-1'
+	 *
+	 * [gam]: https://example.com/
+	 */
+	slot: string; // TODO: Narrow to known IDs
+	/**
+	 * kruX user segments
+	 * https://example.com/
+	 *
+	 * @deprecated We no longer use Krux
+	 */
+	x: string;
 };
 let notSureTargeting: NotSureTargeting;
 let resolveNotSureTargetingReady: () => void;
