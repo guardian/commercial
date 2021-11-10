400abe7af5b7cf5d1886d00de28d1beda4bcc03a
Max Duval <max.duval@theguardian.com>
2021-11-10 17:17:57 +0000

    reorganise index

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,45 @@
 /* istanbul ignore file -- there's no point check this for test coverage */
 
-export { ias } from './third-party-tags/ias';
-export { permutive } from './third-party-tags/permutive';
-export { fbPixel } from './third-party-tags/facebook-pixel';
-export { twitter } from './third-party-tags/twitter-uwt';
-export { inizio } from './third-party-tags/inizio';
-export { remarketing } from './third-party-tags/remarketing';
-export { EventTimer } from './EventTimer';
-export { sendCommercialMetrics } from './sendCommercialMetrics';
-export type { ThirdPartyTag } from './types';
-export { adSizes } from './ad-sizes';
-export type { SizeKeys, AdSizeString, AdSize } from './ad-sizes';
-export { isAdBlockInUse } from './detectAdBlocker';
-export {
-	clearPermutiveSegments,
-	getPermutiveSegments,
-	getPermutivePFPSegments,
-} from './permutive';
-export { buildAdsConfigWithConsent, disabledAds } from './ad-targeting-youtube';
 export type {
 	AdsConfig,
 	AdsConfigBasic,
 	AdsConfigDisabled,
 	AdTargetingBuilder,
 	CustomParams,
+	ThirdPartyTag,
 } from './types';
-export * as constants from './constants';
 
-export { initAdTargeting, onAdTargetingUpdate } from './targeting/';
+export { ias } from './third-party-tags/ias';
+export { permutive } from './third-party-tags/permutive';
+export { fbPixel } from './third-party-tags/facebook-pixel';
+export { twitter } from './third-party-tags/twitter-uwt';
+export { inizio } from './third-party-tags/inizio';
+export { remarketing } from './third-party-tags/remarketing';
+
+export { EventTimer } from './EventTimer';
+
+export { sendCommercialMetrics } from './sendCommercialMetrics';
+
+export type { SizeKeys, AdSizeString, AdSize } from './ad-sizes';
+export { adSizes } from './ad-sizes';
+
+export { isAdBlockInUse } from './detectAdBlocker';
+
+export {
+	clearPermutiveSegments,
+	getPermutiveSegments,
+	getPermutivePFPSegments,
+} from './permutive';
+
+export { buildAdsConfigWithConsent, disabledAds } from './ad-targeting-youtube';
+export {
+	getAdFreeTargeting,
+	getContentTargeting,
+	getPersonalisedTargeting,
+	getSessionTargeting,
+	getUnsureTargeting,
+	getViewportTargeting,
+} from './targeting/';
+export type { AdTargeting } from './targeting/';
+
+export * as constants from './constants';
