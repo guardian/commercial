004ef716fa0c093b70f42ad333bf623d9150d177
Max Duval <max.duval@theguardian.com>
2021-12-09 10:48:07 +0000

    feat: Pick targeting values from object (#446)
    * pick targeting values in a type-safe way
    
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

---
+++ src/targeting/pick-targeting-values.spec.ts
@@ -0,0 +1,70 @@
+import { pickTargetingValues } from './pick-targeting-values';
+
+describe('Pick defined values from an object', () => {
+	test('TypeScript validates input', () => {
+		// @ts-expect-error -- we don’t want nulls
+		expect(pickTargetingValues({ null: null })).toEqual({});
+
+		// @ts-expect-error -- we don’t want booleans
+		expect(pickTargetingValues({ true: true })).toEqual({});
+
+		// @ts-expect-error -- we don’t want booleans
+		expect(pickTargetingValues({ false: false })).toEqual({});
+
+		expect(
+			pickTargetingValues({
+				arrayOfNonStrings: [
+					// @ts-expect-error -- we don’t want undefined
+					undefined,
+					// @ts-expect-error -- we don’t want null
+					null,
+					// @ts-expect-error -- we don’t want boolean
+					true,
+					// @ts-expect-error -- we don’t want boolean
+					false,
+					// @ts-expect-error -- we don’t want numbers
+					0,
+					// @ts-expect-error -- we don’t want numbers
+					1,
+					// @ts-expect-error -- we don’t want numbers
+					NaN,
+					// @ts-expect-error -- we don’t want numbers
+					Infinity,
+				],
+			}),
+		).toEqual({});
+	});
+	test('TypeScript validates output', () => {
+		const maybeTargeting = {
+			string: 'one',
+			strings: ['two', 'three'],
+			undefined: undefined,
+			emptyString: '',
+			emptyArray: [],
+			arrayOfEmptyString: [''],
+			arrayOfEmptyStrings: ['', '', ''],
+		} as const;
+
+		const targeting = pickTargetingValues(maybeTargeting);
+
+		expect(targeting.string).toEqual(maybeTargeting.string);
+		expect(targeting.strings).toEqual(maybeTargeting.strings);
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.undefined).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.null).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.true).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.false).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.emptyString).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.emptyArray).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.arrayOfEmptyString).toBeUndefined();
+		expect(targeting.arrayOfEmptyStrings).toBeUndefined();
+		// @ts-expect-error -- this is what we’re checking
+		expect(targeting.arrayOfNonStrings).toBeUndefined();
+	});
+});
---
+++ src/targeting/pick-targeting-values.ts
@@ -0,0 +1,67 @@
+import { isString } from '@guardian/libs';
+
+type ValidTargeting<T, K> = '' extends T
+	? never
+	: [] extends T
+	? never
+	: [''] extends T
+	? never
+	: T extends boolean
+	? never
+	: T extends NonNullable<T>
+	? K
+	: never;
+type DefinedKeys<T> = { [K in keyof T]-?: ValidTargeting<T[K], K> }[keyof T];
+type ObjectWithDefinedValues<T> = Pick<T, DefinedKeys<T>>;
+
+const isTargetingString = (string: unknown): boolean =>
+	isString(string) && string !== '';
+
+const isTargetingArray = (array: unknown): boolean =>
+	Array.isArray(array) && array.filter(isTargetingString).length > 0;
+
+const isValidTargeting = (value: unknown): value is string | string[] => {
+	if (isTargetingString(value)) return true;
+	if (isTargetingArray(value)) return true;
+	return false;
+};
+
+/**
+ * Picks only keys with targeting values from an object.
+ * A targeting values is defined as either:
+ * - a non-empty string
+ * - an array of non-empty strings
+ *
+ * If you object is read-only, you can safely access properties on the result.
+ * For example:
+ *
+ * ```ts
+ * dirty = {
+ *   valid: 'real',
+ *   invalid: undefined,
+ * } as const;
+ *
+ * clean = pickDefinedValues(dirty);
+ *
+ * // @ts-expect-error -- you can’t access this property
+ * clean.invalid
+ * ```
+ */
+export const pickTargetingValues = <
+	T extends Record<string, string | Readonly<string[]> | undefined>,
+>(
+	obj: T,
+): ObjectWithDefinedValues<T> => {
+	const initialValue = {} as ObjectWithDefinedValues<T>;
+	return Object.entries(obj).reduce<ObjectWithDefinedValues<T>>(
+		(valid, [key, value]) => {
+			if (isValidTargeting(value)) {
+				// @ts-expect-error -- isValidTargeting checks this
+				valid[key] = value;
+			}
+
+			return valid;
+		},
+		initialValue,
+	);
+};
