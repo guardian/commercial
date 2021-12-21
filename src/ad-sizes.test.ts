ff1de26d4cd97364e0d9f5b4811e0780ec8a593e
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-12-21 17:23:00 +0000

    Test improvements
    Co-Authored-By: Max Duval <hi@mxdvl.com>

--- src/ad-sizes.test.ts
+++ src/ad-sizes.test.ts
@@ -1,4 +1,6 @@
+import type { AdSizeTuple } from './ad-sizes';
 import { _, getTuple } from './ad-sizes';
+import type { SizeKeys } from '.';
 
 const { getAdSize } = _;
 
@@ -20,15 +22,17 @@ describe('ad sizes', () => {
 	);
 });
 
+const sizes: Array<[SizeKeys, AdSizeTuple]> = [
+	['mpu', [300, 250]],
+	['fluid', [0, 0]],
+	['googleCard', [300, 274]],
+	['outstreamGoogleDesktop', [550, 310]],
+	['video', [620, 1]],
+	['300x600', [300, 600]],
+];
+
 describe('getTuple', () => {
-	it.each([
-		['mpu' as const, [300, 250]],
-		['fluid' as const, [0, 0]],
-		['googleCard' as const, [300, 274]],
-		['outstreamGoogleDesktop' as const, [550, 310]],
-		['video' as const, [620, 1]],
-		['300x600' as const, [300, 600]],
-	])(
+	it.each(sizes)(
 		'getTuple(%s: SizeKey) outputs tuple [$d, %d]',
 		(input, expectedTuple) => {
 			const tuple = getTuple(input);
