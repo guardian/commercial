4842322f13d371187fef3fde30f0024104664f32
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-12-21 15:51:06 +0000

    Add tests

--- src/ad-sizes.test.ts
+++ src/ad-sizes.test.ts
@@ -1,4 +1,4 @@
-import { _ } from './ad-sizes';
+import { _, getTuple } from './ad-sizes';
 
 const { getAdSize } = _;
 
@@ -16,6 +16,23 @@ describe('ad sizes', () => {
 			expect(adSize.width).toEqual(expectedWidth);
 			expect(adSize.height).toEqual(expectedHeight);
 			expect(adSize.toString()).toEqual(expectedString);
+		},
+	);
+});
+
+describe('getTuple', () => {
+	it.each([
+		['mpu' as const, [300, 250]],
+		['fluid' as const, [0, 0]],
+		['googleCard' as const, [300, 274]],
+		['outstreamGoogleDesktop' as const, [550, 310]],
+		['video' as const, [620, 1]],
+		['300x600' as const, [300, 600]],
+	])(
+		'getTuple(%s: SizeKey) outputs tuple [$d, %d]',
+		(input, expectedTuple) => {
+			const tuple = getTuple(input);
+			expect(tuple).toEqual(expectedTuple);
 		},
 	);
 });
