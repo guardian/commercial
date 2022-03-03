299107f1d161c87ef395e8df224f7f97da4ad575
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-03-03 16:13:35 +0000

    track-scroll-depth.spec

---
+++ src/track-scroll-depth.spec.ts
@@ -0,0 +1,61 @@
+import { EventTimer } from './EventTimer';
+import { initTrackScrollDepth } from './track-scroll-depth';
+
+describe('initTrackScrollDepth', () => {
+	const observe = jest.fn();
+	class IntersectionObserver {
+		constructor() {
+			return Object.freeze({
+				observe: observe,
+				unobserve: () => {
+					return;
+				},
+			});
+		}
+	}
+
+	const originalOffsetHeight = Object.getOwnPropertyDescriptor(
+		HTMLElement.prototype,
+		'offsetHeight',
+	);
+	beforeAll(() => {
+		Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
+			configurable: true,
+			value: 1000,
+		});
+
+		Object.defineProperty(global, 'IntersectionObserver', {
+			value: IntersectionObserver,
+			writable: true,
+		});
+	});
+
+	afterAll(() => {
+		Object.defineProperty(
+			HTMLElement.prototype,
+			'offsetHeight',
+			originalOffsetHeight ?? 0,
+		);
+	});
+
+	test('Hidden elements are inserted and observed', () => {
+		// Given a 1000px page and a viewport height of 100px,
+		// 10 hidden elements are inserted
+		// NOTE we can't simulate scrolling down the page and assert that the callback
+		// to IntersectionObserver works as expected.
+		const eventTimer = EventTimer.get();
+		window.innerHeight = 100;
+
+		initTrackScrollDepth();
+
+		// height of viewport recorded
+		expect(eventTimer.properties['pageHeightVH']).toEqual(10);
+		const hiddenElements = document.querySelectorAll(
+			'.scroll-depth-marker',
+		);
+		// the hidden elements are inserted
+		expect(hiddenElements.length).toEqual(10);
+		// and observed
+		expect(observe).toHaveBeenCalledTimes(10);
+	});
+});
--- src/track-scroll-depth.ts
+++ src/track-scroll-depth.ts
@@ -7,34 +7,36 @@ import { EventTimer } from './EventTimer';
  * then use an intersection observer to mark the time when the viewport intersects with these elements.
  */
 const initTrackScrollDepth = () => {
-	const body = document.querySelector('body');
-	if (body === null) return;
-	const pageHeight = body.offsetHeight;
+	const pageHeight = document.body.offsetHeight;
 	const intViewportHeight = window.innerHeight;
 	// how many viewports tall is the page?
 	const pageHeightVH = Math.floor(pageHeight / intViewportHeight);
 	const eventTimer = EventTimer.get();
 	eventTimer.setProperty('pageHeightVH', pageHeightVH);
 
-	const observer = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				const currentDepthVH = String(
-					entry.target.getAttribute('data-depth'),
-				);
-				log('commercial', `current scroll depth ${currentDepthVH}`);
-				eventTimer.trigger(`scroll-depth-vh-${currentDepthVH}`);
-				observer.unobserve(entry.target);
-			}
-		});
-	});
+	const observer = new IntersectionObserver(
+		/* istanbul ignore next */
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					const currentDepthVH = String(
+						entry.target.getAttribute('data-depth'),
+					);
+					log('commercial', `current scroll depth ${currentDepthVH}`);
+					eventTimer.trigger(`scroll-depth-vh-${currentDepthVH}`);
+					observer.unobserve(entry.target);
+				}
+			});
+		},
+	);
 
 	for (let depth = 1; depth <= pageHeightVH; depth++) {
 		const div = document.createElement('div');
 		div.dataset.depth = String(depth);
 		div.style.top = String(100 * depth) + '%';
 		div.style.position = 'absolute';
-		body.appendChild(div);
+		div.className = 'scroll-depth-marker';
+		document.body.appendChild(div);
 		observer.observe(div);
 	}
 };
