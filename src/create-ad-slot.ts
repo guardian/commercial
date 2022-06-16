543354ee9594bf139f76d366e583fd9276999ae6
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2022-06-16 15:40:57 +0100

    use slotSizeMappings in create-ad-slot

--- src/create-ad-slot.ts
+++ src/create-ad-slot.ts
@@ -1,5 +1,5 @@
 import type { AdSize, SizeMapping } from './ad-sizes';
-import { adSizes } from './ad-sizes';
+import { slotSizeMappings } from './ad-sizes';
 
 const adSlotIdPrefix = 'dfp-ad--';
 
@@ -31,129 +31,59 @@ type CreateSlotOptions = {
 	sizes?: Record<string, AdSize[]>;
 };
 
-const commonSizeMappings: SizeMapping = {
-	mobile: [
-		adSizes.outOfPage,
-		adSizes.empty,
-		adSizes.outstreamMobile,
-		adSizes.mpu,
-		adSizes.googleCard,
-		adSizes.fluid,
-	],
-	phablet: [
-		adSizes.outOfPage,
-		adSizes.empty,
-		adSizes.outstreamMobile,
-		adSizes.mpu,
-		adSizes.googleCard,
-		adSizes.fluid,
-	],
-	desktop: [
-		adSizes.outOfPage,
-		adSizes.empty,
-		adSizes.mpu,
-		adSizes.googleCard,
-		adSizes.fluid,
-	],
-};
-
-/**
- * mark: 432b3a46-90c1-4573-90d3-2400b51af8d0
- * Some of these may or may not need to be synced for with the sizes in ./ad-sizes.ts
- * these were originally from frontend, ad-sizes.ts ones were in DCR.
- **/
-
 const adSlotConfigs: AdSlotConfigs = {
 	im: {
 		label: false,
 		refresh: false,
-		sizeMappings: {
-			mobile: [
-				adSizes.outOfPage,
-				adSizes.empty,
-				adSizes.inlineMerchandising,
-				adSizes.fluid,
-			],
-		},
+		sizeMappings: slotSizeMappings['im'],
 	},
 	'high-merch': {
 		label: false,
 		refresh: false,
 		name: 'merchandising-high',
-		sizeMappings: {
-			mobile: [
-				adSizes.outOfPage,
-				adSizes.empty,
-				adSizes.merchandisingHigh,
-				adSizes.fluid,
-			],
-		},
+		sizeMappings: slotSizeMappings['merchandising-high'],
 	},
 	'high-merch-lucky': {
 		label: false,
 		refresh: false,
 		name: 'merchandising-high-lucky',
-		sizeMappings: {
-			mobile: [adSizes.outOfPage, adSizes.empty, adSizes.fluid],
-		},
+		sizeMappings: slotSizeMappings['merchandising-high-lucky'],
 	},
 	'high-merch-paid': {
 		label: false,
 		refresh: false,
 		name: 'merchandising-high',
-		sizeMappings: {
-			mobile: [
-				adSizes.outOfPage,
-				adSizes.empty,
-				adSizes.merchandisingHighAdFeature,
-				adSizes.fluid,
-			],
-		},
+		sizeMappings: slotSizeMappings['merchandising-high'],
 	},
 	inline: {
-		sizeMappings: commonSizeMappings,
+		sizeMappings: slotSizeMappings['inline'],
 	},
 	mostpop: {
-		sizeMappings: commonSizeMappings,
+		sizeMappings: slotSizeMappings['mostpop'],
 	},
 	comments: {
-		sizeMappings: commonSizeMappings,
+		sizeMappings: slotSizeMappings['comments'],
 	},
 	'top-above-nav': {
-		sizeMappings: {
-			mobile: [
-				adSizes.outOfPage,
-				adSizes.empty,
-				adSizes.fabric,
-				adSizes.outstreamMobile,
-				adSizes.mpu,
-				adSizes.fluid,
-			],
-		},
+		sizeMappings: slotSizeMappings['top-above-nav'],
 	},
 	carrot: {
 		label: false,
 		refresh: false,
 		name: 'carrot',
-		sizeMappings: {
-			mobile: [adSizes.fluid],
-		},
+		sizeMappings: slotSizeMappings['merchandising-high'],
 	},
 	epic: {
 		label: false,
 		refresh: false,
 		name: 'epic',
-		sizeMappings: {
-			mobile: [adSizes.fluid],
-		},
+		sizeMappings: slotSizeMappings['epic'],
 	},
 	'mobile-sticky': {
 		label: true,
 		refresh: true,
 		name: 'mobile-sticky',
-		sizeMappings: {
-			mobile: [adSizes.mobilesticky],
-		},
+		sizeMappings: slotSizeMappings['mobile-sticky'],
 	},
 };
 
