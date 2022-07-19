b04ab99f8c85b80b20e68f33065be67209a6dc4d
Emma Imber <108270776+emma-imber@users.noreply.github.com>
2022-07-19 09:06:02 +0100

    feat: Add crossword banner slot to slot size definitions (#619)
    * Add crossword banner slot to slot size definitions
    
    * feat: Add crossword banner slot to slot size definitions
    
    * Fixed linting issues

--- src/ad-sizes.ts
+++ src/ad-sizes.ts
@@ -83,7 +83,8 @@ type SlotName =
 	| 'top-above-nav'
 	| 'carrot'
 	| 'epic'
-	| 'mobile-sticky';
+	| 'mobile-sticky'
+	| 'crossword-banner';
 
 type Breakpoint = 'mobile' | 'desktop' | 'phablet' | 'tablet';
 
@@ -301,6 +302,10 @@ const slotSizeMappings: SlotSizeMappings = {
 	},
 	'mobile-sticky': {
 		mobile: [adSizes.mobilesticky],
+	},
+	'crossword-banner': {
+		tablet: [adSizes.outOfPage, adSizes.empty, adSizes.leaderboard],
+		phablet: [adSizes.outOfPage, adSizes.empty, adSizes.leaderboard],
 	},
 };
 
