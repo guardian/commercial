127b93d10c060fdbb8d2788f9854849032117eaa
Simon Adcock <simonadcock2@gmail.com>
2022-09-16 22:17:10 +0100

    feat: add typings for connectiontype and navigator

--- src/global.ts
+++ src/global.ts
@@ -6,10 +6,28 @@ import type {
 import type { EventTimer } from '.';
 
 declare global {
+	/**
+	 * lib.dom.d.ts only includes features that are supported in at least two main browsers.
+	 * NetworkInformation is supported in Chrome only – it's behind a flag in Firefox.
+	 * If or when the NetworkInformation API is more widely supported, we can remove this shim.
+	 * https://github.com/mdn/browser-compat-data/blob/main/api/NetworkInformation.json
+	 */
+	type ConnectionType =
+		| 'bluetooth'
+		| 'cellular'
+		| 'ethernet'
+		| 'mixed'
+		| 'none'
+		| 'other'
+		| 'unknown'
+		| 'wifi';
 	interface NetworkInformation extends EventTarget {
 		readonly type: ConnectionType;
 		readonly downlink?: number;
 		readonly effectiveType?: string;
+	}
+	interface Navigator {
+		connection: NetworkInformation;
 	}
 	interface Window {
 		google_trackConversion?: (arg0: GoogleTrackConversionObject) => void;
