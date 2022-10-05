087d468f3a415b25d2fac50b335c695ae6098dbe
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-10-05 16:30:07 +0100

    buildImaAdTagUrl

--- src/ad-targeting-youtube.ts
+++ src/ad-targeting-youtube.ts
@@ -89,4 +89,71 @@ const buildAdsConfigWithConsent = (
 	return buildAdsConfig(consentState, adUnit, customParamsToMerge);
 };
 
-export { buildAdsConfigWithConsent, disabledAds };
+/**
+ * @param  {Record<string, MaybeArray<string|number|boolean>>
+ * do this https://support.google.com/admanager/answer/1080597
+ */
+const encodeVastTagKeyValues = (
+	query: Record<string, MaybeArray<string | number | boolean>>,
+): string => {
+	const unencodedUrl = Object.entries(query)
+		.map(([key, value]) => {
+			let queryValue: string;
+			if (Array.isArray(value)) {
+				queryValue = value.join(',');
+			} else if (typeof value == 'boolean' || typeof value == 'number') {
+				queryValue = value.toString();
+			} else {
+				queryValue = value;
+			}
+			return `${key}=${queryValue}`;
+		})
+		.join('&');
+	return unencodedUrl
+		.replace(/=/g, '%3D')
+		.replace(/&/g, '%26')
+		.replace(/,/g, '%2C');
+};
+
+const buildImaAdTagUrl = (
+	adUnit: string,
+	customParams: CustomParams,
+): string => {
+	const customParameters = {
+		...customParams,
+		...buildCustomParamsFromCookies(),
+	};
+	// const customParameters = { at: 'fixed-puppies' };
+	console.log('raw cust params', customParameters);
+
+	const queryParams = {
+		// iu: adUnit,
+		iu: '/59666047/theguardian.com',
+		description_url: '[placeholder]', // do we need this?
+		tfcd: '0',
+		npa: '0',
+		sz: '480x360|480x361|400x300',
+		gdfp_req: '1',
+		output: 'vast',
+		unviewed_position_start: '1',
+		env: 'vp',
+		impl: 's',
+		correlator: '', // do we need this?
+		vad_type: 'linear',
+		// vpos: 'preroll',
+		cust_params: encodeVastTagKeyValues(customParameters),
+	};
+
+	console.log(queryParams);
+
+	const queryParamsArray = [];
+	for (const [k, v] of Object.entries(queryParams)) {
+		queryParamsArray.push(`${k}=${v}`);
+	}
+	return (
+		'https://pubads.g.doubleclick.net/gampad/live/ads?' +
+		queryParamsArray.join('&')
+	);
+};
+
+export { buildAdsConfigWithConsent, buildImaAdTagUrl, disabledAds };
