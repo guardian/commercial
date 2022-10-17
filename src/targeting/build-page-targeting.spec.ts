8a956bfd1488215ce91f5df2ec454c21312fe2b1
Simon Byford <simon.byford@guardian.co.uk>
2022-10-17 16:03:11 +0100

    Port `buildPageTargeting` spec from frontend
    Co-authored-by: Ravi Lal <ravi.lal.freelancer@guardian.co.uk>

---
+++ src/targeting/build-page-targeting.spec.ts
@@ -0,0 +1,609 @@
+import { cmp as cmp_ } from '@guardian/consent-management-platform';
+import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
+import type { TCFv2ConsentState } from '@guardian/consent-management-platform/dist/types/tcfv2';
+import { setCookie, storage } from '@guardian/libs';
+import { getLocale as getLocale_ } from '../lib/get-locale';
+import type { Edition } from '../types';
+import { buildPageTargeting } from './build-page-targeting';
+
+const getLocale = getLocale_ as jest.MockedFunction<typeof getLocale_>;
+
+const cmp = {
+	hasInitialised: cmp_.hasInitialised as jest.MockedFunction<
+		typeof cmp_.hasInitialised
+	>,
+	willShowPrivacyMessageSync:
+		cmp_.willShowPrivacyMessageSync as jest.MockedFunction<
+			typeof cmp_.willShowPrivacyMessageSync
+		>,
+};
+
+jest.mock('../lib/get-locale', () => ({
+	getLocale: jest.fn(),
+}));
+
+jest.mock('@guardian/consent-management-platform', () => ({
+	cmp: {
+		hasInitialised: jest.fn(),
+		willShowPrivacyMessageSync: jest.fn(),
+	},
+}));
+
+const mockViewport = (width: number, height: number): void => {
+	Object.defineProperties(window, {
+		innerWidth: {
+			value: width,
+		},
+		innerHeight: {
+			value: height,
+		},
+	});
+};
+
+// CCPA
+const ccpaWithConsentMock: ConsentState = {
+	ccpa: { doNotSell: false },
+	canTarget: true,
+	framework: 'ccpa',
+};
+
+const ccpaWithoutConsentMock: ConsentState = {
+	ccpa: { doNotSell: true },
+	canTarget: false,
+	framework: 'ccpa',
+};
+
+// AUS
+const ausWithConsentMock: ConsentState = {
+	aus: { personalisedAdvertising: true },
+	canTarget: true,
+	framework: 'aus',
+};
+
+const ausWithoutConsentMock: ConsentState = {
+	aus: { personalisedAdvertising: false },
+	canTarget: false,
+	framework: 'aus',
+};
+
+// TCFv2
+const defaultState: TCFv2ConsentState = {
+	consents: { 1: false },
+	eventStatus: 'tcloaded',
+	vendorConsents: { abc: false },
+	addtlConsent: 'xyz',
+	gdprApplies: true,
+	tcString: 'YAAA',
+};
+
+const tcfv2WithConsentMock: ConsentState = {
+	tcfv2: {
+		...defaultState,
+		consents: { '1': true, '2': true },
+		eventStatus: 'useractioncomplete',
+	},
+	canTarget: true,
+	framework: 'tcfv2',
+};
+
+const tcfv2WithoutConsentMock: ConsentState = {
+	tcfv2: { ...defaultState, consents: {}, eventStatus: 'cmpuishown' },
+	canTarget: false,
+	framework: 'tcfv2',
+};
+
+const tcfv2NullConsentMock: ConsentState = {
+	tcfv2: undefined,
+	canTarget: false,
+	framework: 'tcfv2',
+};
+
+const tcfv2MixedConsentMock: ConsentState = {
+	tcfv2: {
+		...defaultState,
+		consents: { '1': false, '2': true },
+		eventStatus: 'useractioncomplete',
+	},
+	canTarget: false,
+	framework: 'tcfv2',
+};
+
+const emptyConsent: ConsentState = {
+	canTarget: false,
+	framework: null,
+};
+
+describe('Build Page Targeting', () => {
+	beforeEach(() => {
+		window.guardian = {
+			config: {
+				ophan: { pageViewId: 'presetOphanPageViewId' },
+				page: {
+					edition: 'US' as Edition,
+					pageId: 'football/series/footballweekly',
+					videoDuration: 63,
+					section: 'football',
+					sharedAdTargeting: {
+						bl: ['blog'],
+						br: 'p',
+						co: ['gabrielle-chan'],
+						ct: 'video',
+						edition: 'us',
+						k: [
+							'prince-charles-letters',
+							'uk/uk',
+							'prince-charles',
+						],
+						ob: 't',
+						p: 'ng',
+						se: ['filmweekly'],
+						su: ['5'],
+						tn: ['news'],
+						url: '/football/series/footballweekly',
+					},
+					isSensitive: false,
+				} as unknown as typeof window.guardian.config.page,
+			} as unknown as typeof window.guardian.config,
+		};
+
+		setCookie({ name: 'adtest', value: 'ng101' });
+
+		mockViewport(0, 0);
+
+		setCookie({ name: 'GU_U', value: 'test' });
+
+		storage.local.setRaw('gu.alreadyVisited', String(0));
+
+		getLocale.mockReturnValue('US');
+
+		jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+
+		expect.hasAssertions();
+	});
+
+	afterEach(() => {
+		jest.spyOn(global.Math, 'random').mockRestore();
+		jest.resetAllMocks();
+	});
+
+	it('should exist', () => {
+		expect(buildPageTargeting).toBeDefined();
+	});
+
+	it('should build correct page targeting', () => {
+		const pageTargeting = buildPageTargeting(emptyConsent, false, {});
+
+		expect(pageTargeting.sens).toBe('f');
+		expect(pageTargeting.edition).toBe('us');
+		expect(pageTargeting.ct).toBe('video');
+		expect(pageTargeting.p).toBe('ng');
+		expect(pageTargeting.su).toEqual(['5']);
+		expect(pageTargeting.bp).toBe('mobile');
+		expect(pageTargeting.at).toBe('ng101');
+		expect(pageTargeting.si).toEqual('t');
+		expect(pageTargeting.co).toEqual(['gabrielle-chan']);
+		expect(pageTargeting.bl).toEqual(['blog']);
+		expect(pageTargeting.tn).toEqual(['news']);
+		expect(pageTargeting.vl).toEqual('90');
+		expect(pageTargeting.pv).toEqual('presetOphanPageViewId');
+		expect(pageTargeting.pa).toEqual('f');
+		expect(pageTargeting.cc).toEqual('US');
+		expect(pageTargeting.rp).toEqual('dotcom-platform');
+	});
+
+	it('should set correct personalized ad (pa) param', () => {
+		expect(buildPageTargeting(tcfv2WithConsentMock, false, {}).pa).toBe(
+			't',
+		);
+		expect(buildPageTargeting(tcfv2WithoutConsentMock, false, {}).pa).toBe(
+			'f',
+		);
+		expect(buildPageTargeting(tcfv2NullConsentMock, false, {}).pa).toBe(
+			'f',
+		);
+		expect(buildPageTargeting(tcfv2MixedConsentMock, false, {}).pa).toBe(
+			'f',
+		);
+		expect(buildPageTargeting(ccpaWithConsentMock, false, {}).pa).toBe('t');
+		expect(buildPageTargeting(ccpaWithoutConsentMock, false, {}).pa).toBe(
+			'f',
+		);
+	});
+
+	it('Should correctly set the RDP flag (rdp) param', () => {
+		expect(buildPageTargeting(tcfv2WithoutConsentMock, false, {}).rdp).toBe(
+			'na',
+		);
+		expect(buildPageTargeting(tcfv2NullConsentMock, false, {}).rdp).toBe(
+			'na',
+		);
+		expect(buildPageTargeting(ccpaWithConsentMock, false, {}).rdp).toBe(
+			'f',
+		);
+		expect(buildPageTargeting(ccpaWithoutConsentMock, false, {}).rdp).toBe(
+			't',
+		);
+	});
+
+	it('Should correctly set the TCFv2 (consent_tcfv2, cmp_interaction) params', () => {
+		expect(
+			buildPageTargeting(tcfv2WithConsentMock, false, {}).consent_tcfv2,
+		).toBe('t');
+		expect(
+			buildPageTargeting(tcfv2WithConsentMock, false, {}).cmp_interaction,
+		).toBe('useractioncomplete');
+
+		expect(
+			buildPageTargeting(tcfv2WithoutConsentMock, false, {})
+				.consent_tcfv2,
+		).toBe('f');
+		expect(
+			buildPageTargeting(tcfv2WithoutConsentMock, false, {})
+				.cmp_interaction,
+		).toBe('cmpuishown');
+
+		expect(
+			buildPageTargeting(tcfv2MixedConsentMock, false, {}).consent_tcfv2,
+		).toBe('f');
+		expect(
+			buildPageTargeting(tcfv2MixedConsentMock, false, {})
+				.cmp_interaction,
+		).toBe('useractioncomplete');
+	});
+
+	it('should set correct edition param', () => {
+		expect(buildPageTargeting(emptyConsent, false, {}).edition).toBe('us');
+	});
+
+	it('should set correct se param', () => {
+		expect(buildPageTargeting(emptyConsent, false, {}).se).toEqual([
+			'filmweekly',
+		]);
+	});
+
+	it('should set correct k param', () => {
+		expect(buildPageTargeting(emptyConsent, false, {}).k).toEqual([
+			'prince-charles-letters',
+			'uk/uk',
+			'prince-charles',
+		]);
+	});
+
+	it('should set correct ab param', () => {
+		expect(
+			buildPageTargeting(emptyConsent, false, {
+				MtMaster: {
+					variant: 'variantName',
+				},
+			}).ab,
+		).toEqual(['MtMaster-variantName']);
+	});
+
+	it('should set Observer flag for Observer content', () => {
+		expect(buildPageTargeting(emptyConsent, false, {}).ob).toEqual('t');
+	});
+
+	it('should set correct branding param for paid content', () => {
+		expect(buildPageTargeting(emptyConsent, false, {}).br).toEqual('p');
+	});
+
+	it('should not contain an ad-free targeting value', () => {
+		expect(buildPageTargeting(emptyConsent, false, {}).af).toBeUndefined();
+	});
+
+	it('should remove empty values', () => {
+		window.guardian.config.page = {
+			// pageId should always be defined
+			pageId: 'football/series/footballweekly',
+		} as unknown as typeof window.guardian.config.page;
+		window.guardian.config.ophan = { pageViewId: '123456' };
+
+		expect(buildPageTargeting(emptyConsent, false, {})).toEqual({
+			at: 'ng101',
+			bp: 'mobile',
+			cc: 'US',
+			cmp_interaction: 'na',
+			consent_tcfv2: 'na',
+			dcre: 'f',
+			fr: '0',
+			inskin: 'f',
+			pa: 'f',
+			pv: '123456',
+			rdp: 'na',
+			rp: 'dotcom-platform',
+			sens: 'f',
+			si: 't',
+			skinsize: 's',
+			urlkw: ['footballweekly'],
+		});
+	});
+
+	describe('Breakpoint targeting', () => {
+		it('should set correct breakpoint targeting for a mobile device', () => {
+			mockViewport(320, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'mobile',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a medium mobile device', () => {
+			mockViewport(375, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'mobile',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a mobile device in landscape mode', () => {
+			mockViewport(480, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'mobile',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a phablet device', () => {
+			mockViewport(660, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'tablet',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a tablet device', () => {
+			mockViewport(740, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'tablet',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a desktop device', () => {
+			mockViewport(980, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'desktop',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a leftCol device', () => {
+			mockViewport(1140, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'desktop',
+			);
+		});
+
+		it('should set correct breakpoint targeting for a wide device', () => {
+			mockViewport(1300, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).bp).toEqual(
+				'desktop',
+			);
+		});
+	});
+
+	describe('Build Page Targeting (ad-free)', () => {
+		it('should set the ad-free param to t when enabled', () => {
+			expect(buildPageTargeting(emptyConsent, true, {}).af).toBe('t');
+		});
+	});
+
+	describe('Already visited frequency', () => {
+		it('can pass a value of five or less', () => {
+			storage.local.setRaw('gu.alreadyVisited', String(5));
+			expect(
+				buildPageTargeting(ccpaWithConsentMock, false, {}).fr,
+			).toEqual('5');
+		});
+
+		it('between five and thirty, includes it in a bucket in the form "x-y"', () => {
+			storage.local.setRaw('gu.alreadyVisited', String(18));
+			expect(
+				buildPageTargeting(ccpaWithConsentMock, false, {}).fr,
+			).toEqual('16-19');
+		});
+
+		it('over thirty, includes it in the bucket "30plus"', () => {
+			storage.local.setRaw('gu.alreadyVisited', String(300));
+			expect(
+				buildPageTargeting(ccpaWithConsentMock, false, {}).fr,
+			).toEqual('30plus');
+		});
+
+		it('passes a value of 0 if the value is not stored', () => {
+			storage.local.remove('gu.alreadyVisited');
+			expect(
+				buildPageTargeting(ccpaWithConsentMock, false, {}).fr,
+			).toEqual('0');
+		});
+
+		it('passes a value of 0 if the number is invalid', () => {
+			storage.local.setRaw('gu.alreadyVisited', 'not-a-number');
+			expect(
+				buildPageTargeting(ccpaWithConsentMock, false, {}).fr,
+			).toEqual('0');
+		});
+
+		it('passes a value of 0 if consent is not given', () => {
+			storage.local.setRaw('gu.alreadyVisited', String(5));
+			expect(
+				buildPageTargeting(ccpaWithoutConsentMock, false, {}).fr,
+			).toEqual('0');
+		});
+
+		it('passes a value of 0 if empty consent', () => {
+			storage.local.setRaw('gu.alreadyVisited', String(5));
+			expect(buildPageTargeting(emptyConsent, false, {}).fr).toEqual('0');
+		});
+	});
+
+	describe('Referrer', () => {
+		it('should set ref to Facebook', () => {
+			jest.spyOn(document, 'referrer', 'get').mockReturnValue(
+				'https://www.facebook.com/feel-the-force',
+			);
+			expect(buildPageTargeting(emptyConsent, false, {}).ref).toEqual(
+				'facebook',
+			);
+		});
+
+		it('should set ref to Twitter', () => {
+			jest.spyOn(document, 'referrer', 'get').mockReturnValue(
+				'https://t.co/you-must-unlearn-what-you-have-learned',
+			);
+			expect(buildPageTargeting(emptyConsent, false, {}).ref).toEqual(
+				'twitter',
+			);
+		});
+
+		it('should set ref to reddit', () => {
+			jest.spyOn(document, 'referrer', 'get').mockReturnValue(
+				'https://www.reddit.com/its-not-my-fault',
+			);
+			expect(buildPageTargeting(emptyConsent, false, {}).ref).toEqual(
+				'reddit',
+			);
+		});
+
+		it('should set ref to google', () => {
+			jest.spyOn(document, 'referrer', 'get').mockReturnValue(
+				'https://www.google.com/i-find-your-lack-of-faith-distrubing',
+			);
+			expect(buildPageTargeting(emptyConsent, false, {}).ref).toEqual(
+				'google',
+			);
+		});
+
+		it('should set ref empty string if referrer does not match', () => {
+			jest.spyOn(document, 'referrer', 'get').mockReturnValue(
+				'https://theguardian.com',
+			);
+			expect(buildPageTargeting(emptyConsent, false, {}).ref).toEqual(
+				undefined,
+			);
+		});
+	});
+
+	describe('URL Keywords', () => {
+		it('should return correct keywords from pageId', () => {
+			expect(buildPageTargeting(emptyConsent, false, {}).urlkw).toEqual([
+				'footballweekly',
+			]);
+		});
+
+		it('should extract multiple url keywords correctly', () => {
+			window.guardian.config.page.pageId =
+				'stage/2016/jul/26/harry-potter-cursed-child-review-palace-theatre-london';
+			expect(buildPageTargeting(emptyConsent, false, {}).urlkw).toEqual([
+				'harry',
+				'potter',
+				'cursed',
+				'child',
+				'review',
+				'palace',
+				'theatre',
+				'london',
+			]);
+		});
+
+		it('should get correct keywords when trailing slash is present', () => {
+			window.guardian.config.page.pageId =
+				'stage/2016/jul/26/harry-potter-cursed-child-review-palace-theatre-london/';
+			expect(buildPageTargeting(emptyConsent, false, {}).urlkw).toEqual([
+				'harry',
+				'potter',
+				'cursed',
+				'child',
+				'review',
+				'palace',
+				'theatre',
+				'london',
+			]);
+		});
+	});
+
+	describe('inskin targeting', () => {
+		it('should not allow inskin if cmp has not initialised', () => {
+			cmp.hasInitialised.mockReturnValue(false);
+			cmp.willShowPrivacyMessageSync.mockReturnValue(false);
+			mockViewport(1920, 1080);
+			expect(buildPageTargeting(emptyConsent, false, {}).inskin).toBe(
+				'f',
+			);
+		});
+
+		it('should not allow inskin if cmp will show a banner', () => {
+			cmp.hasInitialised.mockReturnValue(true);
+			cmp.willShowPrivacyMessageSync.mockReturnValue(true);
+			mockViewport(1920, 1080);
+			expect(buildPageTargeting(emptyConsent, false, {}).inskin).toBe(
+				'f',
+			);
+		});
+	});
+
+	describe('skinsize targetting', () => {
+		it.each([
+			['s', 1280],
+			['s', 1440],
+			['s', 1559],
+			['l', 1560],
+			['l', 1561],
+			['l', 1920],
+			['l', 2560],
+		])("should return '%s' if viewport width is %s", (expected, width) => {
+			cmp.hasInitialised.mockReturnValue(true);
+			cmp.willShowPrivacyMessageSync.mockReturnValue(false);
+			mockViewport(width, 800);
+			expect(buildPageTargeting(emptyConsent, false, {}).skinsize).toBe(
+				expected,
+			);
+		});
+
+		it("should return 's' if vp does not have a width", () => {
+			mockViewport(0, 0);
+			expect(buildPageTargeting(emptyConsent, false, {}).skinsize).toBe(
+				's',
+			);
+		});
+	});
+
+	describe('ad manager group value', () => {
+		const STORAGE_KEY = 'gu.adManagerGroup';
+		it('if present in localstorage, use value from storage', () => {
+			storage.local.setRaw(STORAGE_KEY, '10');
+			expect(
+				buildPageTargeting(tcfv2WithConsentMock, false, {}).amtgrp,
+			).toEqual('10');
+			storage.local.remove(STORAGE_KEY);
+		});
+
+		it.each([
+			[ccpaWithConsentMock, '9'],
+			[ccpaWithoutConsentMock, '9'],
+
+			[ausWithConsentMock, '9'],
+			[ausWithoutConsentMock, '9'],
+
+			[tcfv2WithConsentMock, '9'],
+			[tcfv2WithoutConsentMock, undefined],
+			[tcfv2MixedConsentMock, undefined],
+			[tcfv2MixedConsentMock, undefined],
+		])('Framework %p => amtgrp is %s', (consentState, value) => {
+			storage.local.setRaw(STORAGE_KEY, '9');
+			expect(buildPageTargeting(consentState, false, {}).amtgrp).toEqual(
+				value,
+			);
+			storage.local.remove(STORAGE_KEY);
+		});
+
+		it('if not present in localstorage, generate a random group 1-12, store in localstorage', () => {
+			// restore Math.random for this test so we can assert the group value range is 1-12
+			jest.spyOn(global.Math, 'random').mockRestore();
+			const valueGenerated = buildPageTargeting(
+				tcfv2WithConsentMock,
+				false,
+				{},
+			).amtgrp;
+			expect(valueGenerated).toBeDefined();
+			expect(Number(valueGenerated)).toBeGreaterThanOrEqual(1);
+			expect(Number(valueGenerated)).toBeLessThanOrEqual(12);
+			const valueFromStorage = storage.local.getRaw(STORAGE_KEY);
+			expect(valueFromStorage).toEqual(valueGenerated);
+		});
+	});
+});
