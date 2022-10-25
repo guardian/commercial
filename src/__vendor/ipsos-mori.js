36f1c749e51263b7f8d2b4f566936b39ca4d4e98
Chris Jones <christopher.jones@guardian.co.uk>
2022-10-25 16:18:35 +0100

    feat: Vendor scripts from Frontend (#698)
    * feat: Add vendor scripts from Frontend
    
    * Use TSDoc and disable eslint in vendor files
    
    * Remove eslint-disable directives
    
    We ignore the entire directory in .eslintrc.json
    
    * Remove unnecessary not
    
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>
    
    * Remove unnecessary semicolon
    
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>
    
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

---
+++ src/__vendor/ipsos-mori.js
@@ -0,0 +1,14 @@
+/**
+ * Load the Ipsos Mori Stub
+ *
+ * See {@Link https://github.com/guardian/dotcom-rendering/blob/150fc2d81e6a66d9c3336185e874fc8cd0288546/dotcom-rendering/docs/architecture/3rd%20party%20technical%20review/002-ipsos-mori.md documentation here }
+ */
+export const ipsosMoriStub = () => {
+	window.dm = window.dm || { AjaxData: [] };
+	window.dm.AjaxEvent = (et, d, ssid, ad) => {
+		dm.AjaxData.push({ et, d, ssid, ad });
+		if (window.DotMetricsObj) {
+			DotMetricsObj.onAjaxDataUpdate();
+		}
+	};
+};
