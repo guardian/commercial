5741de72562e4a9e0be14afd4dcaea2965c99834
Jake Lee Kennedy <jlkennedy127@gmail.com>
2022-10-27 10:13:40 +0100

    feat: ditch facebook pixel

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 
 export { ias } from './third-party-tags/ias';
 export { permutive } from './third-party-tags/permutive';
-export { fbPixel } from './third-party-tags/facebook-pixel';
 export { twitter } from './third-party-tags/twitter-uwt';
 export { inizio } from './third-party-tags/inizio';
 export { remarketing } from './third-party-tags/remarketing';
