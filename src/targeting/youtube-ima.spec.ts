4f398be2e467fece0c219631269cb2919bbbf89a
Ravi <7014230+arelra@users.noreply.github.com>
2022-11-08 13:41:06 +0000

    Update youtube-ima.spec to check cust_params encoding

--- src/targeting/youtube-ima.spec.ts
+++ src/targeting/youtube-ima.spec.ts
@@ -26,6 +26,7 @@ describe('Builds an IMA ad tag URL', () => {
 	it('default values and encoded custom parameters', () => {
 		(buildPageTargeting as jest.Mock).mockReturnValue({
 			at: 'adTestValue',
+			encodeMe: '=&,'
 		})
 		const adTagURL = buildImaAdTagUrl('someAdUnit', {
 			param1: 'hello1',
@@ -35,7 +36,7 @@ describe('Builds an IMA ad tag URL', () => {
 			param5: 5, // not a string so filtered out by filterValues
 		}, emptyConsent);
 		expect(adTagURL).toEqual(
-			'https://pubads.g.doubleclick.net/gampad/ads?iu=someAdUnit&description_url=[placeholder]&tfcd=0&npa=0&sz=480x360|480x361|400x300&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=&vad_type=linear&vpos=preroll&cust_params=param1%3Dhello1%26param2%3Dhello2%26param3%3Dhello3%2Chello4%26at%3DadTestValue',
+			'https://pubads.g.doubleclick.net/gampad/ads?iu=someAdUnit&description_url=[placeholder]&tfcd=0&npa=0&sz=480x360|480x361|400x300&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=&vad_type=linear&vpos=preroll&cust_params=param1%3Dhello1%26param2%3Dhello2%26param3%3Dhello3%252Chello4%26at%3DadTestValue%26encodeMe%3D%253D%2526%252C',
 		);
 	});
 });
