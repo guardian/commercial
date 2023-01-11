5ce3c9fe1b103b9ad1a49c25d85ac986fbcb5ae9
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-01-11 17:08:46 +0000

    add migration script

---
+++ scripts/frontend-migration.mjs
@@ -0,0 +1,51 @@
+
+import madge from 'madge';
+import { copyFileSync, mkdirSync } from 'fs';
+import {resolve, parse, dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * Usage
+ *
+ * node scripts/frontend-migration.js <path-to-frontend-root>
+ */
+
+const args = process.argv.slice(2);
+
+const frontendDirectory = args[0];
+
+const entry = resolve(frontendDirectory, 'static/src/javascripts/bootstraps/standalone.commercial.ts');
+
+const baseDir = resolve(frontendDirectory, 'static/src/javascripts');
+
+const targetDir = resolve(__dirname, '../standalone/src');
+
+const replacePath = (path) => path.replace(
+		baseDir,
+		targetDir
+	)
+
+
+const config = {
+	webpackConfig: resolve(frontendDirectory, 'webpack.config.commercial.prod.js'),
+	tsConfig: resolve(frontendDirectory, 'tsconfig.json'),
+	baseDir,
+	includeNpm: true,
+};
+
+const data = await madge(entry, config).then((res) => res.obj());
+
+Object.keys(data).forEach((file) => {
+	const fileInfo = parse(file);
+
+	console.log(fileInfo.dir);
+
+	mkdirSync(
+		resolve(targetDir, fileInfo.dir),
+		{ recursive: true }
+	);
+	copyFileSync(resolve(baseDir, file), resolve(targetDir, file));
+	// console.log(`Copied ${resolve(baseDir, file)} to ${resolve(targetDir, file)}`);
+});
