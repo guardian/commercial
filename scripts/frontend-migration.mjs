c418f2bd6d0c24efeaaacecf6da71fbe80341515
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-01-24 16:11:51 +0000

    chore: Include additional files in migration script like tests and get them working (#758)

--- scripts/frontend-migration.mjs
+++ scripts/frontend-migration.mjs
@@ -1,19 +1,20 @@
 import madge from "madge";
-import { copyFileSync, mkdirSync } from "fs";
+import { copyFileSync, existsSync, mkdirSync } from "fs";
 import { resolve, parse, dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /**
+ * Madge misbehaves when the cwd is not the root of the project, missing files and such, so use from the frontend directory
+ *
  * Usage
  *
- * node scripts/frontend-migration.js <path-to-frontend-root>
+ * node ../commercial-core/scripts/frontend-migration.js
+ *
  */
 
-const args = process.argv.slice(2);
-
-const frontendDirectory = args[0];
+const frontendDirectory = process.cwd();
 
 const entry = resolve(
 	frontendDirectory,
@@ -25,23 +26,50 @@ const baseDir = resolve(frontendDirectory, "static/src/javascripts");
 const targetDir = resolve(__dirname, "../standalone/src");
 
 const config = {
-	webpackConfig: resolve(
-		frontendDirectory,
-		"webpack.config.commercial.prod.js"
-	),
 	tsConfig: resolve(frontendDirectory, "tsconfig.json"),
 	baseDir,
 	includeNpm: true,
 };
 
-const data = await madge(entry, config).then((res) => res.obj());
+const specificFiles = [
+	"types/global.d.ts",
+	"types/ias.d.ts",
+	"types/ophan.d.ts",
+	"types/utils.d.ts",
+	"projects/commercial/modules/header-bidding/types.d.ts",
+	"lib/cookies.js",
+	"lib/config.js",
+	"lib/config.d.ts",
+	"projects/common/modules/analytics/beacon.js",
+	"projects/common/modules/onward/geo-most-popular.js",
+	"lib/__mocks__/raven.ts",
+	"lib/__mocks__/fastdom-promise.ts",
+	"lib/__mocks__/fastdom.ts",
+	"lib/__mocks__/config.js",
+	"lib/__mocks__/fetch-json.js",
+	"projects/common/modules/experiments/__mocks__/ab-tests.ts",
+	"projects/common/modules/experiments/__fixtures__/ab-test.ts",
+];
 
-Object.keys(data).forEach((file) => {
+const graphFiles = await madge(entry, config).then((res) => res.obj());
+
+const allFiles = Object.keys(graphFiles).concat(specificFiles);
+
+
+allFiles.forEach((file) => {
 	const fileInfo = parse(file);
 
 	mkdirSync(resolve(targetDir, fileInfo.dir), { recursive: true });
 	console.log(`Created path "${fileInfo.dir} or it already exists`);
 
 	copyFileSync(resolve(baseDir, file), resolve(targetDir, file));
-	console.log(`Copied ${resolve(baseDir, file)} to ${resolve(targetDir, file)}`);
+	console.log(`Copied ${file} to ${resolve(targetDir, file)}`);
+
+	const test = resolve(baseDir, fileInfo.dir, `${fileInfo.name}.spec.ts`);
+
+	if(existsSync(test)) {
+		copyFileSync(test, resolve(targetDir, fileInfo.dir, `${fileInfo.name}.spec.ts`));
+		console.log(`Copied test ${test} to ${resolve(targetDir, fileInfo.dir, `${fileInfo.name}.spec.ts`)}`);
+	}
 });
+
---
+++ standalone/jest.setupTestFrameworkScriptFile.js
@@ -0,0 +1,44 @@
+// Polyfill test environment (done by polyfill.io in production)
+require('core-js');
+
+jest.mock('./src/projects/commercial/am-i-used', () => ({
+    amIUsed: jest.fn()
+}));
+
+
+// Stub global Guardian config
+// eslint-disable-next-line id-denylist -- this is on purpose
+window.guardian = {
+	config: {
+		switches: {},
+		page: {
+            idApiUrl: 'https://idapi.theguardian.com',
+            idUrl: 'https://profile.theguardian.com',
+        },
+		images: {
+			commercial: {},
+		},
+		libs: {},
+        ophan: {
+            browserId: 'dummy_bwid_24680'
+        }
+	},
+    ophan: {
+        pageViewId: 'dummy_pvid_123456790',
+    },
+	css: {},
+	adBlockers: {
+		active: undefined,
+		onDetect: [],
+	},
+};
+
+// Stub matchmedia
+window.matchMedia =
+	window.matchMedia ||
+	jest.fn().mockImplementation(() => ({
+            matches: false,
+            addListener: jest.fn(),
+            addEventListener: jest.fn(),
+        }));
+
