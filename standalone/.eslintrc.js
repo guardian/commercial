1c103d0fd8e26f805e56288675ee7f8db520e4aa
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-01-25 16:42:23 +0000

    add commands for formatting and linting

--- standalone/.eslintrc.js
+++ standalone/.eslintrc.js
@@ -1,41 +1,24 @@
 module.exports = {
-	plugins: ['guardian-frontend', 'prettier'],
-			extends: '@guardian/eslint-config-typescript',
-			parserOptions: {
-				project: './tsconfig.json',
-			},
-			rules: {
-				'import/no-unresolved': 0,
-				'no-restricted-imports': [
-					'error',
-					{
-						name: 'bonzo',
-						message: 'Use `lib/$$` instead.',
-					},
-					{
-						name: 'qwery',
-						message: 'Use `lib/$$` instead.',
-					},
-					{
-						name: 'bean',
-						message: 'Use `lib/$$` instead.',
-					},
-				],
-			},
+	root: true,
+	plugins: ['@typescript-eslint', 'import'],
+	extends: [
+		'@guardian/eslint-config-typescript',
+		'plugin:import/recommended',
+	],
 	rules: {
 		// disallow naming variables 'guardian', because
 		// window.guardian is our global config/settings object
 		'id-denylist': ['error', 'guardian'],
 	},
 	overrides: [
-        {
-            files: ['*.spec.ts'],
-            rules: {
-              // This rule erroneously flags up instances where you expect(obj.fn).toHaveBeenCalled
-              // Enabled for test files only
-              '@typescript-eslint/unbound-method': 'off',
-            },
-          },
+		{
+			files: ['*.spec.ts'],
+			rules: {
+				// This rule erroneously flags up instances where you expect(obj.fn).toHaveBeenCalled
+				// Enabled for test files only
+				'@typescript-eslint/unbound-method': 'off',
+			},
+		},
 	],
-	ignorePatterns: ['*.js']
+	ignorePatterns: ['*.js'],
 };
--- standalone/babel.config.js
+++ standalone/babel.config.js
@@ -1,42 +1,44 @@
 module.exports = {
-    "presets": [
-        "@babel/preset-react"
-    ],
-    "plugins": [
-        "@babel/plugin-proposal-object-rest-spread",
-        "@babel/plugin-syntax-dynamic-import"
-    ],
-    "env": {
-        "test": {
-            "presets": [
-                ["@babel/preset-env", {
-                    "targets": {
-                        "node": "current"
-                    }
-                }]
-            ],
-            "plugins": [
-                "@babel/plugin-transform-runtime",
-                "@babel/plugin-proposal-class-properties",
-                "dynamic-import-node",
-            ],
-        },
-        "internal": {
-            "presets": [
-                ["@babel/preset-env", {
-                    "targets": {
-                        "browsers": [
-                            "last 2 Chrome versions",
-                            "last 1 Safari version",
-                            "last 2 Firefox versions",
-                            "last 2 Edge versions",
-                        ],
-                    }
-                }],
-            ],
-        },
-    },
-    "ignore": [
-        "eslintrc.js",
-    ],
+	presets: ['@babel/preset-react'],
+	plugins: [
+		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-syntax-dynamic-import',
+	],
+	env: {
+		test: {
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: {
+							node: 'current',
+						},
+					},
+				],
+			],
+			plugins: [
+				'@babel/plugin-transform-runtime',
+				'@babel/plugin-proposal-class-properties',
+				'dynamic-import-node',
+			],
+		},
+		internal: {
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: {
+							browsers: [
+								'last 2 Chrome versions',
+								'last 1 Safari version',
+								'last 2 Firefox versions',
+								'last 2 Edge versions',
+							],
+						},
+					},
+				],
+			],
+		},
+	},
+	ignore: ['eslintrc.js'],
 };
