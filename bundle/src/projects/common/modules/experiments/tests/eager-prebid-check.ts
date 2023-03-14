086f1530c7d8f4f59488fb4fa4fd2f5abdab2b55
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-03-14 17:52:05 +0000

    add switching of lazy load margin based on variant

--- bundle/src/projects/common/modules/experiments/tests/eager-prebid-check.ts
+++ bundle/src/projects/common/modules/experiments/tests/eager-prebid-check.ts
@@ -3,14 +3,19 @@ import { isInUk } from 'common/modules/commercial/geo-utils';
 import { getSynchronousTestsToRun } from '../ab';
 import { eagerPrebid } from './eager-prebid';
 
-// determine if the user is in any of the the eager prebid variants
-export const isInEagerPrebidVariant = memoize((): boolean => {
+export const getEagerPrebidVariant = memoize((): string => {
 	if (!isInUk()) {
-		return false;
+		return 'notInTest';
 	}
 	const tests = getSynchronousTestsToRun();
 	const test = tests.find(
 		(test) => test.id === eagerPrebid.id,
 	);
-	return test ? test.variantToRun.id !== 'control' : false;
+	return test ? test.variantToRun.id : 'control';
+});
+
+// determine if the user is in any of the the eager prebid variants
+export const isInEagerPrebidVariant = memoize((): boolean => {
+	const variant = getEagerPrebidVariant();
+	return variant !== 'control' && variant !== 'notInTest';
 });
