183db0466cd19f44f49cbc26ddf0230c157d1918
Ravi <7014230+arelra@users.noreply.github.com>
2023-03-22 15:10:52 +0000

    test: Percy updates (#827)
    * Add allowed hostnames to snapshot options
    
    * Set HEAD commit hash for PRs and regular branches
    
    * Add back pull_request paths
    
    * Add gha comment
    
    * Correct workflow paths
    
    * Correct workflow paths again
    
    * Set the commit hash after project checkout
    
    * Conditionally set commit sha
    
    * Add comments
    
    * Correct not syntax
    
    * Remove commit hash log
    
    * Create and upload cypress screenshots
    
    * Add Cypress screenshots to all visual tests
    
    * Add i.guim.co.uk as an allowedhostname
    
    * Set PERCY_BRANCH
    
    * Revert "Set PERCY_BRANCH"
    
    This reverts commit e1f2384f05b9c2bde7a7e4d919c3906a0bb8fd31.
    
    * Remove working directories

--- e2e/cypress/snapshot/snapshot-article-long-read.cy.ts
+++ e2e/cypress/snapshot/snapshot-article-long-read.cy.ts
@@ -1,19 +1,19 @@
-import { storage } from '@guardian/libs';
-import { getStage, getTestUrl } from '../lib/util';
-import { stubApiRequests } from '../lib/stubApiRequests';
-import '@percy/cypress';
+import { storage } from "@guardian/libs";
+import { getStage, getTestUrl } from "../lib/util";
+import { stubApiRequests } from "../lib/stubApiRequests";
+import "@percy/cypress";
 
-describe('Visually snapshot long read article', () => {
+describe("Visually snapshot long read article", () => {
 	it(`snapshots long read article`, () => {
 		const path = getTestUrl(
 			getStage(),
-			'/business/2022/apr/14/a-day-in-the-life-of-almost-every-vending-machine-in-the-world',
-			{ isDcr: true },
+			"/business/2022/apr/14/a-day-in-the-life-of-almost-every-vending-machine-in-the-world",
+			{ isDcr: true }
 		);
 		// stub all api requests
 		stubApiRequests();
 		// force geolocation to UK
-		storage.local.set('gu.geo.override', 'GB');
+		storage.local.set("gu.geo.override", "GB");
 		// load article
 		cy.visit(path);
 		cy.allowAllConsent();
@@ -23,9 +23,19 @@ describe('Visually snapshot long read article', () => {
 		cy.checkAdsRendered();
 		// workaround for onwards sections
 		// the intercept intermittently fails to stub the request
-		cy.get('[name=OnwardsUpper]').invoke('remove');
-		// snapshot
-		cy.percySnapshot('article-long-read', {
+		cy.get("[name=OnwardsUpper]").invoke("remove");
+		// cypress screenshot
+		// helpful to compare to Percy
+		cy.screenshot("article-long-read");
+		// percy snapshot
+		cy.percySnapshot("article-long-read", {
+			discovery: {
+				allowedHostnames: [
+					"localhost",
+					"i.guim.co.uk",
+					"tpc.googlesyndication.com",
+				],
+			},
 			widths: [320, 740, 1300],
 		});
 	});
