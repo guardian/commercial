817cb21e1c8c2875f2495b799ae21714523ebc75
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-03-23 10:13:16 +0000

    0 percent to test on prod

--- bundle/src/projects/common/modules/experiments/tests/eager-prebid.ts
+++ bundle/src/projects/common/modules/experiments/tests/eager-prebid.ts
@@ -4,9 +4,9 @@ import { bypassMetricsSampling } from '../utils';
 export const eagerPrebid: ABTest = {
 	id: 'EagerPrebid',
 	author: '@commercial-dev',
-	start: '2023-03-13',
-	expiry: '2023-03-30',
-	audience: 6 / 100,
+	start: '2023-03-23',
+	expiry: '2023-04-7',
+	audience: 0 / 100,
 	audienceOffset: 0 / 100,
 	audienceCriteria: 'All pageviews',
 	successMeasure:
