7308bd0cd3872494ca0f7ff01848c7444f5bd78c
Chris Jones <chrislomaxjones96@gmail.com>
2023-05-25 10:24:22 +0100

    Randomly limit inline merch in test

---
+++ bundle/src/projects/common/modules/commercial/inline-merch.spec.ts
@@ -0,0 +1,44 @@
+import { isInVariantSynchronous } from '../experiments/ab';
+import { shouldAddInlineMerchAd } from './inline-merch';
+
+jest.mock('common/modules/experiments/ab', () => ({
+	isInVariantSynchronous: jest.fn(),
+}));
+
+afterEach(() => {
+	jest.clearAllMocks();
+});
+
+describe('shouldAddInlineMerchAd', () => {
+	it('returns false when in the variant and page is not eligible', () => {
+		(isInVariantSynchronous as jest.Mock).mockReturnValue(true);
+		window.guardian.config.page.hasInlineMerchandise = false;
+		expect(shouldAddInlineMerchAd()).toBe(false);
+	});
+
+	it('returns false when not in the variant and page is not eligible', () => {
+		(isInVariantSynchronous as jest.Mock).mockReturnValue(false);
+		window.guardian.config.page.hasInlineMerchandise = false;
+		expect(shouldAddInlineMerchAd()).toBe(false);
+	});
+
+	it('returns true when not in the variant and page is eligible', () => {
+		(isInVariantSynchronous as jest.Mock).mockReturnValue(false);
+		window.guardian.config.page.hasInlineMerchandise = true;
+		expect(shouldAddInlineMerchAd()).toBe(true);
+	});
+
+	it('returns true when in the variant, the page is eligible, and we randomly choose not to limit the slot', () => {
+		(isInVariantSynchronous as jest.Mock).mockReturnValue(true);
+		window.guardian.config.page.hasInlineMerchandise = true;
+		jest.spyOn(global.Math, 'random').mockReturnValue(0.51);
+		expect(shouldAddInlineMerchAd()).toBe(true);
+	});
+
+	it('returns false when in the variant, the page is eligible, but we randomly choose to limit the slot', () => {
+		(isInVariantSynchronous as jest.Mock).mockReturnValue(true);
+		window.guardian.config.page.hasInlineMerchandise = true;
+		jest.spyOn(global.Math, 'random').mockReturnValue(0.49);
+		expect(shouldAddInlineMerchAd()).toBe(false);
+	});
+});
