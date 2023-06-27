40b830e06a1a9d4b0569a8b5850db2fc8408c48a
Emma Imber <emma-jo.imber@guardian.co.uk>
2023-06-27 16:27:51 +0100

    Remove TODO comment now that it is done

--- src/commercial.consentless.ts
+++ src/commercial.consentless.ts
@@ -8,13 +8,6 @@ import { init as setAdTestCookie } from './lib/set-adtest-cookie';
 import { init as setAdTestInLabelsCookie } from './lib/set-adtest-in-labels-cookie';
 
 const bootConsentless = async (consentState: ConsentState): Promise<void> => {
-	/*  In the consented ad stack, we set the ad free cookie for users who
-		don't consent to targeted ads in order to hide empty ads slots.
-		We remove the cookie here so that we can show Opt Out ads.
-		TODO: Stop setting ad free cookie for users who opt out when
-		consentless ads are rolled out to all users.
- 	*/
-
 	await Promise.all([
 		setAdTestCookie(),
 		setAdTestInLabelsCookie(),
