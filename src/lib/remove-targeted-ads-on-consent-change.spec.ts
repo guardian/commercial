d40119ffa1309d792a2794831536ad829d9409e7
Emma Imber <emma-jo.imber@guardian.co.uk>
2023-06-27 17:09:13 +0100

    Update module address

--- src/lib/remove-targeted-ads-on-consent-change.spec.ts
+++ src/lib/remove-targeted-ads-on-consent-change.spec.ts
@@ -10,7 +10,7 @@ import { _ } from './remove-targeted-ads-on-consent-change';
 const { _removeTargetedAdsOnConsentChange: removeTargetedAdsOnConsentChange } =
 	_;
 
-jest.mock('common/modules/experiments/ab', () => ({
+jest.mock('./experiments/ab', () => ({
 	isInVariantSynchronous: jest.fn(() => false),
 }));
 
