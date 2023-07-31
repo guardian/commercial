9744b2f23b3da1f941e57ec4997a370934475d69
Ravi <7014230+arelra@users.noreply.github.com>
2023-07-31 18:27:48 +0100

    Log when removing ad slots

--- src/lib/remove-slots.ts
+++ src/lib/remove-slots.ts
@@ -1,3 +1,4 @@
+import { log } from '@guardian/libs';
 import { once } from 'lodash-es';
 import { isInVariantSynchronous } from 'lib/experiments/ab';
 import { elementsManager } from 'lib/experiments/tests/elements-manager';
@@ -5,7 +6,6 @@ import { dfpEnv } from './dfp/dfp-env';
 import fastdom from './fastdom-promise';
 
 // Remove ad slots
-// Remove toggled ad labels that sit outside of the ad slot
 const selectors: string[] = [dfpEnv.adSlotSelector, '.top-banner-ad-container'];
 
 const selectNodes = () =>
@@ -23,7 +23,12 @@ const isDisabled = (node: Element) =>
 const filterDisabledNodes = (nodes: Element[]) => nodes.filter(isDisabled);
 
 const removeNodes = (nodes: Element[]): Promise<void> =>
-	fastdom.mutate(() => nodes.forEach((node) => node.remove()));
+	fastdom.mutate(() =>
+		nodes.forEach((node) => {
+			log('commercial', `Removing ad slot ${node.id}`);
+			node.remove();
+		}),
+	);
 
 const removeSlots = (): Promise<void> => {
 	// Don't collapse slots when in the Elements Manager AB test variant.
