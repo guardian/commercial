4560642e25d7f277dffa2b987bbbb418f544493d
Ravi <7014230+arelra@users.noreply.github.com>
2023-08-14 12:29:27 +0100

    Use ConsentState.canTarget for aus and ccpa

--- src/core/targeting/youtube.ts
+++ src/core/targeting/youtube.ts
@@ -39,20 +39,11 @@ const buildAdsConfig = (
 		},
 	};
 
-	if (cmpConsent.ccpa) {
-		const canTarget = !cmpConsent.ccpa.doNotSell;
+	if (cmpConsent.framework === 'ccpa' || cmpConsent.framework === 'aus') {
 		return {
 			...defaultAdsConfig,
-			restrictedDataProcessor: !canTarget,
-		} as AdsConfigCCPAorAus;
-	}
-
-	if (cmpConsent.aus) {
-		const canTarget = cmpConsent.aus.personalisedAdvertising;
-		return {
-			...defaultAdsConfig,
-			restrictedDataProcessor: !canTarget,
-		} as AdsConfigCCPAorAus;
+			restrictedDataProcessor: !cmpConsent.canTarget,
+		} satisfies AdsConfigCCPAorAus;
 	}
 
 	if (cmpConsent.tcfv2) {
@@ -67,7 +58,7 @@ const buildAdsConfig = (
 		return {
 			adTagParameters: mergedAdTagParameters,
 			nonPersonalizedAd: !canTarget,
-		} as AdsConfigTCFV2;
+		} satisfies AdsConfigTCFV2;
 	}
 
 	// Shouldn't happen but handle if no matching framework
