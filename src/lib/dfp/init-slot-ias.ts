e0647d2ff5c24a0cea1498ef7c8a2517c51846eb
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-08-18 18:36:19 +0100

    tidy up timeout functionality

--- src/lib/dfp/init-slot-ias.ts
+++ src/lib/dfp/init-slot-ias.ts
@@ -11,8 +11,26 @@ const adUnit = once((): string => {
 		: window.guardian.config.page.adUnit;
 });
 
-const initSlotIas = (id: string, slot: googletag.Slot) => {
-	/*
+const timeout = async <T>(
+	promise: Promise<T>,
+	ms: number,
+): Promise<T | void> => {
+	let timeoutId: number;
+	return Promise.race([
+		promise,
+		new Promise<void>((resolve) => {
+			timeoutId = window.setTimeout(resolve, ms);
+		}),
+	]).then((result) => {
+		window.clearTimeout(timeoutId);
+		return result;
+	});
+};
+
+const initSlotIas = (id: string, slot: googletag.Slot) =>
+	timeout(
+		new Promise<void>((resolve) => {
+			/*
         For each ad slot defined, we request information from IAS, based
         on slot name, ad unit and sizes. We then add this targeting to the
         slot prior to requesting it from DFP.
@@ -24,102 +42,87 @@ const initSlotIas = (id: string, slot: googletag.Slot) => {
 
 		this should all have been instantiated by lib/third-party-tags/ias.js
 	*/
-	window.__iasPET = window.__iasPET ?? {};
-	const iasPET = window.__iasPET;
+			window.__iasPET = window.__iasPET ?? {};
+			const iasPET = window.__iasPET;
 
-	iasPET.queue = iasPET.queue ?? [];
-	iasPET.pubId = '10249';
+			iasPET.queue = iasPET.queue ?? [];
+			iasPET.pubId = '10249';
 
-	// need to reorganize the type due to https://github.com/microsoft/TypeScript/issues/33591
-	const slotSizes: Array<googletag.Size | 'fluid'> = slot.getSizes();
+			// need to reorganize the type due to https://github.com/microsoft/TypeScript/issues/33591
+			const slotSizes: Array<googletag.Size | 'fluid'> = slot.getSizes();
 
-	// IAS Optimization Targeting
-	const iasPETSlots: IasPETSlot[] = [
-		{
-			adSlotId: id,
-			size: slotSizes
-				.filter(
-					(size: 'fluid' | googletag.Size): size is googletag.Size =>
-						size !== 'fluid',
-				)
-				.map((size) => [size.getWidth(), size.getHeight()]),
-			adUnitPath: adUnit(), // why do we have this method and not just slot.getAdUnitPath()?
-		},
-	];
+			// IAS Optimization Targeting
+			const iasPETSlots: IasPETSlot[] = [
+				{
+					adSlotId: id,
+					size: slotSizes
+						.filter(
+							(
+								size: 'fluid' | googletag.Size,
+							): size is googletag.Size => size !== 'fluid',
+						)
+						.map((size) => [size.getWidth(), size.getHeight()]),
+					adUnitPath: adUnit(), // why do we have this method and not just slot.getAdUnitPath()?
+				},
+			];
 
-	// this is stored so that the timeout can be cancelled in the event of IAS not timing out
-	let timeoutId: NodeJS.Timeout;
-
-	// this is resolved by either the iasTimeout or iasDataCallback, defined below
-	let loadedResolve: () => void;
-	const iasDataPromise = new Promise<void>((resolve) => {
-		loadedResolve = resolve;
-	});
-
-	const iasDataCallback = (targetingJSON: string) => {
-		clearTimeout(timeoutId);
-
-		/*  There is a name-clash with the `fr` targeting returned by IAS
+			const iasDataCallback = (targetingJSON: string) => {
+				/*  There is a name-clash with the `fr` targeting returned by IAS
                 and the `fr` paramater we already use for frequency. Therefore
                 we apply the targeting to the slot ourselves and rename the IAS
                 fr parameter to `fra` (given that, here, it relates to fraud).
             */
-		const targeting = JSON.parse(targetingJSON) as IasTargeting;
+				const targeting = JSON.parse(targetingJSON) as IasTargeting;
 
-		// brand safety is on a page level
-		Object.keys(targeting.brandSafety).forEach((key) => {
-			const brandSafetyValue = targeting.brandSafety[key];
-			if (brandSafetyValue) {
-				window.googletag.pubads().setTargeting(key, brandSafetyValue);
-			}
-		});
-		if (targeting.fr) {
-			window.googletag.pubads().setTargeting('fra', targeting.fr);
-		}
-		if (targeting.custom?.['ias-kw']) {
-			window.googletag
-				.pubads()
-				.setTargeting('ias-kw', targeting.custom['ias-kw']);
-		}
-
-		// viewability targeting is on a slot level
-		const ignoredKeys = ['pub'];
-		const slotTargeting = targeting.slots[id];
-
-		/* eslint-disable @typescript-eslint/no-unnecessary-condition -- TODO temporary until no unchecked index access can be turned on */
-		if (slotTargeting) {
-			Object.keys(slotTargeting)
-				.filter((x) => !ignoredKeys.includes(x))
-				.forEach((key) => {
-					const targetingSlot = targeting.slots[id];
-
-					if (targetingSlot) {
-						const targetingValue = targetingSlot[key];
-
-						if (slot && targetingValue) {
-							slot.setTargeting(key, targetingValue);
-						}
+				// brand safety is on a page level
+				Object.keys(targeting.brandSafety).forEach((key) => {
+					const brandSafetyValue = targeting.brandSafety[key];
+					if (brandSafetyValue) {
+						window.googletag
+							.pubads()
+							.setTargeting(key, brandSafetyValue);
 					}
 				});
-		}
-		/* eslint-enable @typescript-eslint/no-unnecessary-condition */
+				if (targeting.fr) {
+					window.googletag.pubads().setTargeting('fra', targeting.fr);
+				}
+				if (targeting.custom?.['ias-kw']) {
+					window.googletag
+						.pubads()
+						.setTargeting('ias-kw', targeting.custom['ias-kw']);
+				}
 
-		loadedResolve();
-	};
+				// viewability targeting is on a slot level
+				const ignoredKeys = ['pub'];
+				const slotTargeting = targeting.slots[id];
 
-	iasPET.queue.push({
-		adSlots: iasPETSlots,
-		dataHandler: iasDataCallback,
-	});
+				/* eslint-disable @typescript-eslint/no-unnecessary-condition -- TODO temporary until no unchecked index access can be turned on */
+				if (slotTargeting) {
+					Object.keys(slotTargeting)
+						.filter((x) => !ignoredKeys.includes(x))
+						.forEach((key) => {
+							const targetingSlot = targeting.slots[id];
 
-	const iasTimeoutDuration = 1000;
+							if (targetingSlot) {
+								const targetingValue = targetingSlot[key];
 
-	const iasTimeout = () =>
-		new Promise<void>((resolve) => {
-			timeoutId = setTimeout(resolve, iasTimeoutDuration);
-		});
+								if (slot && targetingValue) {
+									slot.setTargeting(key, targetingValue);
+								}
+							}
+						});
+				}
+				/* eslint-enable @typescript-eslint/no-unnecessary-condition */
 
-	return Promise.race([iasTimeout(), iasDataPromise]);
-};
+				resolve();
+			};
+
+			iasPET.queue.push({
+				adSlots: iasPETSlots,
+				dataHandler: iasDataCallback,
+			});
+		}),
+		1000,
+	);
 
 export { initSlotIas };
