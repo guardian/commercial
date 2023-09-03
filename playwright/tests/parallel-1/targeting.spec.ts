9e00bbbc7fea86179a27974d776520938b65bb09
Ravi <7014230+arelra@users.noreply.github.com>
2023-09-03 22:48:32 +0100

    Move parameter assertions out of network intercepts

--- playwright/tests/parallel-1/targeting.spec.ts
+++ playwright/tests/parallel-1/targeting.spec.ts
@@ -6,85 +6,109 @@ import { bidderURLs, wins } from '../../fixtures/prebid';
 
 const gamUrl = /https:\/\/securepubads.g.doubleclick.net\/gampad\/ads/;
 
-const assertGAMRequestParameter = ({
-	page,
-	parameter,
-	matcher,
-	isCustParam,
-}: {
-	page: Page;
-	parameter: string;
-	matcher: (value: string) => boolean;
-	isCustParam: boolean;
-}) => {
+const getEncodedParamsFromRequest = (
+	request: Request,
+	paramName: string,
+): URLSearchParams | null => {
+	const url = new URL(request.url());
+	const param = url.searchParams.get(paramName);
+	if (!param) return null;
+	const paramDecoded = decodeURIComponent(param);
+	const searchParams = new URLSearchParams(paramDecoded);
+	return searchParams;
+};
+
+const waitForGAMRequestForSlot = (page: Page, slotExpected: string) => {
 	return page.waitForRequest((request) => {
 		const isURL = request.url().match(gamUrl);
 		if (!isURL) return false;
-		const url = new URL(request.url());
-		let paramValue: string | null = '';
-		if (isCustParam) {
-			const custParams = url.searchParams.get('cust_params');
-			if (!custParams) return false;
-			const decodedCustParams = decodeURIComponent(custParams);
-			paramValue = new URLSearchParams(decodedCustParams).get(parameter);
-		} else {
-			paramValue = url.searchParams.get(parameter);
-		}
-		if (paramValue === null) return false;
-		return matcher(paramValue);
+		const searchParams = getEncodedParamsFromRequest(request, 'prev_scp');
+		if (searchParams === null) return false;
+		const slot = searchParams.get('slot');
+		if (slot !== slotExpected) return false;
+		console.log('found request for slot:', slotExpected);
+		return true;
 	});
+};
+
+const assertRequestParameter = (
+	request: Request,
+	name: string,
+	matcher: (value: string) => boolean,
+	isEncoded: boolean = false,
+	encodedParam: string = '',
+): boolean => {
+	const url = new URL(request.url());
+	let params: URLSearchParams | null = url.searchParams;
+	if (isEncoded) {
+		params = getEncodedParamsFromRequest(request, encodedParam);
+	}
+	if (params === null) return false;
+	const paramValue = params.get(name);
+	if (paramValue === null) return false;
+	return matcher(paramValue);
 };
 
 test.describe('GAM targeting', () => {
 	test('checks that a request is made', async ({ page }) => {
-		const gamRequestPromise = page.waitForRequest(gamUrl);
+		const gamRequestPromise = waitForGAMRequestForSlot(
+			page,
+			'top-above-nav',
+		);
 		await loadPage(page, articles[0].path);
 		await acceptAll(page);
 		await gamRequestPromise;
 	});
 
 	test('checks the gdpr_consent param', async ({ page }) => {
-		const gamRequestPromise = assertGAMRequestParameter({
+		const gamRequestPromise = waitForGAMRequestForSlot(
 			page,
-			parameter: 'gdpr_consent',
-			matcher: (value) => value.length > 0,
-			isCustParam: false,
-		});
+			'top-above-nav',
+		);
 		await loadPage(page, articles[0].path);
 		await acceptAll(page);
-		await gamRequestPromise;
+		const request = await gamRequestPromise;
+		const matched = assertRequestParameter(
+			request,
+			'gdpr_consent',
+			(value) => value.length > 0,
+		);
+		expect(matched).toBeTruthy();
 	});
 
 	test(`checks sensitive content is marked as sensitive`, async ({
 		page,
 	}) => {
-		const gamRequestPromise = assertGAMRequestParameter({
+		const gamRequestPromise = waitForGAMRequestForSlot(
 			page,
-			parameter: 'sens',
-			matcher: (value) => value === 't',
-			isCustParam: true,
-		});
+			'top-above-nav',
+		);
 		const sensitivePage = allPages.find(
 			(page) => page?.name === 'sensitive-content',
 		);
-		if (!sensitivePage)
+		if (!sensitivePage) {
 			throw new Error('No sensitive articles found to run test.');
-
+		}
 		await loadPage(page, sensitivePage.path);
 		await acceptAll(page);
-		await gamRequestPromise;
+		const request = await gamRequestPromise;
+		const matched = assertRequestParameter(
+			request,
+			'sens',
+			(value) => value === 't',
+			true,
+			'cust_params',
+		);
+		expect(matched).toBeTruthy();
 	});
 });
 
 test.describe('Prebid targeting', () => {
 	test.beforeEach(async ({ page }) => {
 		page.route(bidderURLs.criteo, async (route) => {
-			const request = route.request();
-			if (request.url().includes(wins.criteo.url)) {
-				console.log(
-					'replying with criteo response for: ',
-					request.url(),
-				);
+			const url = route.request().url();
+			if (url.includes(wins.criteo.url)) {
+				console.log('replying with criteo response for: ', url);
 				route.fulfill({
 					body: JSON.stringify(wins.criteo.response),
 				});
@@ -98,15 +122,14 @@ test.describe('Prebid targeting', () => {
 			if (!isURL) return false;
 			const isCriteoBid = request.url().includes('hb_bidder%3Dcriteo');
 			if (!isCriteoBid) return false;
-			console.log('matched url', request.url());
+			console.log('matched gam criteo request:', request.url());
 			return true;
 		});
 		const assertGamCriteoRequest = (request: Request) => {
-			const url = new URL(request.url());
-			const prevScp = url.searchParams.get('prev_scp');
-			if (!prevScp) return false;
-			const prevScpDecoded = decodeURIComponent(prevScp);
-			const prevScpParams = new URLSearchParams(prevScpDecoded);
+			const prevScpParams = getEncodedParamsFromRequest(
+				request,
+				'prev_scp',
+			);
 			if (!prevScpParams) return false;
 			console.log('prevScpParams', prevScpParams);
 			const allMatched = Object.entries(wins.criteo.targeting).every(
