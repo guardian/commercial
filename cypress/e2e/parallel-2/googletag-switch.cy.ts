2f1c63c783b7eccfc11663c4b59e3721240997a9
Chris Jones <chrislomaxjones96@gmail.com>
2023-09-05 10:50:39 +0100

    construct fixtures URLs

--- cypress/e2e/parallel-2/googletag-switch.cy.ts
+++ cypress/e2e/parallel-2/googletag-switch.cy.ts
@@ -1,11 +1,18 @@
+import { getStage, getTestUrl } from '../../lib/util';
+
 describe('shouldLoadGoogletagSwitch', () => {
 	beforeEach(() => {
 		cy.useConsentedSession('should-load-googletag-switch');
 	});
 
 	it('ad slot should be filled when switch is true', () => {
-		const path =
-			'http://localhost:3030/Front/http://localhost:3031/renderFixture/overwriteShouldLoadGoogletagTrue/uk';
+		const path = getTestUrl(
+			getStage(),
+			'uk',
+			'front',
+			undefined,
+			'overwriteShouldLoadGoogletagTrue',
+		);
 		cy.visit(path);
 
 		// Check that the top-above-nav ad slot is on the page
@@ -18,20 +25,20 @@ describe('shouldLoadGoogletagSwitch', () => {
 		cy.findAdSlotIframeBySlotId('dfp-ad--top-above-nav').should('exist');
 	});
 
-	it('ad slot should be filled when switch is true', () => {
-		const path =
-			'http://localhost:3030/Front/http://localhost:3031/renderFixture/overwriteShouldLoadGoogletagFalse/uk';
+	it('ad slot should be filled when switch is false', () => {
+		const path = getTestUrl(
+			getStage(),
+			'uk',
+			'front',
+			undefined,
+			'overwriteShouldLoadGoogletagFalse',
+		);
 		cy.visit(path);
 
-		// Check that the top-above-nav ad slot is on the page
-		cy.get('#dfp-ad--top-above-nav').should('exist');
+		// eslint-disable-next-line cypress/no-unnecessary-waiting -- Wait for top-above-nav to be removed
+		cy.wait(5_000);
 
-		// creative isn't loaded unless slot is in view
-		cy.get('#dfp-ad--top-above-nav').scrollIntoView();
-
-		// Check that an iframe is placed inside the ad slot
-		cy.findAdSlotIframeBySlotId('dfp-ad--top-above-nav').should(
-			'not.exist',
-		);
+		// Check that the top-above-nav ad slot is not on the page
+		cy.get('#dfp-ad--top-above-nav').should('not.exist');
 	});
 });
