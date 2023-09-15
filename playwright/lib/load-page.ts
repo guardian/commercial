926ebefef10960e409bff3cdcafaa9b2568d9244
Ravi <7014230+arelra@users.noreply.github.com>
2023-09-15 16:34:37 +0100

    Allow region to be passed to page load

--- playwright/lib/load-page.ts
+++ playwright/lib/load-page.ts
@@ -1,6 +1,6 @@
 import { type Page } from '@playwright/test';
 
-const loadPage = async (page: Page, path: string) => {
+const loadPage = async (page: Page, path: string, region = 'GB') => {
 	// abort all ophan requests as it stops the page from firing the 'load' event
 	// await page.route(/ophan.theguardian.com/, (route) => {
 	// 	route.abort();
@@ -8,7 +8,7 @@ const loadPage = async (page: Page, path: string) => {
 	await page.addInitScript(() => {
 		window.localStorage.setItem(
 			'gu.geo.override',
-			JSON.stringify({ value: 'GB' }),
+			JSON.stringify({ value: region }),
 		);
 	});
 	await page.goto(path, { waitUntil: 'domcontentloaded' });
