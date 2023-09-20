48c667b9bdbe04c31805b884177cb9121e9e1256
Ravi <7014230+arelra@users.noreply.github.com>
2023-09-20 18:00:33 +0100

    Refactor cmp lib

--- playwright/lib/cmp.ts
+++ playwright/lib/cmp.ts
@@ -1,28 +1,29 @@
 import type { Page } from '@playwright/test';
 
-const SP_LAYER1_IFRAME_LOCATOR = '[id*="sp_message_iframe"]';
-const SP_ACCEPT_ALL_LOCATOR = 'button.sp_choice_type_11';
-const SP_MANAGE_MY_COOKIES_BUTTON = 'button.sp_choice_type_12';
+const SP_LAYER1_IFRAME = '[id*="sp_message_iframe"]';
+const SP_LAYER1_ACCEPT_ALL_BUTTON = 'button.sp_choice_type_11';
+const SP_LAYER2_MANAGE_MY_COOKIES_BUTTON = 'button.sp_choice_type_12';
 
-const SP_LAYER2_IFRAME_LOCATOR = 'iframe[title="SP Consent Message"]';
-const SP_REJECT_ALL_BUTTON = 'button.sp_choice_type_REJECT_ALL';
+const SP_LAYER2_IFRAME = 'iframe[title="SP Consent Message"]';
+const SP_LAYER2_ACCEPT_ALL_BUTTON = 'button.sp_choice_type_ACCEPT_ALL';
+const SP_LAYER2_REJECT_ALL_BUTTON = 'button.sp_choice_type_REJECT_ALL';
 
 const cmpAcceptAll = async (page: Page) => {
 	const acceptAllButton = page
-		.frameLocator(SP_LAYER1_IFRAME_LOCATOR)
-		.locator(SP_ACCEPT_ALL_LOCATOR);
+		.frameLocator(SP_LAYER1_IFRAME)
+		.locator(SP_LAYER1_ACCEPT_ALL_BUTTON);
 	await acceptAllButton.click();
 	await new Promise((r) => setTimeout(r, 2000));
 };
 
 const cmpRejectAll = async (page: Page) => {
 	const manageMyCookiesButton = page
-		.frameLocator(SP_LAYER1_IFRAME_LOCATOR)
-		.locator(SP_MANAGE_MY_COOKIES_BUTTON);
+		.frameLocator(SP_LAYER1_IFRAME)
+		.locator(SP_LAYER2_MANAGE_MY_COOKIES_BUTTON);
 	await manageMyCookiesButton.click();
 	const rejectAllButton = page
-		.frameLocator(SP_LAYER2_IFRAME_LOCATOR)
-		.locator(SP_REJECT_ALL_BUTTON);
+		.frameLocator(SP_LAYER2_IFRAME)
+		.locator(SP_LAYER2_REJECT_ALL_BUTTON);
 	await rejectAllButton.click();
 	await new Promise((r) => setTimeout(r, 2000));
 };
@@ -31,7 +32,11 @@ const cmpReconsent = async (page: Page) => {
 	const privacySettingsSelector = '[data-link-name="privacy-settings"]';
 	await page.locator(privacySettingsSelector).scrollIntoViewIfNeeded();
 	await page.locator(privacySettingsSelector).click();
-	await cmpAcceptAll(page);
+	const acceptAllButton = page
+		.frameLocator(SP_LAYER2_IFRAME)
+		.locator(SP_LAYER2_ACCEPT_ALL_BUTTON);
+	await acceptAllButton.click();
+	await new Promise((r) => setTimeout(r, 2000));
 };
 
 export { cmpAcceptAll, cmpReconsent, cmpRejectAll };
