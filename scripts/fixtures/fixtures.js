3cb799b3b560533fce10f892d907bc31d3a77b4c
Chris Jones <chrislomaxjones96@gmail.com>
2023-09-21 15:21:03 +0100

    e2e test for ad firing custom event

--- scripts/fixtures/fixtures.js
+++ scripts/fixtures/fixtures.js
@@ -118,6 +118,161 @@ const sponsorshipLogoInThrasher = {
 	},
 };
 
+const foobar = `<div class="us-soccer-header">
+<div class="us-soccer-header--img">
+  <picture>
+	<source
+	  media="(min-width:980px)"
+	  srcset="https://uploads.guim.co.uk/2023/08/02/soccer-desktop.png"
+	/>
+	<source
+	  media="(min-width:1300px)"
+	  srcset="https://uploads.guim.co.uk/2023/08/02/soccer-desktop.png 2x"
+	/>
+	<img
+	  src="https://uploads.guim.co.uk/2023/08/02/soccer-mobile.png"
+	  alt="Soccer Thrasher image"
+	/>
+  </picture>
+</div>
+<div class="us-soccer-header--text">
+  <div>
+	<h1>Soccer</h1>
+	<h2>A US home for the global game</h2>
+  </div>
+  <div>
+	<div class="us-soccer-header--logo-container">
+	  <div class="ad-slot-container">
+	  </div>
+	</div>
+  </div>
+</div>
+</div>`;
+
+const foo = `setTimeout(() => {
+	const adSlot = document.createElement('div');
+	adSlot.id = 'dfp-ad--sponsor-logo';
+	adSlot.dataset.name = 'sponsor-logo';
+	adSlot.dataset.label = 'false';
+	adSlot.dataset.refresh = 'false';
+
+	const container = document.querySelector('.us-soccer-header .ad-slot-container');
+
+	if (!container) {
+		throw Error('ad slot container not found');
+	}
+
+	container.appendChild(adSlot);
+
+	document.dispatchEvent(
+		new CustomEvent('gu.commercial.slot.fill', {
+			detail: { slotId: 'dfp-ad--sponsor-logo' },
+		}),
+	);
+}, 5_000);`;
+
+const advertThatFiresEventInThrasher = {
+	pressedPage: {
+		collections: [
+			{
+				id: '37eb5448-482f-4e0e-9850-fbfb25efbe78',
+				displayName: 'Thrasher Test',
+				curated: [
+					{
+						properties: {
+							isBreaking: false,
+							showMainVideo: false,
+							showKickerTag: false,
+							showByline: false,
+							imageSlideshowReplace: false,
+							isLiveBlog: false,
+							isCrossword: false,
+							byline: 'Guardian Visuals',
+							webTitle: '',
+							embedType: 'interactive',
+							embedUri:
+								'https://content.guardianapis.com/atom/interactive/interactives/thrashers/2023/01/us-soccer-thrasher/default',
+							maybeFrontPublicationDate: 1691057681991,
+							href: 'https://content.guardianapis.com/atom/interactive/interactives/thrashers/2023/01/us-soccer-thrasher/default',
+							webUrl: 'https://content.guardianapis.com/atom/interactive/interactives/thrashers/2023/01/us-soccer-thrasher/default',
+							editionBrandings: [],
+							atomId: 'atom/interactive/interactives/thrashers/2023/01/us-soccer-thrasher/default',
+						},
+						header: {
+							isVideo: false,
+							isComment: false,
+							isGallery: false,
+							isAudio: false,
+							headline: 'Default — default',
+							url: 'snap/1691057660962',
+							hasMainVideoElement: false,
+						},
+						card: {
+							id: 'snap/1691057660962',
+							cardStyle: {
+								type: 'ExternalLink',
+							},
+							shortUrl: '',
+							group: '0',
+							isLive: false,
+						},
+						discussion: {
+							isCommentable: false,
+							isClosedForComments: false,
+						},
+						display: {
+							isBoosted: false,
+							showBoostedHeadline: false,
+							showQuotedHeadline: false,
+							imageHide: false,
+							showLivePlayable: false,
+						},
+						format: {
+							design: 'ArticleDesign',
+							theme: 'NewsPillar',
+							display: 'StandardDisplay',
+						},
+						enriched: {
+							embedHtml: foobar,
+							embedCss:
+								'@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Light.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Light.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Light.ttf) format("truetype");font-weight:300;font-style:normal}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-LightItalic.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-LightItalic.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-LightItalic.ttf) format("truetype");font-weight:300;font-style:italic}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Regular.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Regular.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Regular.ttf) format("truetype");font-weight:400;font-style:normal}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-RegularItalic.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-RegularItalic.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-RegularItalic.ttf) format("truetype");font-weight:400;font-style:italic}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Medium.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Medium.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Medium.ttf) format("truetype");font-weight:500;font-style:normal}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-MediumItalic.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-MediumItalic.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-MediumItalic.ttf) format("truetype");font-weight:500;font-style:italic}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Semibold.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Semibold.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Semibold.ttf) format("truetype");font-weight:600;font-style:normal}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-SemiboldItalic.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-SemiboldItalic.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-SemiboldItalic.ttf) format("truetype");font-weight:600;font-style:italic}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Bold.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Bold.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Bold.ttf) format("truetype");font-weight:700;font-style:normal}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-BoldItalic.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-BoldItalic.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-BoldItalic.ttf) format("truetype");font-weight:700;font-style:italic}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Black.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Black.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-Black.ttf) format("truetype");font-weight:900;font-style:normal}@font-face{font-family:Guardian Headline Full;src:url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-BlackItalic.woff2) format("woff2"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-BlackItalic.woff) format("woff"),url(https://assets.guim.co.uk/static/frontend/fonts/guardian-headline/noalts-not-hinted/GHGuardianHeadline-BlackItalic.ttf) format("truetype");font-weight:900;font-style:italic}@font-face{font-family:Guardian Titlepiece;src:url(https://interactive.guim.co.uk/fonts/garnett/GTGuardianTitlepiece-Bold.woff2) format("woff2"),url(https://interactive.guim.co.uk/fonts/garnett/GTGuardianTitlepiece-Bold.woff) format("woff"),url(https://interactive.guim.co.uk/fonts/garnett/GTGuardianTitlepiece-Bold.ttf) format("truetype");font-weight:700;font-style:normal}.us-soccer-header{background-color:#185e37;position:relative;overflow:hidden;height:48.5vw}@media (min-width: 46.25em){.us-soccer-header{height:250px}}@media (min-width: 61.25em){.us-soccer-header{height:273px}}.us-soccer-header--img{height:100%;width:100%;z-index:0}.us-soccer-header--img picture{width:100%;height:100%;display:block}.us-soccer-header--img img{position:absolute;top:0;right:0;height:100%}@media (min-width: 61.25em){.us-soccer-header--img img{right:0}}@media (min-width: 71.25em){.us-soccer-header--img img{right:10%}}.us-soccer-header--text{padding:0 10px;color:#fff;position:absolute;top:0;left:0;display:flex;flex-flow:column nowrap;height:100%;width:100%;justify-content:space-between;box-sizing:border-box;z-index:1}.us-soccer-header--text>div{width:100%;height:100%;position:relative}.us-soccer-header--text h1{font-family:Guardian Titlepiece,Guardian Headline Full,Guardian Headline,Guardian Egyptian Web,Georgia,serif;font-size:2.2813rem;line-height:2.7369rem;font-weight:700;margin:0}.us-soccer-header--text h2{font-family:Guardian Headline,Guardian Headline Full,Guardian Egyptian Web,Georgia,serif;font-size:1.125rem;font-weight:300;margin:0 0 0 3px;line-height:1.1875rem;max-width:40%}@media (min-width: 41.25em){.us-soccer-header--text{padding:0 20px 12px}}@media (min-width: 46.25em){.us-soccer-header--text h1{font-size:3.2rem;line-height:3rem}.us-soccer-header--text h2{font-size:1.5rem;line-height:1.8rem;max-width:unset}}@media (min-width: 61.25em){.us-soccer-header--text{padding:0 20px 12px}.us-soccer-header--text h1{font-size:4.6875rem;line-height:4.625rem}.us-soccer-header--text h2{font-size:1.5rem;line-height:1.8rem;max-width:unset}}@media (min-width: 81.25em){.us-soccer-header--text{padding:0 20px}}.us-soccer-header--logo-container{padding-left:.6875rem;margin:0 0 14px auto;position:absolute;bottom:0;right:0}@media (min-width: 61.25em){.us-soccer-header--logo-container{margin:0 0 12px auto}}@media (min-width: 81.25em){.us-soccer-header--logo-container{margin:0 0 10px auto}}\n',
+							embedJs: foo,
+						},
+						type: 'LinkSnap',
+					},
+				],
+				backfill: [],
+				treats: [],
+				lastUpdated: 1691057753473,
+				href: 'atom/interactive/interactives/thrashers/2019/12/charity-appeal/default',
+				collectionType: 'fixed/thrasher',
+				uneditable: false,
+				showTags: false,
+				showSections: false,
+				hideKickers: false,
+				showDateHeader: false,
+				showLatestUpdate: false,
+				config: {
+					displayName: 'Thrasher Test',
+					collectionType: 'fixed/thrasher',
+					href: 'atom/interactive/interactives/thrashers/2019/12/charity-appeal/default',
+					uneditable: false,
+					showTags: false,
+					showSections: false,
+					hideKickers: false,
+					showDateHeader: false,
+					showLatestUpdate: false,
+					excludeFromRss: false,
+					showTimestamps: false,
+					hideShowMore: false,
+					platform: 'Any',
+				},
+				hasMore: false,
+			},
+		],
+	},
+};
+
 /**
  * The fixtures represent a set of objects that is deeply merged into the JSON
  * data that is used by DCR. It can be used to override properties for the
@@ -130,6 +285,7 @@ const fixtures = {
 	overwriteShouldLoadGoogletagTrue,
 	overwriteShouldLoadGoogletagFalse,
 	sponsorshipLogoInThrasher,
+	advertThatFiresEventInThrasher,
 };
 
 module.exports = {
