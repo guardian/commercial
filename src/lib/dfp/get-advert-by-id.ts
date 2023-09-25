5549626b7fed1b98d46479fc56e5076713e06cae
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2023-09-25 17:16:47 +0100

    fix get-ad-by-id undefined

--- src/lib/dfp/get-advert-by-id.ts
+++ src/lib/dfp/get-advert-by-id.ts
@@ -1,7 +1,11 @@
 import type { Advert } from './Advert';
 import { dfpEnv } from './dfp-env';
 
-const getAdvertById = (id: string): Advert | null =>
-	id in dfpEnv.advertIds ? dfpEnv.adverts[dfpEnv.advertIds[id]] : null;
-
+const getAdvertById = (id: string): Advert | null => {
+	const advertIndex = dfpEnv.advertIds[id];
+	if (advertIndex !== undefined) {
+		return dfpEnv.adverts[advertIndex] ?? null;
+	}
+	return null;
+};
 export { getAdvertById };
