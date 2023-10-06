0b2e3142cdc0d11ae47aaf5c9aeeb26e18710dd1
Dominik Lander <dominik.lander@guardian.co.uk>
2023-10-06 14:45:12 +0100

    Enable mobile pageskin Playwright test

--- playwright/tests/parallel-4/pageskin.spec.ts
+++ playwright/tests/parallel-4/pageskin.spec.ts
@@ -52,12 +52,15 @@ test.describe('pageskin on uk front', () => {
 		});
 	});
 
-	/**
-	 * TODO e2e flakey test
-	 * The test for tablet always passes locallly but always fails on CI
-	 */
 	small.forEach(({ breakpoint, width, height }) => {
-		test.skip(`Test pageskin front on ${breakpoint} should NOT display the pageskin and NOT use single request mode`, async ({
+		if (breakpoint === 'tablet') {
+			/**
+			 * TODO e2e flakey test
+			 * The test for tablet always passes locally but always fails on CI
+			 */
+			return;
+		}
+		test(`Test pageskin front on ${breakpoint} should NOT display the pageskin and NOT use single request mode`, async ({
 			page,
 		}: {
 			page: Page;
