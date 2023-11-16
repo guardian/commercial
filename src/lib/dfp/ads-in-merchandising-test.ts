4a71451b215dbabcdc8d069fd2c0fef25fc34f97
Simon Adcock <simonadcock2@gmail.com>
2023-11-16 13:02:56 +0000

    remove ads-in-merch ab test

--- src/lib/dfp/ads-in-merchandising-test.ts
+++ src/lib/dfp/ads-in-merchandising-test.ts
@@ -1,6 +1,3 @@
-import { isInVariantSynchronous } from 'lib/experiments/ab';
-import { adsInMerch } from 'lib/experiments/tests/ads-in-merch';
-
 export const includeAdsInMerch = () => {
-	return isInVariantSynchronous(adsInMerch, 'variant');
+	return window.guardian.config.switches.adsInMerch === true;
 };
