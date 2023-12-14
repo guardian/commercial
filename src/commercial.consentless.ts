32da018cc50230f17520f58e5c73c0d24a05a3fc
Dominik Lander <dominik.lander@guardian.co.uk>
2023-12-14 17:30:41 +0000

    Remove script that creates inline ad slots on liveblogs. This is now done by DCR.

--- src/commercial.consentless.ts
+++ src/commercial.consentless.ts
@@ -1,7 +1,6 @@
 import type { ConsentState } from '@guardian/consent-management-platform/dist/types';
 import { initArticleInline } from './consentless/dynamic/article-inline';
 import { initExclusionSlot } from './consentless/dynamic/exclusion-slot';
-import { initLiveblogInline } from './consentless/dynamic/liveblog-inline';
 import { initFixedSlots } from './consentless/init-fixed-slots';
 import { initConsentless } from './consentless/prepare-ootag';
 import { init as setAdTestCookie } from './lib/set-adtest-cookie';
@@ -15,7 +14,6 @@ const bootConsentless = async (consentState: ConsentState): Promise<void> => {
 		initExclusionSlot(),
 		initFixedSlots(),
 		initArticleInline(),
-		initLiveblogInline(),
 	];
 
 	await Promise.all(consentlessModuleList);
