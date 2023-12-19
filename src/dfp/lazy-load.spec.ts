2e160ce74aa2bb543a72131543e955e8b31250df
Jake Lee Kennedy <jlkennedy127@gmail.com>
2023-12-19 16:06:02 +0000

    add eager prebid test

--- src/dfp/lazy-load.spec.ts
+++ src/dfp/lazy-load.spec.ts
@@ -1,9 +1,13 @@
+import { getCurrentBreakpoint as getCurrentBreakpoint_ } from 'detect/detect-breakpoint';
 import type { Advert } from './Advert';
 import { dfpEnv } from './dfp-env';
 import { getAdvertById } from './get-advert-by-id';
 import { enableLazyLoad } from './lazy-load';
 import { loadAdvert } from './load-advert';
 
+const getCurrentBreakpoint = getCurrentBreakpoint_ as jest.MockedFunction<
+	typeof getCurrentBreakpoint_
+>;
 jest.mock('lodash-es', () => ({
 	...jest.requireActual('lodash-es'),
 	// Mock `once` as the identity function so we can re-run `enableLazyLoad`
@@ -25,6 +29,10 @@ jest.mock('./get-advert-by-id');
 
 jest.mock('./load-advert', () => ({
 	loadAdvert: jest.fn(),
+}));
+
+jest.mock('detect/detect-breakpoint', () => ({
+	getCurrentBreakpoint: jest.fn(),
 }));
 
 describe('enableLazyLoad', () => {
@@ -54,14 +62,35 @@ describe('enableLazyLoad', () => {
 		expect(windowIntersectionObserver).toBe(undefined);
 	});
 
-	it('should create a 10% observer if lazyLoadObserve is true', () => {
+	it('should create a 10% and 50% observer if lazyLoadObserve is true and on desktop', () => {
 		dfpEnv.lazyLoadObserve = true;
+
+		getCurrentBreakpoint.mockReturnValue('desktop');
+
 		enableLazyLoad(testAdvert as unknown as Advert);
 		expect(loadAdvert).not.toHaveBeenCalled();
 		expect(
 			window.IntersectionObserver as jest.Mock,
 		).toHaveBeenNthCalledWith(1, expect.anything(), {
 			rootMargin: '10% 0px',
+		});
+
+		expect(
+			window.IntersectionObserver as jest.Mock,
+		).toHaveBeenNthCalledWith(2, expect.anything(), {
+			rootMargin: '50% 0px',
+		});
+	});
+
+	it('should create a 20% observer if lazyLoadObserve is true and on mobile', () => {
+		getCurrentBreakpoint.mockReturnValue('mobile');
+		dfpEnv.lazyLoadObserve = true;
+		enableLazyLoad(testAdvert as unknown as Advert);
+		expect(loadAdvert).not.toHaveBeenCalled();
+		expect(
+			window.IntersectionObserver as jest.Mock,
+		).toHaveBeenNthCalledWith(1, expect.anything(), {
+			rootMargin: '20% 0px',
 		});
 	});
 
