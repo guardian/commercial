cbb22d78fbec74dbedc7affd0cb4bb1522f04342
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2024-01-25 10:19:17 +0000

    add mobile sticky below crossword controls static

---
+++ src/init/consented/mobile-sticky-frontend-mobile.ts
@@ -0,0 +1,46 @@
+import { createAdSlot, wrapSlotInContainer } from 'core/create-ad-slot';
+import fastdom from '../../lib/fastdom-promise';
+
+const insertCrosswordsAd = (anchor: HTMLElement) => {
+	const testSlot = createAdSlot('mobile-sticky');
+
+	const testContainer = wrapSlotInContainer(testSlot, {
+		className:
+			'fc-container fc-container--commercial dfp-ad--mobile-sticky ad-slot-container--centre-slot',
+	});
+
+	testContainer.style.display = 'flex';
+	testContainer.style.justifyContent = 'center';
+	void fastdom.mutate(() => {
+		if (anchor.parentNode) {
+			anchor.parentNode.insertBefore(testContainer, anchor);
+		}
+	});
+};
+
+export const init = (): Promise<void> => {
+	if (window.guardian.config.isDotcomRendering) {
+		return Promise.resolve();
+	}
+
+	const testAnchorSelector = window.guardian.config.page.commentable
+		? '.below_controls + *'
+		: '.content-footer > :first-child';
+
+	const testAnchor: HTMLElement | null =
+		document.querySelector(testAnchorSelector);
+
+	if (testAnchor) {
+		insertCrosswordsAd(testAnchor);
+	} else {
+		window.addEventListener('crosswordLoaded', () => {
+			const anchor: HTMLElement | null =
+				document.querySelector(testAnchorSelector);
+			if (anchor) {
+				insertCrosswordsAd(anchor);
+			} else return;
+		});
+	}
+
+	return Promise.resolve();
+};
