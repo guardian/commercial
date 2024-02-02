b39f275e39bd1e12f344ad74ea01b188fe23471f
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-02-02 17:28:27 +0000

    Move reload module

--- src/init/consentless.ts
+++ src/init/consentless.ts
@@ -3,9 +3,9 @@ import { initArticleInline } from 'init/consentless/dynamic/article-inline';
 import { initExclusionSlot } from 'init/consentless/dynamic/exclusion-slot';
 import { initFixedSlots } from 'init/consentless/init-fixed-slots';
 import { initConsentless } from 'init/consentless/prepare-ootag';
+import { reloadPageOnConsentChange } from 'init/shared/reload-page-on-consent-change';
 import { init as setAdTestCookie } from 'init/shared/set-adtest-cookie';
 import { init as setAdTestInLabelsCookie } from 'init/shared/set-adtest-in-labels-cookie';
-import { reloadPageOnConsentChange } from 'lib/reload-page-on-consent-change';
 
 const bootConsentless = async (consentState: ConsentState): Promise<void> => {
 	const consentlessModuleList = [
