20ebfd45b1b15abe0373041f76f8ddc926972ba0
Dina Hafez <dina.hafez@guardian.co.uk>
2024-03-28 12:30:55 +0000

    Extend mpuWhenNoEpic AB test (#1325)

--- src/experiments/tests/mpu-when-no-epic.ts
+++ src/experiments/tests/mpu-when-no-epic.ts
@@ -4,7 +4,7 @@ export const mpuWhenNoEpic: ABTest = {
 	id: 'MpuWhenNoEpic',
 	author: '@commercial-dev',
 	start: '2023-11-22',
-	expiry: '2024-03-29',
+	expiry: '2024-04-30',
 	audience: 0 / 100,
 	audienceOffset: 5 / 100,
 	audienceCriteria: '',
