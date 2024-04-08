c19dbc6044dde8542b32f5f655e51104bb2571dc
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-04-08 11:24:22 +0100

    check lineitem id in header

--- playwright/tests/exclusion.spec.ts
+++ playwright/tests/exclusion.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { frontWithExclusion } from '../fixtures/pages/fronts';
 import { cmpAcceptAll } from '../lib/cmp';
-import { waitForGAMRequestForSlot } from '../lib/gam';
+import { assertHeader, waitForGAMResponseForSlot } from '../lib/gam';
 import { loadPage } from '../lib/load-page';
 
 const { path } = frontWithExclusion;
@@ -10,11 +10,22 @@ test.describe('Exclusion targeting', () => {
 	test(`Front targeted with exclusion line item should remove slots and containers`, async ({
 		page,
 	}) => {
+		const gamResponse = waitForGAMResponseForSlot(page, 'top-above-nav');
+
 		await loadPage(page, path);
 
 		await cmpAcceptAll(page);
 
-		await waitForGAMRequestForSlot(page, 'top-above-nav');
+		const response = await gamResponse;
+
+		// check single request mode
+		const matched = await assertHeader(
+			response,
+			'google-lineitem-id',
+			(value) => value === '6694738482',
+		);
+
+		expect(matched).toBeTruthy();
 
 		expect(
 			await page.locator('.top-banner-ad-container').isVisible({
