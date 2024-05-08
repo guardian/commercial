87e36aaf8843804c9ca71821a49ffd00ed459f28
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-05-08 10:21:58 +0100

    Separate out ad-load speed from other playwright tests

--- playwright/tests/test-ad-load-speed.spec.ts
+++ playwright/tests/test-ad-load-speed.spec.ts
@@ -6,43 +6,50 @@ import { cmpAcceptAll } from '../lib/cmp';
 import { loadPage } from '../lib/load-page';
 import { waitForSlot } from '../lib/util';
 
-test.describe('top-above-nav slot', () => {
-	test(`Test page  has slot and iframe`, async ({ page }) => {
-		const { path } = articles[0] as unknown as GuPage;
+test(`Test how long top-above-nav takes to load`, async ({ page }) => {
+	test.setTimeout(100000);
 
-		await loadPage(page, path);
+	const { path } = articles[0] as unknown as GuPage;
 
-		const client = await page.context().newCDPSession(page);
+	await loadPage(page, path);
 
-		await client.send('Network.emulateNetworkConditions', {
-			offline: false,
-			downloadThroughput: 4000 * (1024 / 8),
-			uploadThroughput: 2000 * (1024 / 8),
-			latency: 150,
-		});
+	const client = await page.context().newCDPSession(page);
 
-		await page.setViewportSize({ width: 1400, height: 800 });
-
-		await cmpAcceptAll(page);
-
-		loadTimePages.forEach((article) => {
-			async () => {
-				const { path } = article;
-
-				const startRenderingTime = Date.now();
-
-				await loadPage(page, path);
-
-				await waitForSlot(page, 'top-above-nav');
-
-				const endRenderingTime = Date.now();
-
-				console.log(
-					`Ad rendered in ${
-						endRenderingTime - startRenderingTime
-					} ms`,
-				);
-			};
-		});
+	await client.send('Network.emulateNetworkConditions', {
+		offline: false,
+		downloadThroughput: 6000 * (1024 / 8),
+		uploadThroughput: 3000 * (1024 / 8),
+		latency: 150,
 	});
+
+	await page.setViewportSize({ width: 1400, height: 800 });
+
+	await cmpAcceptAll(page);
+
+	let totalAdRenderTime = 0;
+
+	for (const article of loadTimePages) {
+		const { path } = article;
+
+		await new Promise((r) => setTimeout(r, 2000));
+
+		const startRenderingTime = Date.now();
+
+		await page.goto(path, {
+			waitUntil: 'domcontentloaded',
+			timeout: 0,
+		});
+
+		await waitForSlot(page, 'top-above-nav');
+
+		const endRenderingTime = Date.now();
+
+		console.log(
+			`Ad rendered in ${endRenderingTime - startRenderingTime} ms`,
+		);
+
+		totalAdRenderTime += endRenderingTime - startRenderingTime;
+	}
+
+	console.log(`Average ad render time is ${totalAdRenderTime / 5} ms`);
 });
