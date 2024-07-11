87f8fa6e510df2cefaf048c842a730bd63b30011
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2024-07-11 15:12:37 +0100

    remove redundant modules

--- src/experiments/ab-url.ts
+++ src/experiments/ab-url.ts
@@ -21,13 +21,3 @@ export const getForcedParticipationsFromUrl = (): Participations => {
 
 	return {};
 };
-
-// // If the given test has a variant which is forced by the URL, return it
-// export const getVariantFromUrl = (test: ABTest): Variant | null | undefined =>
-// 	testAndParticipationsToVariant(test, getForcedParticipationsFromUrl());
-
-// // Useful if you want to force a test even when it normally wouldn't run
-// // (for example, to display a specific epic variant to verify it renders
-// // okay).
-// export const getIgnoreCanRunFromUrl = (): boolean =>
-// 	window.location.hash.includes('ignoreCanRun=true');
