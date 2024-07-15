0a69e3648b72b1a6d73e894b7ae7b37840ada005
Dina Hafez <dina.hafez@guardian.co.uk>
2024-07-15 12:46:18 +0100

    Changing the prebid.js package name
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

--- src/init/consented/prepare-prebid.ts
+++ src/init/consented/prepare-prebid.ts
@@ -20,7 +20,7 @@ const loadPrebid = async (framework: ConsentFramework): Promise<void> => {
 	if (shouldLoadPrebid()) {
 		await import(
 			// @ts-expect-error -- there’s no types for Prebid.js
-			/* webpackChunkName: "Prebid.js" */ '@guardian/prebid.js/build/dist/prebid'
+			/* webpackChunkName: "Prebid.js" */ 'prebid.js/build/dist/prebid'
 		);
 		prebid.initialise(window, framework);
 	}
