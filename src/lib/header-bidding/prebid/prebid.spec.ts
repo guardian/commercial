432104041bdd2e1d1ee5a62b1b8c157f0de7077f
Dina Hafez <dina.hafez@guardian.co.uk>
2024-07-17 10:16:37 +0100

    Upgrade Prebid.js v8.52.0 (#1466)
    Co-authored-by: Ravi <7014230+arelra@users.noreply.github.com>

--- src/lib/header-bidding/prebid/prebid.spec.ts
+++ src/lib/header-bidding/prebid/prebid.spec.ts
@@ -71,6 +71,7 @@ describe('initialise', () => {
 			deviceAccess: true,
 			disableAjaxTimeout: false,
 			enableSendAllBids: true,
+			maxBid: 5000,
 			maxNestedIframes: 10,
 			mediaTypePriceGranularity: {},
 			priceGranularity: 'custom',
