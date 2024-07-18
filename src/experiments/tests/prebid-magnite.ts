e441dc0df32038a47ee317dc51b253504abff9c3
Dina Hafez <dina.hafez@guardian.co.uk>
2024-07-18 14:41:37 +0100

    Update typo and start date

--- src/experiments/tests/prebid-magnite.ts
+++ src/experiments/tests/prebid-magnite.ts
@@ -3,13 +3,13 @@ import type { ABTest } from '@guardian/ab-core';
 export const prebidMagnite: ABTest = {
 	id: 'PrebidMagnite',
 	author: '@commercial-dev',
-	start: '2024-07-22',
+	start: '2024-07-18',
 	expiry: '2024-09-30',
 	audience: 0 / 100,
 	audienceOffset: 0 / 100,
 	audienceCriteria: '',
 	successMeasure: '',
-	description: 'Test Magnite as a prebid bdidder.',
+	description: 'Test Magnite as a prebid bidder.',
 	variants: [
 		{
 			id: 'control',
