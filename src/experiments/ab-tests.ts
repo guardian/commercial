b24ad47161d3cd47997992d4620ec8ddb2b412b1
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-08-21 15:20:30 +0100

    Add spacefinder test to test array

--- src/experiments/ab-tests.ts
+++ src/experiments/ab-tests.ts
@@ -1,6 +1,7 @@
 import type { ABTest } from '@guardian/ab-core';
 import { mpuWhenNoEpic } from './tests/mpu-when-no-epic';
 import { optOutFrequencyCap } from './tests/opt-out-frequency-cap';
+import { optimiseSpacefinderInline } from './tests/optimise-spacefinder-inline';
 
 /**
  * You only need to add tests to this file if the code you are testing is here in
@@ -11,4 +12,5 @@ export const concurrentTests: ABTest[] = [
 	// one test per line
 	mpuWhenNoEpic,
 	optOutFrequencyCap,
+	optimiseSpacefinderInline,
 ];
