d4aa2e2273c0cc9e9435382eff79c2f256cd8e3e
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2024-09-13 12:04:57 +0100

    remove comment

--- src/utils/report-error.ts
+++ src/utils/report-error.ts
@@ -1,4 +1,3 @@
-// The report error function replace the older Raven based error reporting with the new Sentry based error reporting.
 // This function is used to report errors to Sentry.
 // This uses the `reportError` function from the `window.guardian.modules.sentry` object.
 export const reportError = window.guardian.modules.sentry.reportError;
