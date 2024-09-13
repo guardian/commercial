61c7b4e0fdbc065b4aebfc275946187a34c6af2b
Demetrios Skamiotis <demetrios.skamiotis@guardian.co.uk>
2024-09-13 11:15:40 +0100

    add custom wrap report error function

--- src/utils/report-error.ts
+++ src/utils/report-error.ts
@@ -2,3 +2,16 @@
 // This function is used to report errors to Sentry.
 // This uses the `reportError` function from the `window.guardian.modules.sentry` object.
 export const reportError = window.guardian.modules.sentry.reportError;
+
+export const wrapWithErrorReporting = (
+	fn: (...args: unknown[]) => void,
+	tags: Record<string, string> = {},
+) => {
+	return function (...args: unknown[]) {
+		try {
+			fn(...args);
+		} catch (e) {
+			reportError(e, JSON.stringify(tags));
+		}
+	};
+};
