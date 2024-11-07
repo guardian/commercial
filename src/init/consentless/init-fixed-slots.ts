e8a87175ec9efd10d503c1cd20d4bd0115e724b7
Emma Imber <108270776+emma-imber@users.noreply.github.com>
2024-11-07 15:07:47 +0000

    Don't define static top-above-nav on consentless mobile DCR (#1641)
    * Don't define static top-above-nav on mobile DCR
    
    * Changeset

--- src/init/consentless/init-fixed-slots.ts
+++ src/init/consentless/init-fixed-slots.ts
@@ -1,14 +1,24 @@
+import { getCurrentBreakpoint } from '../../lib/detect/detect-breakpoint';
 import { removeDisabledSlots } from '../consented/remove-slots';
 import { defineSlot } from './define-slot';
 
 const initFixedSlots = async (): Promise<void> => {
 	await removeDisabledSlots();
 
+	const isDCRMobile =
+		window.guardian.config.isDotcomRendering &&
+		getCurrentBreakpoint() === 'mobile';
+
 	const adverts = [
 		...document.querySelectorAll<HTMLElement>(
 			'.js-ad-slot:not(.ad-slot--survey)',
 		),
-	];
+	]
+		// we need to not init top-above-nav on mobile view in DCR
+		// as the DOM element needs to be removed and replaced to be inline
+		.filter(
+			(adSlot) => !(isDCRMobile && adSlot.id === 'dfp-ad--top-above-nav'),
+		);
 
 	// define slots
 	adverts.forEach((slotElement) => {
