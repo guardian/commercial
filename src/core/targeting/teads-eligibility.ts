b4ee197c6303c7e1dc747edca634d77776b3e9b8
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-11-12 12:45:58 +0000

    Make variable name clearer

--- src/core/targeting/teads-eligibility.ts
+++ src/core/targeting/teads-eligibility.ts
@@ -3,7 +3,7 @@ const allowedContentTypes = ['Article', 'LiveBlog'];
 const isEligibleForTeads = (slotId: string) => {
 	const { contentType, isSensitive } = window.guardian.config.page;
 
-	const hasBannedKeywords = window.googletag
+	const isNotBrandSafe = window.googletag
 		.pubads()
 		.getTargeting('ias-kw')
 		.includes('IAS_16425_KW');
@@ -12,7 +12,7 @@ const isEligibleForTeads = (slotId: string) => {
 		slotId === 'dfp-ad--inline1' &&
 		allowedContentTypes.includes(contentType) &&
 		!isSensitive &&
-		!hasBannedKeywords
+		!isNotBrandSafe
 	) {
 		return true;
 	}
