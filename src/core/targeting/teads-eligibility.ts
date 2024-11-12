7ee700b4eb5998edce16fb1aa78615b2157d0a35
Emma Imber <emma-jo.imber@guardian.co.uk>
2024-11-12 12:52:45 +0000

    Add comment for clarity

--- src/core/targeting/teads-eligibility.ts
+++ src/core/targeting/teads-eligibility.ts
@@ -3,6 +3,7 @@ const allowedContentTypes = ['Article', 'LiveBlog'];
 const isEligibleForTeads = (slotId: string) => {
 	const { contentType, isSensitive } = window.guardian.config.page;
 
+	// This IAS value is returned when a page is thought to contain content which is not brand safe
 	const isNotBrandSafe = window.googletag
 		.pubads()
 		.getTargeting('ias-kw')
