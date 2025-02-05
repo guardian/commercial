ac0259de01df6b7eaa73b5a9776901ce949a130b
Imogen Hardy <imogen.hardy@guardian.co.uk>
2025-02-05 11:38:36 +0000

    remove unused comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
-import globals from "globals";
+import globals from 'globals';
 import guardian from '@guardian/eslint-config';
-
 
 export default [
 	{
@@ -8,79 +7,78 @@ export default [
 			'**/*.js',
 			'**/*.mjs',
 			'**/dist',
-			'src/__vendor',
 			'src/lib/__mocks__/ad-sizes.ts',
 		],
 	},
 	...guardian.configs.recommended,
 	...guardian.configs.jest,
 	{
-		    languageOptions: {
-		        globals: {
-		            ...globals.jest,
-		            ...globals.browser,
-		            ...globals.node,
-		            googletag: "readonly",
-		        },
-
-		        ecmaVersion: 5,
-		        sourceType: "commonjs",
-
-		        parserOptions: {
-		            project: ["./tsconfig.json"],
-		            tsconfigRootDir: "./",
-		        },
-		    },
-
-		    settings: {
-		        "import/resolver": {
-		            alias: {
-		                map: [["svgs", "./static/svg"]],
-		            },
-		        },
-		    },
-
-		    rules: {
-		        "id-denylist": ["error"],
-		        "@typescript-eslint/no-unsafe-argument": "off",
-		        "@typescript-eslint/no-unsafe-return": "off",
-		        "@typescript-eslint/unbound-method": "off",
-		        curly: ["error", "multi-line"],
-
-		        "no-use-before-define": ["error", {
-		            functions: true,
-		            classes: true,
-		        }],
-
-		        "import/exports-last": "error",
-		        "no-else-return": "error",
-
-		        "no-restricted-imports": ["error", {
-		            patterns: [{
-		                group: [
-		                    "define/*",
-		                    "display/*",
-		                    "events/*",
-		                    "experiments/*",
-		                    "init/*",
-		                    "lib/*",
-		                    "insert/*",
-		                    "types/*",
-		                ],
-
-		                message: "Non-relative imports from src are forbidden. Please use a relative path instead",
-		            }],
-		        }],
-		    },
-		}, {
-		    files: ["**/*.spec.ts"],
-
-		    rules: {
-		        "@typescript-eslint/unbound-method": "off",
-		    },
-		}
+		languageOptions: {
+			globals: {
+				...globals.jest,
+				...globals.browser,
+				...globals.node,
+				googletag: 'readonly',
+			},
+			ecmaVersion: 5,
+			sourceType: 'commonjs',
+			parserOptions: {
+				project: ['./tsconfig.json'],
+				tsconfigRootDir: './',
+			},
+		},
+		settings: {
+			'import/resolver': {
+				alias: {
+					map: [['svgs', './static/svg']],
+				},
+			},
+		},
+		rules: {
+			'id-denylist': ['error'],
+			'@typescript-eslint/no-unsafe-argument': 'off',
+			'@typescript-eslint/no-unsafe-return': 'off',
+			'@typescript-eslint/unbound-method': 'off',
+			curly: ['error', 'multi-line'],
+			'no-use-before-define': [
+				'error',
+				{
+					functions: true,
+					classes: true,
+				},
+			],
+			'import/exports-last': 'error',
+			'no-else-return': 'error',
+			'no-restricted-imports': [
+				'error',
+				{
+					patterns: [
+						{
+							group: [
+								'define/*',
+								'display/*',
+								'events/*',
+								'experiments/*',
+								'init/*',
+								'lib/*',
+								'insert/*',
+								'types/*',
+							],
+							message:
+								'Non-relative imports from src are forbidden. Please use a relative path instead',
+						},
+					],
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.spec.ts'],
+		rules: {
+			'@typescript-eslint/unbound-method': 'off',
+		},
+	},
 ];
-
 
 // export default [{
 //     ignores: [
@@ -164,7 +162,6 @@ export default [
 //         "@typescript-eslint/unbound-method": "off",
 //     },
 // }];
-
 
 // export default [{
 //     ignores: [
