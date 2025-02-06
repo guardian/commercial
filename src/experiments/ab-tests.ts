3fdc8ef927c11520afcd6edcd2bf78fc4f1fbeee
Charlotte Emms <43961396+cemms1@users.noreply.github.com>
2025-02-06 09:22:48 +0000

    Add Prebid@9.27.0 behind AB test (#1784)
    * add prebid9 as a PNPM alias
    
    * add ab test for prebid9
    
    * use AB test participation to determine which prebid version to use
    
    * use AB test participation to avoid setting deprecated field fastBidVersion for prebid v9

--- src/experiments/ab-tests.ts
+++ src/experiments/ab-tests.ts
@@ -2,6 +2,7 @@ import type { ABTest } from '@guardian/ab-core';
 import { mpuWhenNoEpic } from './tests/mpu-when-no-epic';
 import { optOutFrequencyCap } from './tests/opt-out-frequency-cap';
 import { prebidKeywords } from './tests/prebid-keywords';
+import { prebidV9 } from './tests/prebid-v9';
 
 /**
  * You only need to add tests to this file if the code you are testing is here in
@@ -13,4 +14,5 @@ export const concurrentTests: ABTest[] = [
 	mpuWhenNoEpic,
 	optOutFrequencyCap,
 	prebidKeywords,
+	prebidV9,
 ];
--- src/init/consented/prepare-prebid.ts
+++ src/init/consented/prepare-prebid.ts
@@ -1,6 +1,8 @@
 import type { ConsentFramework } from '@guardian/libs';
 import { getConsentFor, log, onConsent } from '@guardian/libs';
 import { once } from 'lodash-es';
+import { isUserInVariant } from '../../experiments/ab';
+import { prebidV9 } from '../../experiments/tests/prebid-v9';
 import { commercialFeatures } from '../../lib/commercial-features';
 import { isGoogleProxy } from '../../lib/detect/detect-google-proxy';
 import { isInCanada } from '../../lib/geo/geo-utils';
@@ -16,11 +18,12 @@ const shouldLoadPrebid = () =>
 	!shouldIncludeOnlyA9 &&
 	!isInCanada();
 
+const shouldLoadPrebid9 = isUserInVariant(prebidV9, 'variant');
+
 const loadPrebid = async (framework: ConsentFramework): Promise<void> => {
 	if (shouldLoadPrebid()) {
 		await import(
-			// @ts-expect-error -- there’s no types for Prebid.js
-			/* webpackChunkName: "Prebid.js" */ '@guardian/prebid.js/build/dist/prebid'
+			/* webpackChunkName: "Prebid.js" */ `@guardian/prebid${shouldLoadPrebid9 ? '9' : ''}.js/build/dist/prebid`
 		);
 		prebid.initialise(window, framework);
 	}
