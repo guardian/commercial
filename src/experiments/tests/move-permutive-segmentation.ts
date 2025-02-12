5f64c101af4a5008a96aa39dd5f2bef3954a3317
Emma Imber <emma-jo.imber@guardian.co.uk>
2025-02-12 16:06:59 +0000

    Update test dates

--- src/experiments/tests/move-permutive-segmentation.ts
+++ src/experiments/tests/move-permutive-segmentation.ts
@@ -3,8 +3,8 @@ import type { ABTest } from '@guardian/ab-core';
 export const movePermutiveSegmentation: ABTest = {
 	id: 'MovePermutiveSegmentation',
 	author: '@commercial-dev',
-	start: '2025-01-14',
-	expiry: '2025-02-28',
+	start: '2025-02-12',
+	expiry: '2025-03-21',
 	audience: 0 / 100,
 	audienceOffset: 20 / 100,
 	audienceCriteria: '',
