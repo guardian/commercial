a7437c5f63270771fe27b4caa9fa822febc64dfa
Jake Lee Kennedy <jlkennedy127@gmail.com>
2025-02-20 17:27:08 +0000

    fix liveblog test

--- playwright/tests/liveblog-inline-slots.spec.ts
+++ playwright/tests/liveblog-inline-slots.spec.ts
@@ -5,49 +5,45 @@ import { cmpAcceptAll } from '../lib/cmp';
 import { loadPage } from '../lib/load-page';
 import { countLiveblogInlineSlots } from '../lib/util';
 
-const blogPages = blogs.filter(
-	(page) =>
-		'expectedMinInlineSlotsOnDesktop' in page &&
-		'expectedMinInlineSlotsOnMobile' in page,
-);
+const blogPages = blogs.filter((page) => 'expectedMinInlineSlots' in page);
 
 test.describe.serial('A minimum number of ad slots load', () => {
 	blogPages.forEach(({ path, expectedMinInlineSlots }) => {
 		breakpoints.forEach(({ breakpoint, width, height }) => {
 			const isMobile = breakpoint === 'mobile';
 			const expectedMinSlotsOnPage =
-				expectedMinInlineSlots?.[
+				expectedMinInlineSlots[
 					breakpoint as keyof typeof expectedMinInlineSlots
 				];
 
-			if (expectedMinSlotsOnPage) {
-				test(`There are at least ${expectedMinSlotsOnPage} inline total slots at breakpoint ${breakpoint}`, async ({
-					page,
-				}) => {
-					await page.setViewportSize({
-						width,
-						height,
-					});
-
-					await loadPage(page, path);
-					await cmpAcceptAll(page);
-
-					const foundSlots = await countLiveblogInlineSlots(
-						page,
-						isMobile,
-					);
-
-					expect(foundSlots).toBeGreaterThanOrEqual(
-						expectedMinSlotsOnPage,
-					);
+			test(`There are at least ${expectedMinSlotsOnPage} inline total slots at breakpoint ${breakpoint}`, async ({
+				page,
+			}) => {
+				await page.setViewportSize({
+					width,
+					height,
 				});
-			}
+
+				await loadPage(page, path);
+				await cmpAcceptAll(page);
+
+				const foundSlots = await countLiveblogInlineSlots(
+					page,
+					isMobile,
+				);
+
+				expect(foundSlots).toBeGreaterThanOrEqual(
+					expectedMinSlotsOnPage,
+				);
+			});
 		});
 	});
 });
 
 test.describe.serial('Correct set of slots are displayed', () => {
-	const testBlogs = blogs.filter(({ name }) => name === 'under-ad-limit');
+	const testBlogs = blogs.filter(
+		(blog) => 'name' in blog && blog.name === 'under-ad-limit',
+	);
 
 	const firstAdSlotSelectorDesktop = 'liveblog-inline--inline1';
 	const firstAdSlotSelectorMobile = 'liveblog-inline-mobile--top-above-nav';
