9b9688031a1e0ab9f5d9ae0aba5b83256aabcc82
Emma Imber <emma-jo.imber@guardian.co.uk>
2025-02-27 14:29:40 +0000

    Remove prebidkeywords test

--- src/experiments/ab-tests.ts
+++ src/experiments/ab-tests.ts
@@ -2,7 +2,6 @@ import type { ABTest } from '@guardian/ab-core';
 import { deferPermutiveLoad } from './tests/defer-permutive-load';
 import { mpuWhenNoEpic } from './tests/mpu-when-no-epic';
 import { optOutFrequencyCap } from './tests/opt-out-frequency-cap';
-import { prebidKeywords } from './tests/prebid-keywords';
 
 /**
  * You only need to add tests to this file if the code you are testing is here in
@@ -13,6 +12,5 @@ export const concurrentTests: ABTest[] = [
 	// one test per line
 	mpuWhenNoEpic,
 	optOutFrequencyCap,
-	prebidKeywords,
 	deferPermutiveLoad,
 ];
