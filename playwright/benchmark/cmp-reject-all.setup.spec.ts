241b7f49ff367695abae6e48e0fb4e2e5f3431f4
Emma Imber <emma-jo.imber@guardian.co.uk>
2025-03-10 17:35:23 +0000

    Add ability to pass path into visitArticleNoOkta

--- playwright/benchmark/cmp-reject-all.setup.spec.ts
+++ playwright/benchmark/cmp-reject-all.setup.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '@playwright/test';
+import { articles } from '../fixtures/pages';
 import { cmpRejectAll } from '../lib/cmp';
 import { visitArticleNoOkta } from '../lib/load-page';
 import { setupFakeLogin } from '../lib/util';
@@ -8,12 +9,14 @@ const viewport = { width: 1400, height: 800 };
 const authFile = 'playwright/.auth/reject-all.json';
 
 test('setup', async ({ page, context }) => {
+	const loadPath = articles[0].path;
+
 	await page.setViewportSize(viewport);
 
 	// After consent or pay, we need to be logged in and mock an ad-lite subscription to be able to reject all
 	await setupFakeLogin(page, context, false, true);
 
-	await visitArticleNoOkta(page);
+	await visitArticleNoOkta(page, loadPath);
 
 	await cmpRejectAll(page);
 
