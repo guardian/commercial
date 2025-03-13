423366a582967520a08bbfba6307778e44df98a2
Demetrios Skamiotis <49187886+dskamiotis@users.noreply.github.com>
2025-03-13 10:28:39 +0000

    add permutive delay init behind 10% test (#1849)

--- src/experiments/tests/defer-permutive-load.ts
+++ src/experiments/tests/defer-permutive-load.ts
@@ -3,9 +3,9 @@ import type { ABTest } from '@guardian/ab-core';
 export const deferPermutiveLoad: ABTest = {
 	id: 'DeferPermutiveLoad',
 	author: '@commercial-dev',
-	start: '2025-02-11',
-	expiry: '2024-02-28',
-	audience: 0 / 100,
+	start: '2025-03-10',
+	expiry: '2025-03-31',
+	audience: 20 / 100,
 	audienceOffset: 0 / 100,
 	audienceCriteria: '',
 	successMeasure: '',
