a523dd2a241cd765ce1b605273ae6d6e1e4d56e5
Dina Hafez <dina.hafez@guardian.co.uk>
2025-03-17 10:53:47 +0000

    Fix expiry date for prebidBidCache test

--- src/experiments/tests/prebid-bid-cache.ts
+++ src/experiments/tests/prebid-bid-cache.ts
@@ -4,7 +4,7 @@ export const prebidBidCache: ABTest = {
 	id: 'PrebidBidCache',
 	author: '@commercial-dev',
 	start: '2025-03-10',
-	expiry: '2024-03-24',
+	expiry: '2025-03-28',
 	audience: 10 / 100,
 	audienceOffset: 30 / 100,
 	audienceCriteria: '',
