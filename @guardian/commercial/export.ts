7b60f2e49de18911ffb9b0c194af309d07bfd559
Charlotte <charlotte.emms@theguardian.com>
2025-03-19 17:19:32 +0000

    separate commercial package into distinct pnpm workspace

---
+++ @guardian/commercial/export.ts
@@ -0,0 +1,16 @@
+export { isAdBlockInUse } from '../../src/lib/detect-ad-blocker';
+export { EventTimer } from '../../src/lib/event-timer';
+export { adSizes } from '../../src/lib/ad-sizes';
+export * as constants from '../../src/lib/constants';
+export {
+	bypassCommercialMetricsSampling,
+	initCommercialMetrics,
+} from '../../src/lib/send-commercial-metrics';
+export { buildPageTargeting } from '../../src/lib/targeting/build-page-targeting';
+export { postMessage } from '../../src/lib/messenger/post-message';
+export { buildImaAdTagUrl } from '../../src/lib/targeting/youtube-ima';
+export { getPermutivePFPSegments } from '../../src/lib/permutive';
+export type { AdsConfigDisabled } from '../../src/lib/types';
+export type { AdSize, SizeMapping, SlotName } from '../../src/lib/ad-sizes';
+export type { PageTargeting } from '../../src/lib/targeting/build-page-targeting';
+export type { AdsConfigUSNATorAus, AdsConfigTCFV2 } from '../../src/lib/types';
