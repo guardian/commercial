d3e0ce89ff66a1442716244841becf7fe9e5213e
Dina Hafez <dina.hafez@guardian.co.uk>
2025-04-24 12:56:04 +0100

    Import multibid module and change start date for testing in CODE

--- src/experiments/tests/prebid-multibid.ts
+++ src/experiments/tests/prebid-multibid.ts
@@ -3,14 +3,14 @@ import type { ABTest } from '@guardian/ab-core';
 export const prebidMultibid: ABTest = {
 	id: 'PrebidMultibid',
 	author: '@commercial-dev',
-	start: '2025-04-28',
+	start: '2025-04-24',
 	expiry: '2025-05-12',
 	audience: 0 / 100,
 	audienceOffset: 0 / 100,
 	audienceCriteria: '',
 	successMeasure: '',
 	description:
-		'Test multibid feature that allows configured bidders for bid cache to pass more than one bid per AdUnit through to the ad server.',
+		'Test multibid feature with useBicCache to allows configured bidders to pass more than one bid per AdUnit through to the ad server.',
 	variants: [
 		{
 			id: 'control',
