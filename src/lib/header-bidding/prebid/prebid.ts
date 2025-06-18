ff9831d2379e727faa1b27e10dfb9e3314da5c0b
Dina Hafez <dina.hafez@guardian.co.uk>
2025-06-18 10:05:47 +0100

    WIP

--- src/lib/header-bidding/prebid/prebid.ts
+++ src/lib/header-bidding/prebid/prebid.ts
@@ -9,6 +9,7 @@ import { prebidMultibid } from '../../../experiments/tests/prebid-multibid';
 import type { AdSize } from '../../../lib/ad-sizes';
 import { createAdSize } from '../../../lib/ad-sizes';
 import { PREBID_TIMEOUT } from '../../../lib/constants/prebid-timeout';
+import { getViewport } from '../../../lib/detect/detect-viewport';
 import { EventTimer } from '../../../lib/event-timer';
 import { getPermutiveSegments } from '../../../lib/permutive';
 import type { PageTargeting } from '../../../lib/targeting/build-page-targeting';
@@ -248,6 +249,7 @@ class PrebidAdUnit {
 		);
 
 		advert.headerBiddingSizes = slot.sizes;
+		// Update this to show the whole message in case of having a groupedSlot inline or fronts-banner otherwise it will only show the advert id
 		log(
 			'commercial',
 			`PrebidAdUnit ${advert.id} | bid cache groupedSlot '${this.code}'`,
@@ -258,6 +260,21 @@ class PrebidAdUnit {
 	isEmpty() {
 		return this.code == null;
 	}
+
+	checkDivInView(advert: Advert): boolean {
+		const element = document.querySelector(`#${advert.id}`);
+		if (!element) {
+			return false;
+		}
+		const rect = element.getBoundingClientRect();
+		log(
+			'commercial',
+			`Checking if advert ${advert.id} is in view`,
+			rect.top < getViewport().height && rect.bottom > 0,
+		);
+		return rect.top < getViewport().height && rect.bottom > 0;
+	}
+
 	private generateGpid(advert: Advert, slot: HeaderBiddingSlot): string {
 		const sectionName = window.guardian.config.page.section;
 		const contentType = window.guardian.config.page.contentType;
@@ -304,7 +321,7 @@ declare global {
 			onEvent: (event: PbjsEvent, handler: PbjsEventHandler) => void;
 			setTargetingForGPTAsync: (
 				codeArr?: string[],
-				customSlotMatching?: (slot: unknown) => unknown,
+				customSlotMatching?: (slot: googletag.Slot) => unknown,
 			) => void;
 			getEvents: () => PrebidEvent[];
 		};
@@ -670,6 +687,33 @@ const initialise = (window: Window, consentState: ConsentState): void => {
 	});
 };
 
+// Check if the div in view includes fronts-banner or inline adUnit code then return that advert id
+const divInView = (advertId: string, adUnit: PrebidAdUnit) => {
+	const isFrontsBannerOrInline =
+		isString(adUnit.code) && advertId.includes(adUnit.code);
+
+	if (isFrontsBannerOrInline) {
+		const element = document.querySelector(`#${advertId}`);
+		if (!element) {
+			return false;
+		}
+		const rect = element.getBoundingClientRect();
+		return rect.top < getViewport().height && rect.bottom > 0;
+	}
+
+	return false;
+};
+
+const customSlotMatching = (slot: googletag.Slot) => {
+	return function (adUnit: PrebidAdUnit) {
+		const advert = getAdvertById(slot.getSlotElementId());
+		return (
+			advert !== null &&
+			(advert.id === adUnit.code || divInView(advert.id, adUnit))
+		);
+	};
+};
+
 const bidsBackHandler = (
 	adUnits: PrebidAdUnit[],
 	eventTimer: EventTimer,
@@ -677,6 +721,7 @@ const bidsBackHandler = (
 	new Promise((resolve) => {
 		window.pbjs?.setTargetingForGPTAsync(
 			adUnits.map((u) => u.code).filter(isString),
+			customSlotMatching,
 		);
 
 		resolve();
@@ -719,7 +764,11 @@ const requestBids = async (
 									consentState,
 								),
 						)
-						.filter((adUnit) => !adUnit.isEmpty()),
+						.filter(
+							(adUnit) =>
+								!adUnit.isEmpty() &&
+								adUnit.checkDivInView(advert),
+						),
 				),
 			);
 		})
