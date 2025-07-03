b5f0a06fc0371927f2d75f85cf49d98f221505e1
Jake <jake.kennedy@guardian.co.uk>
2025-07-03 13:41:13 +0100

    Set up 0% AB test for prebid version bump (#2057)
    Co-authored-by: George Haberis <george_haberis@36869_mac.gnm.int>

---
+++ src/lib/header-bidding/prebid/pbjs-v9.46.0.ts
@@ -0,0 +1,29 @@
+import pbjs from 'prebid-v9.46.0.js';
+
+import 'prebid-v9.46.0.js/modules/adyoulikeBidAdapter';
+import 'prebid-v9.46.0.js/modules/consentManagementTcf';
+import 'prebid-v9.46.0.js/modules/consentManagementUsp';
+import 'prebid-v9.46.0.js/modules/criteoBidAdapter';
+import 'prebid-v9.46.0.js/modules/gridBidAdapter';
+import 'prebid-v9.46.0.js/modules/ixBidAdapter';
+import 'prebid-v9.46.0.js/modules/ozoneBidAdapter';
+import 'prebid-v9.46.0.js/modules/permutiveRtdProvider';
+import 'prebid-v9.46.0.js/modules/prebidServerBidAdapter';
+import 'prebid-v9.46.0.js/modules/pubmaticBidAdapter';
+import 'prebid-v9.46.0.js/modules/sharedIdSystem';
+import 'prebid-v9.46.0.js/modules/tripleliftBidAdapter';
+import 'prebid-v9.46.0.js/modules/kargoBidAdapter';
+import 'prebid-v9.46.0.js/modules/rubiconBidAdapter';
+import 'prebid-v9.46.0.js/modules/ttdBidAdapter';
+import 'prebid-v9.46.0.js/modules/multibid';
+import 'prebid-v9.46.0.js/modules/id5IdSystem';
+import 'prebid-v9.46.0.js/modules/userId';
+
+// Guardian specific adapters that we have modified or created
+import './modules/appnexusBidAdapter';
+import './modules/openxBidAdapter';
+import './modules/analyticsAdapter';
+
+pbjs.processQueue();
+
+export { pbjs };
--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -107,3 +107,113 @@ declare module 'prebid.js/src/ajax.js' {
 	const prebidFetch: typeof fetch;
 	export { prebidFetch as fetch };
 }
+
+/**
+ * The declarations below for prebid-v9.46.0.js
+ * can be deleted once we've tested this dependency
+ **/
+
+declare module 'prebid-v9.46.0.js' {
+	const pbjs: {
+		processQueue: () => void;
+	};
+
+	export default pbjs;
+}
+
+declare module 'prebid-v9.46.0.js/modules/openxBidAdapter' {
+	const spec: {
+		aliases: string[];
+	};
+
+	export { spec };
+}
+
+declare module 'prebid-v9.46.0.js/modules/appnexusBidAdapter' {
+	const spec: {
+		aliases: Array<{ code: string }>;
+	};
+
+	export { spec };
+}
+
+declare module 'prebid-v9.46.0.js/adapters/bidderFactory' {
+	const registerBidder: (spec: unknown) => void;
+
+	export { registerBidder };
+}
+
+declare module 'prebid-v9.46.0.js/src/cpmBucketManager' {
+	type PrebidPriceGranularity = {
+		buckets: Array<{
+			precision?: number;
+			max: number;
+			increment: number;
+		}>;
+	};
+
+	const getPriceBucketString: (
+		cpm: number,
+		priceBuckets: PrebidPriceGranularity,
+	) => {
+		custom: string;
+	};
+
+	export { getPriceBucketString };
+	export type { PrebidPriceGranularity };
+}
+
+// Added type definitions for prebid-v9.46.0.js analytics modules
+declare module 'prebid-v9.46.0.js/libraries/analyticsAdapter/AnalyticsAdapter.js' {
+	// eslint-disable-next-line no-restricted-imports -- can't use relative imports in ambient declarations
+	import type {
+		AnalyticsAdapter,
+		AnalyticsAdapterConfig,
+	} from 'lib/header-bidding/prebid-types';
+
+	function adapter(config: AnalyticsAdapterConfig): AnalyticsAdapter;
+	export default adapter;
+}
+
+declare module 'prebid-v9.46.0.js/src/adapterManager.js' {
+	interface AnalyticsAdapterRegistration {
+		adapter: unknown;
+		code: string;
+	}
+
+	const adapterManager: {
+		registerAnalyticsAdapter: (
+			registration: AnalyticsAdapterRegistration,
+		) => void;
+	};
+
+	export default adapterManager;
+}
+
+declare module 'prebid-v9.46.0.js/src/constants.js' {
+	const EVENTS: {
+		AUCTION_INIT: string;
+		BID_REQUESTED: string;
+		BID_RESPONSE: string;
+		NO_BID: string;
+		AUCTION_END: string;
+		BID_WON: string;
+		[key: string]: string;
+	};
+
+	export { EVENTS };
+}
+
+declare module 'prebid-v9.46.0.js/src/utils.js' {
+	const utils: {
+		logError: (message: string) => void;
+		[key: string]: unknown;
+	};
+
+	export { utils };
+}
+
+declare module 'prebid-v9.46.0.js/src/ajax.js' {
+	const prebidFetch: typeof fetch;
+	export { prebidFetch as fetch };
+}
