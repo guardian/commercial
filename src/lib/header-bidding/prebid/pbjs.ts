ae4dae2f140c87cb3f9a3fa060ac9fd43a3b99c1
Charlotte <charlotte.emms@theguardian.com>
2025-07-10 17:23:08 +0100

    reorganise prebid module imports, include rtdModule

--- src/lib/header-bidding/prebid/pbjs.ts
+++ src/lib/header-bidding/prebid/pbjs.ts
@@ -1,23 +1,31 @@
 import pbjs from 'prebid.js';
 
-import 'prebid.js/modules/adyoulikeBidAdapter';
+// Consent management
 import 'prebid.js/modules/consentManagementTcf';
 import 'prebid.js/modules/consentManagementUsp';
 import 'prebid.js/modules/consentManagementGpp';
+
+// Bid adapters
+import 'prebid.js/modules/adyoulikeBidAdapter';
 import 'prebid.js/modules/criteoBidAdapter';
 import 'prebid.js/modules/gridBidAdapter';
 import 'prebid.js/modules/ixBidAdapter';
 import 'prebid.js/modules/ozoneBidAdapter';
-import 'prebid.js/modules/permutiveRtdProvider';
 import 'prebid.js/modules/prebidServerBidAdapter';
 import 'prebid.js/modules/pubmaticBidAdapter';
-import 'prebid.js/modules/sharedIdSystem';
 import 'prebid.js/modules/tripleliftBidAdapter';
 import 'prebid.js/modules/kargoBidAdapter';
 import 'prebid.js/modules/rubiconBidAdapter';
 import 'prebid.js/modules/ttdBidAdapter';
+
+// User IDs
 import 'prebid.js/modules/id5IdSystem';
+import 'prebid.js/modules/sharedIdSystem';
 import 'prebid.js/modules/userId';
+
+// Real time data
+import 'prebid.js/modules/rtdModule';
+import 'prebid.js/modules/permutiveRtdProvider';
 
 // Guardian specific adapters that we have modified or created
 import './modules/appnexusBidAdapter';
