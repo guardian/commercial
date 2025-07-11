5c6e8265cde4df06347706f1be8505e4adaa0516
Charlotte <charlotte.emms@theguardian.com>
2025-07-11 09:35:29 +0100

    replicate pbjs file changes in beta version for 9.46.0

--- src/lib/header-bidding/prebid/pbjs-v9.46.0.ts
+++ src/lib/header-bidding/prebid/pbjs-v9.46.0.ts
@@ -1,23 +1,31 @@
 import pbjs from 'prebid-v9.46.0.js';
 
-import 'prebid-v9.46.0.js/modules/adyoulikeBidAdapter';
+// Consent management
 import 'prebid-v9.46.0.js/modules/consentManagementTcf';
 import 'prebid-v9.46.0.js/modules/consentManagementUsp';
 import 'prebid-v9.46.0.js/modules/consentManagementGpp';
+
+// Bid adapters
+import 'prebid-v9.46.0.js/modules/adyoulikeBidAdapter';
 import 'prebid-v9.46.0.js/modules/criteoBidAdapter';
 import 'prebid-v9.46.0.js/modules/gridBidAdapter';
 import 'prebid-v9.46.0.js/modules/ixBidAdapter';
 import 'prebid-v9.46.0.js/modules/ozoneBidAdapter';
-import 'prebid-v9.46.0.js/modules/permutiveRtdProvider';
 import 'prebid-v9.46.0.js/modules/prebidServerBidAdapter';
 import 'prebid-v9.46.0.js/modules/pubmaticBidAdapter';
-import 'prebid-v9.46.0.js/modules/sharedIdSystem';
 import 'prebid-v9.46.0.js/modules/tripleliftBidAdapter';
 import 'prebid-v9.46.0.js/modules/kargoBidAdapter';
 import 'prebid-v9.46.0.js/modules/rubiconBidAdapter';
 import 'prebid-v9.46.0.js/modules/ttdBidAdapter';
+
+// User IDs
 import 'prebid-v9.46.0.js/modules/id5IdSystem';
+import 'prebid-v9.46.0.js/modules/sharedIdSystem';
 import 'prebid-v9.46.0.js/modules/userId';
+
+// Real time data
+import 'prebid-v9.46.0.js/modules/rtdModule';
+import 'prebid-v9.46.0.js/modules/permutiveRtdProvider';
 
 // Guardian specific adapters that we have modified or created
 import './modules/appnexusBidAdapter';
