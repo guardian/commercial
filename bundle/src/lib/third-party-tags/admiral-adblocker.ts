3cc1737d9121f89ae271aaacda20cac8740ae7d2
Charlotte <charlotte.emms@theguardian.com>
2025-07-17 16:13:46 +0100

    add event handler for adblock (measure) events

--- bundle/src/lib/third-party-tags/admiral-adblocker.ts
+++ bundle/src/lib/third-party-tags/admiral-adblocker.ts
@@ -1,5 +1,47 @@
+import { log } from '@guardian/libs';
 import { admiralScript } from '../__vendor/admiral';
 import type { GetThirdPartyTag } from '../types';
+
+type MeasureDetectedEvent = {
+	adblocking: boolean;
+	whitelisted: boolean;
+	subscribed: boolean;
+};
+
+const onLoad = () => {
+	// eslint-disable-next-line -- Stub provided by Admiral
+	window.admiral =
+		window.admiral ||
+		function () {
+			// eslint-disable-next-line -- Stub provided by Admiral
+			(admiral.q = admiral.q || []).push(arguments);
+		};
+
+	window.admiral(
+		'after',
+		'measure.detected',
+		function (measureDetectedEvent: MeasureDetectedEvent) {
+			if (measureDetectedEvent.adblocking) {
+				log(
+					'commercial',
+					'❗️ Admiral detection: user has an adblocker and it is enabled',
+				);
+			}
+			if (measureDetectedEvent.whitelisted) {
+				log(
+					'commercial',
+					'⚪️ Admiral detection: user has seen Engage and subsequently disabled their adblocker',
+				);
+			}
+			if (measureDetectedEvent.subscribed) {
+				log(
+					'commercial',
+					'🆗 Admiral detection: user has an active subscription to a transact plan',
+				);
+			}
+		},
+	);
+};
 
 /**
  * Admiral adblocker recovery tag
@@ -9,6 +51,7 @@ const admiralTag: GetThirdPartyTag = ({ shouldRun }) => ({
 	name: 'admiral',
 	insertSnippet: admiralScript,
 	async: true,
+	onLoad,
 });
 
 // Exports for testing only
