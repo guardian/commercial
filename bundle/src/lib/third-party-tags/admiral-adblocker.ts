c8124c4ff91169f405adb0e54e8098ed515c08b1
Charlotte <charlotte.emms@theguardian.com>
2025-07-29 14:49:49 +0100

    migrate from client side to server side Admiral implementation method

--- bundle/src/lib/third-party-tags/admiral-adblocker.ts
+++ bundle/src/lib/third-party-tags/admiral-adblocker.ts
@@ -1,6 +1,10 @@
 import { log } from '@guardian/libs';
-import { admiralScript } from '../__vendor/admiral';
 import type { GetThirdPartyTag } from '../types';
+
+const admiralUrl =
+	window.guardian.config.stage === 'CODE'
+		? 'https://code.api.nextgen.guardianapps.co.uk/commercial/admiral-bootstrap'
+		: 'https://api.nextgen.guardianapps.co.uk/commercial/admiral-bootstrap';
 
 /**
  * Admiral adblock recovery tag
@@ -8,8 +12,8 @@ import type { GetThirdPartyTag } from '../types';
 const admiralTag: GetThirdPartyTag = ({ shouldRun }) => ({
 	shouldRun,
 	name: 'admiral',
-	insertSnippet: admiralScript,
 	async: true,
+	url: admiralUrl,
 	beforeLoad: () =>
 		log('commercial', '🛡️ Loading Admiral script on the page'),
 });
