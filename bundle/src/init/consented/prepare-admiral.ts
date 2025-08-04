32da4c36e13533a31328e6817df59b1cfd5dbe91
Charlotte <charlotte.emms@theguardian.com>
2025-08-04 16:06:51 +0100

    add Admiral candidate events to commercial logger

--- bundle/src/init/consented/prepare-admiral.ts
+++ bundle/src/init/consented/prepare-admiral.ts
@@ -1,54 +1,131 @@
 import { log } from '@guardian/libs';
 
-type AdmiralMeasureDetectedEvent = {
+type AdmiralEvent = Record<string, unknown>;
+type MeasureDetectedEvent = {
 	adblocking: boolean;
 	whitelisted: boolean;
 	subscribed: boolean;
 };
+type CandidateShownEvent = {
+	candidateID: string;
+	variantID: string;
+	candidateGroups: string[];
+};
+type CandidateDismissedEvent = {
+	candidateID: string;
+	candidateGroups: string[];
+};
 
 const admiralLogPrefix = '🛡️ Admiral';
 
-const isAdmiralMeasureDetectedEvent = (
-	e: Record<string, unknown>,
-): e is AdmiralMeasureDetectedEvent => {
-	if (
-		typeof e === 'object' &&
-		'adblocking' in e &&
-		'whitelisted' in e &&
-		'subscribed' in e
-	) {
-		return true;
+const handleMeasureDetectedEvent = (event: AdmiralEvent): void => {
+	const isMeasureDetectedEvent = (
+		e: AdmiralEvent,
+	): e is MeasureDetectedEvent => {
+		if (
+			typeof e === 'object' &&
+			'adblocking' in e &&
+			'whitelisted' in e &&
+			'subscribed' in e
+		) {
+			return true;
+		}
+		return false;
+	};
+
+	if (isMeasureDetectedEvent(event)) {
+		if (event.adblocking) {
+			log(
+				'commercial',
+				`${admiralLogPrefix}: user has an adblocker and it is enabled`,
+			);
+		}
+		if (event.whitelisted) {
+			log(
+				'commercial',
+				`${admiralLogPrefix}: user has seen Engage and subsequently disabled their adblocker`,
+			);
+		}
+		if (event.subscribed) {
+			log(
+				'commercial',
+				`${admiralLogPrefix}: user has an active subscription to a transact plan`,
+			);
+		}
+	} else {
+		log(
+			'commercial',
+			`${admiralLogPrefix}: Event is not of expected format of measure.detected ${JSON.stringify(event)}`,
+		);
 	}
-	return false;
+};
+
+const handleCandidateShownEvent = (event: AdmiralEvent): void => {
+	const isCandidateShownEvent = (
+		e: AdmiralEvent,
+	): e is CandidateShownEvent => {
+		if (
+			typeof e === 'object' &&
+			'candidateID' in e &&
+			'variantID' in e &&
+			'candidateGroups' in e
+		) {
+			return true;
+		}
+		return false;
+	};
+
+	if (isCandidateShownEvent(event)) {
+		log(
+			'commercial',
+			`${admiralLogPrefix}: Launching candidate ${event.candidateID} with variant ${event.variantID}`,
+		);
+	} else {
+		log(
+			'commercial',
+			`${admiralLogPrefix}: Event is not of expected format of candidate.shown ${JSON.stringify(event)}`,
+		);
+	}
+};
+
+const handleCandidateDismissedEvent = (event: AdmiralEvent): void => {
+	const isCandidateDismissedEvent = (
+		e: AdmiralEvent,
+	): e is CandidateDismissedEvent => {
+		if (
+			typeof e === 'object' &&
+			'candidateID' in e &&
+			'candidateGroups' in e
+		) {
+			return true;
+		}
+		return false;
+	};
+
+	if (isCandidateDismissedEvent(event)) {
+		log(
+			'commercial',
+			`${admiralLogPrefix}: Candidate ${event.candidateID} was dismissed`,
+		);
+	} else {
+		log(
+			'commercial',
+			`${admiralLogPrefix}: Event is not of expected format of candidate.dismissed ${JSON.stringify(event)}`,
+		);
+	}
 };
 
 const setUpAdmiralEventLogger = (): void => {
 	window.admiral?.('after', 'measure.detected', function (event) {
-		if (isAdmiralMeasureDetectedEvent(event)) {
-			if (event.adblocking) {
-				log(
-					'commercial',
-					`${admiralLogPrefix}: user has an adblocker and it is enabled`,
-				);
-			}
-			if (event.whitelisted) {
-				log(
-					'commercial',
-					`${admiralLogPrefix}: user has seen Engage and subsequently disabled their adblocker`,
-				);
-			}
-			if (event.subscribed) {
-				log(
-					'commercial',
-					`${admiralLogPrefix}: user has an active subscription to a transact plan`,
-				);
-			}
-		} else {
-			log(
-				'commercial',
-				`${admiralLogPrefix}: Event is not of expected format of measure.detected ${JSON.stringify(event)}`,
-			);
-		}
+		void handleMeasureDetectedEvent(event);
+	});
+
+	window.admiral?.('after', 'candidate.shown', function (event) {
+		void handleCandidateShownEvent(event);
+	});
+
+	window.admiral?.('after', 'candidate.dismissed', function (event) {
+		void handleCandidateDismissedEvent(event);
 	});
 };
 
