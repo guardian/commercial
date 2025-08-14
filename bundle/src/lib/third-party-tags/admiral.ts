844040d5913a9fa3e02be5523c3d74970ca4218d
George Haberis <george_haberis@36869_mac.gnm.int>
2025-08-14 16:54:05 +0100

    admiral banner should not run if the gu_hide_support_messaging cookie is set

--- bundle/src/lib/third-party-tags/admiral.ts
+++ bundle/src/lib/third-party-tags/admiral.ts
@@ -1,5 +1,5 @@
 import { isInUsa } from '@guardian/commercial-core/geo/geo-utils';
-import { cmp, log } from '@guardian/libs';
+import { cmp, getCookie, log } from '@guardian/libs';
 import {
 	getAdmiralAbTestVariant,
 	recordAdmiralOphanEvent,
@@ -15,12 +15,18 @@ const BASE_AJAX_URL =
 const abTestVariant = getAdmiralAbTestVariant();
 const isInVariant = abTestVariant?.startsWith('variant') ?? false;
 
+console.log('***', !getCookie({
+	name: 'gu_hide_support_messaging',
+	shouldMemoize: true,
+}));
+
 /**
  * The Admiral bootstrap script should only run under the following conditions:
  *
  * - Should not run if the CMP is due to show
  * - Should only run in the US
  * - Should only run if in the variant of the AB test
+ * - Should not run if the gu_hide_support_messaging cookie is set
  * - Should not run for content marked as: shouldHideAdverts, shouldHideReaderRevenue, isSensitive
  * - Should not run for paid-content sponsorship type (includes Hosted Content)
  * - Should not run for certain sections
@@ -30,6 +36,10 @@ const shouldRun =
 	!cmp.willShowPrivacyMessageSync() &&
 	isInUsa() &&
 	isInVariant &&
+	!getCookie({
+		name: 'gu_hide_support_messaging',
+		shouldMemoize: true,
+	}) &&
 	!window.guardian.config.page.shouldHideAdverts &&
 	!window.guardian.config.page.shouldHideReaderRevenue &&
 	!window.guardian.config.page.isSensitive &&
