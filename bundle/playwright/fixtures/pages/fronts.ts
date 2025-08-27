9ec11537291927006d051df9f8f1e5f797e3ad3d
Ravi <7014230+arelra@users.noreply.github.com>
2025-08-27 17:16:37 +0100

    Fix exclusion test

--- bundle/playwright/fixtures/pages/fronts.ts
+++ bundle/playwright/fixtures/pages/fronts.ts
@@ -1,70 +1,21 @@
 import type { GuPage } from './Page';
 
-type Front = GuPage & {
-	section: string;
-};
-
-type TagPage = GuPage;
-
 const fronts = [
-	{
-		// path: getTestUrl({
-		// 	path: '/uk',
-		// 	type: 'front',
-		// 	adtest: 'fixed-puppies',
-		// }),
-		path: '/Front/https://www.theguardian.com/uk',
-		section: 'uk',
-	},
-	{
-		// path: getTestUrl({
-		// 	path: '/uk/commentisfree',
-		// 	type: 'front',
-		// 	adtest: 'fixed-puppies',
-		// }),
-		path: '/Front/https://www.theguardian.com/uk/commentisfree',
-		section: 'commentisfree',
-	},
-	{
-		// path: getTestUrl({
-		// 	path: '/uk/sport',
-		// 	type: 'front',
-		// 	adtest: 'fixed-puppies',
-		// }),
-		path: '/Front/https://www.theguardian.com/uk/sport',
-		section: 'sport',
-	},
-] as const satisfies Front[];
+	{ path: '/Front/https://www.theguardian.com/uk' },
+	{ path: '/Front/https://www.theguardian.com/uk/commentisfree' },
+	{ path: '/Front/https://www.theguardian.com/uk/sport' },
+] as const satisfies GuPage[];
 
 const tagPages = [
-	{
-		// path: getTestUrl({
-		// 	path: '/tone/recipes/all',
-		// 	type: 'tagPage',
-		// 	adtest: 'fixed-puppies',
-		// }),
-		path: '/TagPage/https://www.theguardian.com/tone/recipes/all',
-	},
-] as const satisfies TagPage[];
+	{ path: '/TagPage/https://www.theguardian.com/tone/recipes/all' },
+] as const satisfies GuPage[];
 
 const frontWithPageSkin = {
-	// path: getTestUrl({
-	// 	path: '/uk',
-	// 	type: 'front',
-	// 	adtest: 'puppies-pageskin',
-	// }),
 	path: '/Front/https://www.theguardian.com/uk',
-	section: 'uk',
-} as const satisfies Front;
+} as const satisfies GuPage;
 
 const frontWithExclusion = {
-	// path: getTestUrl({
-	// 	path: '/us-news/baltimore-bridge-collapse',
-	// 	type: 'tagPage',
-	// 	adtest: 'clear',
-	// }),
-	path: '/Front/https://www.theguardian.com/us-news/baltimore-bridge-collapse',
-	section: 'us-news',
-} as const satisfies Front;
+	path: '/TagPage/https://www.theguardian.com/us-news/baltimore-bridge-collapse',
+} as const satisfies GuPage;
 
 export { fronts, frontWithExclusion, frontWithPageSkin, tagPages };
