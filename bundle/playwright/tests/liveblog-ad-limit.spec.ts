ed1ef10f4842f6002a8fba8395561d0ca65f645f
Ravi <7014230+arelra@users.noreply.github.com>
2025-08-28 18:49:01 +0100

    Improve liveblog-ad-limit.spec.ts to be less flakely

--- bundle/playwright/tests/liveblog-ad-limit.spec.ts
+++ bundle/playwright/tests/liveblog-ad-limit.spec.ts
@@ -6,7 +6,6 @@ import { breakpoints } from '../lib/breakpoints';
 import { cmpAcceptAll } from '../lib/cmp';
 import { loadPage } from '../lib/load-page';
 import { expectLocatorToBeVisible } from '../lib/locators';
-import { countLiveblogInlineSlots } from '../lib/util';
 
 const pages = blogs.filter(
 	(blog) => 'name' in blog && blog.name === 'under-ad-limit',
@@ -18,37 +17,38 @@ const desktopBreakpoint = breakpoints.filter(
 
 const MAX_AD_SLOTS = 8;
 
-const addAndAwaitNewBlocks = async (page: Page, blockContent: string) => {
+const addAndAwaitNewBlocks = async (page: Page, batch: number) => {
 	// scroll to the top so we get a toast to click on
 	await page.evaluate(() => window.scrollTo(0, 0));
 	// @ts-expect-error -- browser land
 	await page.waitForFunction(() => window.mockLiveUpdate !== undefined);
-	await page.evaluate((blockContent) => {
+	await page.evaluate((batch) => {
 		// @ts-expect-error -- browser land
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-call -- browser land
 		window.mockLiveUpdate({
 			numNewBlocks: 10,
 			html: `
-				<p style="height:2000px;" class="pending block">${blockContent}</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
-				<p style="height:2000px;" class="pending block">New block</p>
+				<p style="height:2000px;" >Batch ${batch} of inserted blocks</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.1</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.2</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.3</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.4</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.5</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.6</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.7</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.8</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.9</p>
+				<p style="height:2000px;" class="pending block">New block ${batch}.10</p>
 			`,
 			mostRecentBlockId: 'abc',
 		});
-	}, blockContent);
+	}, batch);
 
 	// click on the toast to insert and scroll to the new blocks
 	await page.getByRole('button', { name: '10 new updates' }).click();
 
-	// Here, we force the test to wait until the blocks are visible on the page before counting the ad slots
-	const newBlock = page.getByText(blockContent).first();
+	// Here, we force the test to wait until the pending blocks are visible on the page before counting the ad slots
+	const newBlock = page.getByText(`New block ${batch}.10`);
 	await expectLocatorToBeVisible(newBlock);
 };
 
@@ -63,9 +63,11 @@ test.describe('A minimum amount of ad slots load', () => {
 		 * It is important that the initial amount of ad slots on the page does not
 		 * equal the maximum amount of ad slots for this test to have value
 		 */
-		test('Extra ads are inserted upon update, but only up to the limit', async ({
+		test('Extra ads are inserted upon update, but only up to the max limit', async ({
 			page,
 		}) => {
+			const inlineSlotsSelector = `#liveblog-body .ad-slot--liveblog-inline`;
+
 			await page.setViewportSize({
 				width: desktopBreakpoint.width,
 				height: desktopBreakpoint.height,
@@ -74,27 +76,30 @@ test.describe('A minimum amount of ad slots load', () => {
 			await loadPage({ page, path, queryParams: { live: 'true' } });
 			await cmpAcceptAll(page);
 
-			const initialSlotCount = await countLiveblogInlineSlots(
-				page,
-				false,
+			await expect(page.locator(inlineSlotsSelector)).toHaveCount(
+				expectedMinInlineSlots.desktop,
+				{
+					timeout: 10000,
+				},
 			);
 
-			expect(initialSlotCount).toEqual(expectedMinInlineSlots.desktop);
+			await addAndAwaitNewBlocks(page, 1);
 
-			await addAndAwaitNewBlocks(page, 'First batch of inserted blocks');
-			const slotCountAfterFirstInsert = await countLiveblogInlineSlots(
-				page,
-				false,
+			await expect(page.locator(inlineSlotsSelector)).toHaveCount(
+				MAX_AD_SLOTS,
+				{
+					timeout: 10000,
+				},
 			);
-			expect(slotCountAfterFirstInsert).toBeGreaterThan(initialSlotCount);
-			expect(slotCountAfterFirstInsert).toEqual(MAX_AD_SLOTS);
 
-			await addAndAwaitNewBlocks(page, 'Second batch of inserted blocks');
-			const slotCountAfterSecondInsert = await countLiveblogInlineSlots(
-				page,
-				false,
+			await addAndAwaitNewBlocks(page, 2);
+
+			await expect(page.locator(inlineSlotsSelector)).toHaveCount(
+				MAX_AD_SLOTS,
+				{
+					timeout: 10000,
+				},
 			);
-			expect(slotCountAfterSecondInsert).toEqual(MAX_AD_SLOTS);
 		});
 	});
 });
