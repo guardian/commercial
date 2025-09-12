f27d04dac09124e41f8912ad005b6c47bea8e743
Dina Hafez <dina.hafez@guardian.co.uk>
2025-09-12 13:08:48 +0100

    Update brand metrics script based on region (#2217)
    * Update brand metrics script based on region
    
    * Fix unit tests and add more
    
    * Update logic to not run the script when the region isn't supported
    
    * Update uk script to be for UK and fallback to any region except US and AUS
    
    * Add comment to clarify the regions script matches the consent framework

--- bundle/src/lib/third-party-tags/inizio.spec.ts
+++ bundle/src/lib/third-party-tags/inizio.spec.ts
@@ -1,13 +1,95 @@
+import {
+	isInAustralia,
+	isInRow,
+	isInUk,
+	isInUsa,
+} from '@guardian/commercial-core/geo/geo-utils';
 import { _, inizio } from './inizio';
+
+jest.mock('@guardian/commercial-core/geo/geo-utils', () => ({
+	isInUk: jest.fn(() => false),
+	isInUsa: jest.fn(() => false),
+	isInAustralia: jest.fn(() => false),
+	isInRow: jest.fn(() => false),
+}));
 
 describe('index', () => {
 	it('should use the feature switch option', () => {
+		(isInUk as jest.Mock).mockReturnValue(true);
+		(isInUsa as jest.Mock).mockReturnValue(false);
+		(isInAustralia as jest.Mock).mockReturnValue(false);
+
 		const inizioInstance = inizio({ shouldRun: true });
+
 		expect(inizioInstance).toMatchObject({
 			shouldRun: true,
-			url: '//cdn.brandmetrics.com/survey/script/e96d04c832084488a841a06b49b8fb2d.js',
+			url: '//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_uk.js',
 			name: 'inizio',
 		});
+	});
+});
+
+describe('scriptBasedOnRegion', () => {
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	it('should return UK script URL when user is in UK', () => {
+		(isInUsa as jest.Mock).mockReturnValue(false);
+		(isInAustralia as jest.Mock).mockReturnValue(false);
+		(isInRow as jest.Mock).mockReturnValue(false);
+
+		const result = inizio({ shouldRun: true });
+
+		expect(result.url).toBe(
+			'//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_uk.js',
+		);
+		expect(isInUsa).toHaveBeenCalled();
+		expect(isInAustralia).toHaveBeenCalled();
+	});
+
+	it('should return US script URL when user is in US', () => {
+		(isInUsa as jest.Mock).mockReturnValue(true);
+		(isInAustralia as jest.Mock).mockReturnValue(false);
+		(isInRow as jest.Mock).mockReturnValue(false);
+
+		const result = inizio({ shouldRun: true });
+
+		expect(result.url).toBe(
+			'//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_us.js',
+		);
+		expect(isInUsa).toHaveBeenCalled();
+		expect(isInAustralia).not.toHaveBeenCalled();
+	});
+
+	it('should return AUS script URL when user is in AUS', () => {
+		(isInUk as jest.Mock).mockReturnValue(false);
+		(isInUsa as jest.Mock).mockReturnValue(false);
+		(isInAustralia as jest.Mock).mockReturnValue(true);
+		(isInRow as jest.Mock).mockReturnValue(false);
+
+		const result = inizio({ shouldRun: true });
+
+		expect(result.url).toBe(
+			'//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_au.js',
+		);
+		expect(isInUsa).toHaveBeenCalled();
+		expect(isInAustralia).toHaveBeenCalled();
+	});
+
+	it('should return UK script URL when user is in anywhere except US and AUS', () => {
+		(isInUk as jest.Mock).mockReturnValue(false);
+		(isInUsa as jest.Mock).mockReturnValue(false);
+		(isInAustralia as jest.Mock).mockReturnValue(false);
+		(isInRow as jest.Mock).mockReturnValue(true);
+
+		const result = inizio({ shouldRun: true });
+
+		expect(result.url).toBe(
+			'//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_uk.js',
+		);
+		expect(isInUsa).toHaveBeenCalled();
+		expect(isInAustralia).toHaveBeenCalled();
 	});
 });
 
--- bundle/src/lib/third-party-tags/inizio.ts
+++ bundle/src/lib/third-party-tags/inizio.ts
@@ -1,3 +1,7 @@
+import {
+	isInAustralia,
+	isInUsa,
+} from '@guardian/commercial-core/geo/geo-utils';
 import type { GetThirdPartyTag } from '../types';
 
 const handleQuerySurveyDone = (
@@ -26,13 +30,29 @@ const onLoad = (): void => {
 };
 
 /**
+ * This function matches the consent Framework
+ * https://github.com/guardian/csnx/blob/f5b5bd6371e798daf76b777788ccc040cba7727f/libs/%40guardian/libs/src/consent-management-platform/getFramework.ts#L4
+ */
+const scriptBasedOnRegion = (): string => {
+	if (isInUsa()) {
+		return '//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_us.js';
+	}
+
+	if (isInAustralia()) {
+		return '//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_au.js';
+	}
+
+	return '//cdn.brandmetrics.com/tag/c3330059-9ad5-4d32-8e7a-e9f6c7d74957/the_guardian_uk.js';
+};
+
+/**
  * Allows creatives to show survey
  * https://trello.com/c/wHffHVF1/171-integrate-and-test-inizio
  * @param  {} {shouldRun}
  */
 export const inizio: GetThirdPartyTag = ({ shouldRun }) => ({
 	shouldRun,
-	url: '//cdn.brandmetrics.com/survey/script/e96d04c832084488a841a06b49b8fb2d.js',
+	url: scriptBasedOnRegion(),
 	name: 'inizio',
 	onLoad,
 });
