d542ee6fcde3d0f239066f67ced2bae5fadd3ad7
Charlotte <charlotte.emms@theguardian.com>
2025-09-24 09:52:47 +0100

    extend Admiral AB test by a month

--- bundle/src/experiments/tests/admiral-adblocker-recovery.ts
+++ bundle/src/experiments/tests/admiral-adblocker-recovery.ts
@@ -4,7 +4,7 @@ export const admiralAdblockRecovery: ABTest = {
 	id: 'AdmiralAdblockRecovery',
 	author: '@commercial-dev',
 	start: '2025-08-13',
-	expiry: '2025-09-25',
+	expiry: '2025-10-23',
 	audience: 100 / 100,
 	audienceOffset: 0 / 100,
 	audienceCriteria: '',
