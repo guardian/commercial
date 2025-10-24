9c7838f6e92448736d2c2298786a172c4f32d316
Dina Hafez <dina.hafez@guardian.co.uk>
2025-10-24 09:22:14 +0100

    Update deprecated methods in teads and passpack files

--- core/src/targeting/teads-eligibility.ts
+++ core/src/targeting/teads-eligibility.ts
@@ -4,12 +4,7 @@ const isEligibleForTeads = (slotId: string) => {
 	const { contentType, isSensitive } = window.guardian.config.page;
 
 	// This IAS value is returned when a page is thought to contain content which is not brand safe
-	const pubads = window.googletag.pubads() as googletag.PubAdsService & {
-		getConfig: (key: string) => {
-			targeting?: Record<string, string | string[] | null>;
-		};
-	};
-	const iasKw = pubads.getConfig('targeting').targeting?.['ias-kw'];
+	const iasKw = window.googletag.getConfig('targeting').targeting?.['ias-kw'];
 	const iasKwArray = Array.isArray(iasKw) ? iasKw : iasKw ? [iasKw] : [];
 	const isBrandSafe = !iasKwArray.includes('IAS_16425_KW');
 
