ee8ea2289afa8267157f3bcdcddca37e81f0f0e9
Demetrios <dskamiotis@gmail.com>
2026-01-07 11:39:49 +0000

    add playright test for response to events from dcr

---
+++ bundle/playwright/tests/mobile-sticky-slot.ts
@@ -0,0 +1,65 @@
+import { expect, test } from '@playwright/test';
+import { testAtBreakpoints } from '../lib/breakpoints';
+import { loadPage } from '../lib/load-page';
+import { cmpAcceptAll } from '../lib/cmp';
+import { articles } from '../fixtures/pages';
+import { GuPage } from '../fixtures/pages/Page';
+
+const { path } = articles[0] as unknown as GuPage;
+
+testAtBreakpoints(['mobile']).forEach(({ breakpoint, width, height }) => {
+	test(`mobile sticky responds to banner:close event at ${breakpoint}`, async ({
+		page,
+	}) => {
+		await page.setViewportSize({ width, height });
+
+		await page.addInitScript(() => {
+			window.localStorage.removeItem(
+				'gu.prefs.engagementBannerLastClosedAt',
+			);
+		});
+
+		await loadPage({ page, path, region: 'US' });
+		await cmpAcceptAll(page);
+		await loadPage({
+			page,
+			path,
+			region: 'US',
+			queryParams: {
+				adtest: 'mobileStickyTest',
+			},
+		});
+
+		await expect(page.locator('#dfp-ad--mobile-sticky')).not.toBeAttached();
+
+		await page.evaluate(() => {
+			document.dispatchEvent(new Event('banner:close'));
+		});
+
+		await page.waitForTimeout(1000);
+		await expect(page.locator('#dfp-ad--mobile-sticky')).toBeAttached();
+	});
+});
+testAtBreakpoints(['mobile']).forEach(({ breakpoint, width, height }) => {
+	test(`mobile sticky responds to banner:none event at ${breakpoint}`, async ({
+		page,
+	}) => {
+		await page.setViewportSize({ width, height });
+		await loadPage({
+			page,
+			path,
+			region: 'US',
+			queryParams: {
+				adtest: 'mobileStickyTest',
+			},
+		});
+
+		await cmpAcceptAll(page);
+		await page.evaluate(() => {
+			document.dispatchEvent(new Event('banner:none')); 
+		});
+
+		await page.waitForTimeout(1000);
+		await expect(page.locator('#dfp-ad--mobile-sticky')).toBeVisible();
+	});
+});
