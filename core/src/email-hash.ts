85a9f2bdc9e9477a5bfdbf6b3c8bd5d569a16f7b
Demetrios <dskamiotis@gmail.com>
2026-01-13 17:41:13 +0000

    fix linting issue

--- core/src/email-hash.ts
+++ core/src/email-hash.ts
@@ -31,7 +31,7 @@ async function hashEmailForClient(
 	client: HashClient,
 ): Promise<string> {
 	const normalisedEmail = normaliseEmail(email);
-	console.log(`HERE: OG: ${email} hashed::: ${normalisedEmail}`)
+	console.log(`HERE: OG: ${email} hashed::: ${normalisedEmail}`);
 	const utf8 = new TextEncoder().encode(normalisedEmail);
 	const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
 	switch (client) {
