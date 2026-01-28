8ceca98192139918a0bc09f3af8bee53404a052a
Imogen Hardy <imogen.hardy@guardian.co.uk>
2026-01-28 16:37:12 +0000

    top level static and dynamic ad setup

--- bundle/src/init/consented-advertising.ts
+++ bundle/src/init/consented-advertising.ts
@@ -1,3 +1,6 @@
+import type { Advert } from '@guardian/commercial-core/types';
+import { displayAds } from '../display/display-ads';
+import { enableLazyLoad } from '../display/lazy-load';
 import { bootCommercial } from '../lib/commercial-boot-utils';
 import { initDfpListeners } from './consented/dfp-listeners';
 import { initDynamicAdSlots } from './consented/dynamic-ad-slots';
@@ -21,12 +24,50 @@ const commercialModules = [
 // 3. auction and orchestration
 // 4. render and measure
 
-const init = async () => {
-	await prepareGoogletag();
+class AdController {
+	#staticAds: [];
+	#dynamicAds: [];
 
-	void fillStaticAdvertSlots();
+	constructor() {
+		this.#staticAds = [];
+		this.#dynamicAds = [];
+	}
+
+	// Move the responsibility for managing lazy loading to the top level
+	// Reduce dependency on dfpEnv and remove redundant code- we can assume IntersectionObserver support
+	// and enable/disable lazy loading using commercial features
+	public newDynamicAd(ad: Advert) {
+		this.#dynamicAds.push(ad);
+		enableLazyLoad(ad);
+	}
+
+	public newStaticAd(ad: Advert) {
+		this.#staticAds.push(ad);
+		enableLazyLoad(ad);
+	}
+
+	public getAllAds() {
+		return this.#dynamicAds.concat(this.#staticAds);
+	}
+}
+
+const init = async () => {
+	const adController = new AdController();
+
+	const googletag = await prepareGoogletag();
+
+	// Pass in the ability to create new dynamic ads- this cannot be controlled at the top level
+	prepareSpacefinder(googletag, adController.newDynamicAd.bind(adController));
+
+	await fillStaticAdvertSlots(googletag);
 
 	setPageTargeting(consentState, isSignedIn);
+
+	const staticAds = await createStaticAds(googletag, adController);
+
+	staticAds.forEach((ad) => {
+		adController.newStaticAd(ad);
+	});
 };
 
 // const bootCommercialWhenReady = () => {
