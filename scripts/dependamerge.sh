f645f43dce4258203610635cd451ed95b9681236
Jake Lee Kennedy <jake.kennedy@guardian.co.uk>
2026-03-04 10:40:13 +0000

    update dependamerge

--- scripts/dependamerge.sh
+++ scripts/dependamerge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# This script automatically merges any branches created by Dependabot PRs onto a single branch, for testing
-BRANCHES=$(git branch -a --list '*/dependabot*')
+# This script automatically merges any open Dependabot PRs onto a single branch, for testing
+BRANCHES=$(gh pr list --author "app/dependabot" --state open --json headRefName --jq '.[].headRefName')
 CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
 if [[ "$CURRENT_BRANCH" == "main" ]]; then
   echo 'You are on main, aborting script!';
   exit 1;
