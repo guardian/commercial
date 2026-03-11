69e687df9bd3f30272dabe1d67d261efdc97d9fd
Demetrios <dskamiotis@gmail.com>
2026-03-11 12:33:46 +0000

    adds jsDoc comments to register-advert

--- bundle/src/lib/dfp/register-advert.ts
+++ bundle/src/lib/dfp/register-advert.ts
@@ -7,6 +7,11 @@ type AdEventListener = {
 };
 let listenerStore: AdEventListener[] = [];
 
+/**
+ * @param status - the advert status(es) to listen for
+ * @param callback -  callback to run when an advert reaches the given status
+ * @returns void
+ */
 const addListenerToStore = (
 	status: AdvertStatus | AdvertStatus[],
 	callback: (advert: Advert) => void,
@@ -14,12 +19,19 @@ const addListenerToStore = (
 	listenerStore.push({ status, callback });
 };
 
+/**
+ * @param advert - as defined in lib/dfp/Advert
+ * @returns void
+ *
+ *  Registers all stored listeners on the given advert.
+ */
 const registerAdvert = (advert: Advert) => {
 	listenerStore.forEach((listener) => {
 		advert.on(listener.status, () => listener.callback(advert));
 	});
 };
 
+/** Clears the listener store. Used in tests. */
 const clearListenerStore = () => {
 	listenerStore = [];
 };
