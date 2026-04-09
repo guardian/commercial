dc5109250eb53f1210a97878e333baa6666cee94
Demetrios <dskamiotis@gmail.com>
2026-04-09 14:20:34 +0100

    add type guard to check event is AdEventCustomEvent

--- core/src/global-ad-events.ts
+++ core/src/global-ad-events.ts
@@ -27,16 +27,30 @@ function globalAdEvents(
 	listenerHandler: (event: AdEventCustomEvent) => void,
 	slotName?: string,
 ) {
-	const newStatus = Array.isArray(status) ? status : [status];
+	const parsedStatus = Array.isArray(status) ? status : [status];
 	const matches = (event: AdEventCustomEvent) => {
-		const statusMatch = newStatus.includes(event.detail.name);
+		const statusMatch = parsedStatus.includes(event.detail.name);
 		const slotMatch = !slotName || event.detail.slotName === slotName;
 		return statusMatch && slotMatch;
 	};
+
+	const isCustomEvent = (e: Event): e is AdEventCustomEvent => {
+		return (
+			e instanceof CustomEvent &&
+			typeof e.detail === 'object' &&
+			e.detail !== null &&
+			'name' in e.detail &&
+			'slotName' in e.detail &&
+			'status' in e.detail
+		);
+	};
+
 	const listener = (e: Event) => {
-		const event = e as AdEventCustomEvent;
-		if (matches(event)) {
-			listenerHandler(event);
+		if (!isCustomEvent(e)) {
+			return;
+		}
+		if (matches(e)) {
+			listenerHandler(e);
 		}
 	};
 
