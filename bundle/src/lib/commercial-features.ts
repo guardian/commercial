9310b1447ed1546e2e0ba1bb62121e44dd23fcf5
Demetrios Skamiotis <49187886+dskamiotis@users.noreply.github.com>
2026-04-15 14:30:47 +0100

    reomve footballFixturesAdverts from commercialFeatures (#2521)

--- bundle/src/lib/commercial-features.ts
+++ bundle/src/lib/commercial-features.ts
@@ -5,24 +5,11 @@ import { isSecureContactPage } from './is-secure-contact';
 class CommercialFeatures {
 	adFree: boolean;
 	comscore: boolean;
-	footballFixturesAdverts: boolean;
 
 	constructor() {
 		const isIdentityPage =
 			window.guardian.config.page.contentType === 'Identity' ||
 			window.guardian.config.page.section === 'identity'; // needed for pages under profile.* subdomain
-
-		// Detect presence of space for football-right ad slot
-		const { pageId } = window.guardian.config.page;
-		const isFootballPage = pageId.startsWith('football/');
-		const isPageWithRightAdSpace =
-			pageId.endsWith('/fixtures') ||
-			pageId.endsWith('/live') ||
-			pageId.endsWith('/results') ||
-			pageId.endsWith('/tables') ||
-			pageId.endsWith('/table');
-
-		this.footballFixturesAdverts = isFootballPage && isPageWithRightAdSpace;
 
 		this.adFree = isAdFree();
 
