9fdf34a538c16b79a9ffb87aa43f46861ceaded6
Charlotte <charlotte.emms@theguardian.com>
2026-05-06 15:01:09 +0100

    remove webpack public path from declaration file, ignoring the ts-error inline instead

--- bundle/src/types/globals.d.ts
+++ bundle/src/types/globals.d.ts
@@ -1,9 +1,3 @@
 import 'google-publisher-tag';
 
 declare const googletag;
-
-/**
- * The public path for Webpack assets
- * @see https://webpack.js.org/guides/public-path
- */
-declare const __webpack_public_path__: string;
