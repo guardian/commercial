3646330a321a7310a265f3910245fe0bd8fc92b5
Charlotte <charlotte.emms@theguardian.com>
2026-05-06 14:59:41 +0100

    add new globals file with googletag types and webpack public path variable

---
+++ bundle/src/types/globals.d.ts
@@ -0,0 +1,9 @@
+import 'google-publisher-tag';
+
+declare const googletag;
+
+/**
+ * The public path for Webpack assets
+ * @see https://webpack.js.org/guides/public-path
+ */
+declare const __webpack_public_path__: string;
