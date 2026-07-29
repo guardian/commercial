dada8b14b38744401d8a7244d8b899f1c48924e4
ollie_abrahams <ollie.abrahams@guardian.co.uk>
2026-07-29 12:01:44 +0100

    add mute button to interscroller video

--- bundle/src/lib/messenger/background.ts
+++ bundle/src/lib/messenger/background.ts
@@ -239,6 +239,50 @@ const setupBackground = async (
 				video.style.transform = 'translate(-50%, -50%)';
 				background.appendChild(video);
 
+				// Mute/unmute toggle button
+				const muteButton = document.createElement('button');
+				muteButton.classList.add(
+					'interscroller-video__mute-button',
+				);
+				muteButton.setAttribute('aria-label', 'Unmute video');
+				muteButton.style.position = 'absolute';
+				muteButton.style.bottom = '48px';
+				muteButton.style.right = '16px';
+				muteButton.style.zIndex = '2'; // todo check this
+				muteButton.style.width = '32px';
+				muteButton.style.height = '32px';
+				muteButton.style.borderRadius = '50%';
+				muteButton.style.border = 'none';
+				muteButton.style.backgroundColor =
+					'rgba(0, 0, 0, 0.6)';
+				muteButton.style.cursor = 'pointer';
+				muteButton.style.display = 'flex';
+				muteButton.style.alignItems = 'center';
+				muteButton.style.justifyContent = 'center';
+				muteButton.style.padding = '0';
+				muteButton.innerHTML = `<svg width="16" height="16" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M3 9v6h4l5 5V4L7 9H3z"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63z"/><path d="M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71z"/><path d="M4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3z"/></svg>`;
+
+				muteButton.addEventListener('click', (e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					video.muted = !video.muted;
+					if (video.muted) {
+						muteButton.setAttribute(
+							'aria-label',
+							'Unmute video',
+						);
+						muteButton.innerHTML = `<svg width="16" height="16" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M3 9v6h4l5 5V4L7 9H3z"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63z"/><path d="M19 12c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71z"/><path d="M4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3z"/></svg>`;
+					} else {
+						muteButton.setAttribute(
+							'aria-label',
+							'Mute video',
+						);
+						muteButton.innerHTML = `<svg width="16" height="16" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg"><path d="M3 9v6h4l5 5V4L7 9H3z"/><path d="M14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/><path d="M14 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02 0-1.77-1.02-3.29-2.5-4.03z"/></svg>`;
+					}
+				});
+
+				backgroundParent.appendChild(muteButton);
+
 				let played = false;
 				video.onended = () => (played = true);
 
