4163cfd596ef297199d7f39173646d9340b633c2
Dominik Lander <dominik.lander@guardian.co.uk>
2026-09-07 18:21:40 +0100

    Add rich link height calculation function and test

---
+++ bundle/playwright/tests/rich-links.spec.ts
@@ -0,0 +1,55 @@
+import { expect, test } from '@playwright/test';
+import { breakpoints } from '../lib/breakpoints';
+import { cmpAcceptAll } from '../lib/cmp';
+import { loadPage } from '../lib/load-page';
+import { waitForIsland } from '../lib/util';
+
+const url =
+	'/Article/https://www.theguardian.com/football/2026/sep/06/exceptional-arteta-arsenal-fightback-win-chelsea';
+
+/**
+ * Within Spacefinder, we make an assumption on the height of the rich link images in
+ * order to place adverts appropriately. If the display of rich link images changes,
+ * we may need to revisit this assumption.
+ */
+test.describe('Rich links', () => {
+	const expectedImageHeightsByBreakpoint = {
+		mobile: 0,
+		tablet: 112,
+		desktop: 112,
+		wide: 176,
+	};
+
+	breakpoints.forEach(({ breakpoint, width, height }) => {
+		test(`Rich link has expected dimensions ${breakpoint}`, async ({
+			page,
+		}) => {
+			await page.setViewportSize({
+				width,
+				height,
+			});
+
+			await loadPage({ page, path: url });
+			await cmpAcceptAll(page);
+
+			await waitForIsland(page, 'RichLinkComponent');
+
+			// const richLinkImage = page.getByTestId('rich-link-image');
+			const richLinkImage = page.getByAltText(
+				'Martin Ødegaard (right) with Declan Rice',
+			);
+
+			const expectedImageHeight =
+				expectedImageHeightsByBreakpoint[breakpoint];
+
+			if (expectedImageHeight === 0) {
+				await expect(richLinkImage).not.toBeVisible();
+				return;
+			}
+
+			const boundingBox = await richLinkImage.boundingBox();
+
+			expect(boundingBox?.height).toBe(expectedImageHeight);
+		});
+	});
+});
---
+++ bundle/src/insert/spacefinder/richLinks.ts
@@ -0,0 +1,35 @@
+import { getCurrentTweakpoint } from '../../lib/detect/detect-breakpoint';
+
+const IMAGE_HEIGHT_DEFAULT = 112;
+// No image is displayed at the mobile breakpoint. An image is displayed from the mobileMedium breakpoint onwards.
+const IMAGE_HEIGHT_MOBILE = 0;
+const IMAGE_HEIGHT_WIDE = 176;
+
+/**
+ * Spacefinder requires opponents on the page to have consistent dimensions so that it can reliably place adverts
+ * at appropriate places within the article content. Rich links don't fit this requirement, as the image is obtained
+ * via a client-side request which is made after the initial page load, making its dimensions unpredictable.
+ *
+ * We do not wait for the rich link image to load before running Spacefinder, as this may cause a delay. Any delays
+ * to inserting ad slots can be costly.
+ *
+ * Fortunately, the rich link text content is rendered server-side, which means that the dimensions of this part of
+ * the rich link component is consistent. The dimensions of the image can be calculated, given the user's breakpoint.
+ *
+ * However, despite the vast majority of rich links having images, not all do. Therefore we err on the side of
+ * caution: we assume that a rich link _will_ have an image and risk placing an ad further down the page than
+ * we need to, rather than assuming it doesn't and risk colliding with the rich link component.
+ */
+const calculateRichLinkImageHeight = (): number => {
+	const tweakpoint = getCurrentTweakpoint();
+	switch (tweakpoint) {
+		case 'mobile':
+			return IMAGE_HEIGHT_MOBILE;
+		case 'wide':
+			return IMAGE_HEIGHT_WIDE;
+		default:
+			return IMAGE_HEIGHT_DEFAULT;
+	}
+};
+
+export default calculateRichLinkImageHeight;
