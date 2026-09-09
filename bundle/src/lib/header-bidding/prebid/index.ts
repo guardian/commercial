a826cd127ee2e536014a381a80ab5efc3f927588
Daniel Clifton <110032454+DanielCliftonGuardian@users.noreply.github.com>
2026-09-09 11:19:37 +0100

    Launch prebid transaction IDs (#2718)
    * Launch prebid transaction ID's
    
    * Prettier

--- bundle/src/lib/header-bidding/prebid/index.ts
+++ bundle/src/lib/header-bidding/prebid/index.ts
@@ -56,11 +56,6 @@ const initialise = async (
 		'holdback',
 	);
 
-	const shouldEnableTransactionIds = isUserInTestGroup(
-		'commercial-prebid-transaction-ids',
-		'variant',
-	);
-
 	window.pbjs.setConfig({
 		/**
 		 * The amount of time reserved for the auction
@@ -83,7 +78,7 @@ const initialise = async (
 			: {}),
 		priceGranularity: 'custom',
 		customPriceBucket: priceGranularity,
-		enableTIDs: shouldEnableTransactionIds,
+		enableTIDs: true,
 		userSync,
 		ortb2: {
 			site: {
